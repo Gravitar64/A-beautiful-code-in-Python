886cdcba1f9d545d16d4397b85a36b388109539c
Gravitar64 <gravitar@web.de>
2018-03-12 19:39:48 +0100

    Variablennamen sprechend benannt

--- spam.py
+++ spam.py
@@ -1,17 +1,17 @@
 from random import shuffle
 
-liste = "amazing gorgeous blazing stunning tremendous greatest best fantastic phenomenal \
-         delightful ambitious outstanding incredible spectacular super cool magical \
-         revolutionary beautiful jaw-dropping".upper().split()
-shuffle(liste)
+superlative = "amazing gorgeous blazing stunning tremendous greatest best fantastic \
+               phenomenal delightful ambitious outstanding incredible spectacular \
+               super cool magical revolutionary beautiful jaw-dropping lovely".upper().split()
+shuffle(superlative)
 
 for strophe in range(5):
-    for n a in range(2):
-        for i in range(4):
-             print("SPAM ", end='')
+    for zeile in range(2):
+        for word in range(4):
+            print("SPAM ", end='')
         print()
-    el1 = liste.pop()
-    el2 = liste.pop()
+    el1 = superlative.pop()
+    el2 = superlative.pop()
 
     print("{} SPAM, {} SPAM".format(el1, el2))
     print()
