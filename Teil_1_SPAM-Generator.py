81f58474c4601facdccdcb41ef1da2c1b44040f5
Gravitar64 <gravitar@web.de>
2018-03-14 01:01:52 +0100

    Bezeichner für Liste auf Plural geändert

--- Teil_1_SPAM-Generator.py
+++ Teil_1_SPAM-Generator.py
@@ -1,17 +1,17 @@
 from random import shuffle
 
-superlative = "amazing gorgeous blazing stunning tremendous greatest best fantastic \
+superlatives = "amazing gorgeous blazing stunning tremendous greatest best fantastic \
                phenomenal delightful ambitious outstanding incredible spectacular \
                super cool magical revolutionary beautiful jaw-dropping lovely".upper().split()
-shuffle(superlative)
+shuffle(superlatives)
 
 for strophe in range(5):
     for zeile in range(2):
         for word in range(4):
             print("SPAM ", end='')
         print()
-    el1 = superlative.pop()
-    el2 = superlative.pop()
+    el1 = superlatives.pop()
+    el2 = superlatives.pop()
 
     print("{} SPAM, {} SPAM".format(el1, el2))
     print()
