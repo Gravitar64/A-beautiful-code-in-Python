6455eee0f25b92a7dd4dab8b3df088c7fe091be1
Gravitar64 <gravitar@web.de>
2018-03-18 21:25:47 +0100

    Jetzt mit Nutzung des neuen F-Strings (seit Python 3.6)!

--- Teil_2_Schachbrett_Reiskörner.py
+++ Teil_2_Schachbrett_Reiskörner.py
@@ -3,10 +3,11 @@ summe = 0
 for feld in range(64):
     reiskörner = 2**feld
     summe += reiskörner
-    print("Feld {}. = {:>27,} Reiskörner und damit insgesamt"
-          "{:>28,} Reiskörner".format(feld+1, reiskörner, summe))
+    print(f'Feld {feld+1}. = {reiskörner:>27,} Reiskörner und damit insgesamt'
+          f'{summe:>28,} Reiskörner')
+    
 
 gewicht = summe * 0.02 / 1000 / 1000
 print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
-      " Reiskörner {:,.0f} Tonnen".format(gewicht))
+      f" Reiskörner {gewicht:,.0f} Tonnen")
