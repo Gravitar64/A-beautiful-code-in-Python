c9f5514fe5f9578bba4e16d41d76622293e97b41
Gravitar64 <gravitar@web.de>
2018-03-18 21:53:36 +0100

    Teil 3 Schachbrett + Reiskörner grafisch

--- Teil_3_Schachbrett_grafisch.py
+++ Teil_3_Schachbrett_grafisch.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 summe = 0
+reis = []
 
 for feld in range(64):
     reiskörner = 2**feld
+    reis.append(reiskörner)
     summe += reiskörner
     print(f"Feld {feld+1}. = {reiskörner:>27,} Reiskörner und damit insgesamt"
           f"{summe:>28,} Reiskörner")
@@ -11,3 +14,6 @@ gewicht = summe * 0.02 / 1000 / 1000
 print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
       f" Reiskörner {gewicht:,.0f} Tonnen")
+
+plt.plot(reis)
+plt.show()
