c113336bff81a3c3e0714acab00bd6bc49fbff9c
Gravitar64 <gravitar@web.de>
2018-03-18 21:14:56 +0100

    Kleine Optimierungen

--- Teil_2_Schachbrett_Reiskörner.py
+++ Teil_2_Schachbrett_Reiskörner.py
@@ -1,10 +1,10 @@
 summe = 0
 
 for feld in range(64):
-    reiskorn = 2**feld
-    summe = summe + reiskorn
+    reiskörner = 2**feld
+    summe += reiskörner
     print("Feld {}. = {:>30,} Reiskörner und damit insgesamt" \
-          "{:>30,} Reiskörner".format(feld+1, reiskorn, summe))
+          "{:>30,} Reiskörner".format(feld+1, reiskörner, summe))
 
 gewicht = summe * 0.02 / 1000 / 1000
 print()
