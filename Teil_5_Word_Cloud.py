705ac53630e296ee448474ed8225cc8f00bc2226
Gravitar64 <gravitar@web.de>
2018-06-17 14:25:22 +0200

    Twitter API

--- Teil_5_Word_Cloud.py
+++ Teil_5_Word_Cloud.py
@@ -5,11 +5,8 @@ with open("alice_in_wonderland.txt") as f:
     text = f.read()
 
 wordcloud = WordCloud(width=1920, height=1200)
-
-STOPWORDS.add('said')
-STOPWORDS.add('illustration')
-
+STOPWORDS.update(['said', 'illustration'])
 wordcloud.generate(text)
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.imshow(wordcloud)
 plt.axis('off')
-plt.show()    
+plt.show()
---
+++ Teil_6_Twitter.py
@@ -0,0 +1,19 @@
+import tweepy
+from Twitter_Credentials import *
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+auth = tweepy.OAuthHandler(ConsumerKey,ConsumerSecret)
+api = tweepy.API(auth)
+text = ''
+
+tweeds = api.user_timeline(screen_name='realDonaldTrump', count=100, include_rts = False, tweet_mode='extended')
+for tweed in tweeds:
+    text = text + ' '+ tweed.full_text
+
+wordcloud = WordCloud(width=1920, height=1200)
+STOPWORDS.update(['https', 'co'])
+wordcloud.generate(text)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
