8b59728211c64b64e3c90a2ef15924c009389608
Gravitar64 <gravitar@web.de>
2018-11-01 22:32:40 +0100

    Teil 10, Minesweeper Variablennamen sprechend gestaltet

--- Teil_10_Minesweeper.py
+++ Teil_10_Minesweeper.py
@@ -2,24 +2,29 @@ import pygame as pg
 from dataclasses import dataclass
 import random as rnd
 
-
-auflösung = 500
-raster = 9
-anzMinen = 10
+auflösung = 1000
+raster = 20
+anzMinen = 50
 abstand = auflösung // raster
 
 pg.init()
 screen = pg.display.set_mode([auflösung, auflösung])
-cell_normal = pg.transform.scale(pg.image.load(
-    'ms_cell_normal.gif'), (abstand, abstand))
-cell_marked = pg.transform.scale(pg.image.load(
-    'ms_cell_marked.gif'), (abstand, abstand))
-cell_mine = pg.transform.scale(pg.image.load(
-    'ms_cell_mine.gif'), (abstand, abstand))
-cell_selected = []
+
+
+def ladeBild(dateiname):
+  return pg.transform.scale(pg.image.load(dateiname), (abstand, abstand))
+
+
+def gültig(y, x):
+  return y > -1 and y < raster and x > -1 and x < raster
+
+
+bild_normal = ladeBild('ms_cell_normal.gif')
+bild_markiert = ladeBild('ms_cell_marked.gif')
+bild_mine = ladeBild('ms_cell_mine.gif')
+bild_aufgedeckt = []
 for n in range(9):
-  cell_selected.append(pg.transform.scale(
-      pg.image.load(f'ms_cell_{n}.gif'), (abstand, abstand)))
+  bild_aufgedeckt.append(ladeBild(f'ms_cell_{n}.gif'))
 
 
 matrix = []
@@ -32,77 +37,83 @@ class Cell():
   zeile: int
   spalte: int
   mine: bool = False
-  selected: bool = False
-  flagged: bool = False
+  aufgedeckt: bool = False
+  markiert: bool = False
   anzMinenDrumrum = int = 0
 
   def show(self):
     pos = (self.spalte*abstand, self.zeile*abstand)
-    if self.selected:
+    if self.aufgedeckt:
       if self.mine:
-        screen.blit(cell_mine, pos)
+        screen.blit(bild_mine, pos)
       else:
-        screen.blit(cell_selected[self.anzMinenDrumrum], pos)
+        screen.blit(bild_aufgedeckt[self.anzMinenDrumrum], pos)
     else:
-      if self.flagged:
-        screen.blit(cell_marked, pos)
+      if self.markiert:
+        screen.blit(bild_markiert, pos)
       else:
-        screen.blit(cell_normal, pos)
+        screen.blit(bild_normal, pos)
 
   def anzMinenErmitteln(self):
     for pos in benachbarteFelder:
-      neueZeile = self.zeile + pos[0]
-      neueSpalte = self.spalte + pos[1]
-      if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
-        if matrix[neueZeile*raster+neueSpalte].mine:
-          self.anzMinenDrumrum += 1
+      neueZeile, neueSpalte = self.zeile + pos[0], self.spalte + pos[1]
+      if gültig(neueZeile, neueSpalte) and matrix[neueZeile*raster+neueSpalte].mine:
+        self.anzMinenDrumrum += 1
 
-def floodFill(zeile,spalte):
+
+def floodFill(zeile, spalte):
   for pos in benachbarteFelder:
     neueZeile = zeile + pos[0]
     neueSpalte = spalte + pos[1]
-    if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
+    if gültig(neueZeile, neueSpalte):
       cell = matrix[neueZeile*raster+neueSpalte]
-      if cell.anzMinenDrumrum == 0 and not cell.selected:
-        cell.selected = True
+      if cell.anzMinenDrumrum == 0 and not cell.aufgedeckt:
+        cell.aufgedeckt = True
         floodFill(neueZeile, neueSpalte)
       else:
-        cell.selected = True  
+        cell.aufgedeckt = True
 
 
+#Die Objekte werden je Feld angelegt und in der Matrix gespeichert
 for n in range(raster*raster):
   matrix.append(Cell(n // raster, n % raster))
 
+#Die Minen werden zufällig verteilt
 while anzMinen > 0:
-  x = rnd.randrange(raster*raster)
-  if not matrix[x].mine:
-    matrix[x].mine = True
+  cell = matrix[rnd.randrange(raster*raster)]
+  if not cell.mine:
+    cell.mine = True
     anzMinen -= 1
 
+#Die Anzahl der Minen in den benachbarten Feldern werden je Feld ermittelt
 for objekt in matrix:
-  objekt.anzMinenErmitteln()
+  if not objekt.mine:
+    objekt.anzMinenErmitteln()
 
+#Hauptschleife zum Bildschirmzeichnen und zur Auswertung der Ereignisse
 weitermachen = True
 while weitermachen:
+  #Events auswerten
   for event in pg.event.get():
+    #wenn Fenster geschlossen wird
     if event.type == pg.QUIT:
       weitermachen = False
+    #wenn Maustaste gedrückt wurde
     if event.type == pg.MOUSEBUTTONDOWN:
       mouseX, mouseY = pg.mouse.get_pos()
-      spalte = mouseX // abstand
-      zeile = mouseY // abstand
-      i = zeile*raster+spalte
-      cell = matrix[i]
+      cell = matrix[mouseY // abstand * raster + mouseX // abstand]
+      #rechte Maustaste
       if pg.mouse.get_pressed()[2]:
-        cell.flagged = not cell.flagged
+        cell.markiert = not cell.markiert
+      #linke Maustaste
       if pg.mouse.get_pressed()[0]:
-        cell.selected = True
+        cell.aufgedeckt = True
         if cell.anzMinenDrumrum == 0 and not cell.mine:
-          floodFill(zeile,spalte)
+          floodFill(mouseY // abstand, mouseX // abstand)
         if cell.mine:
           for objekt in matrix:
-            objekt.selected=True  
-          
+            objekt.aufgedeckt = True
+
   for objekt in matrix:
     objekt.show()
   pg.display.flip()
