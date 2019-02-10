27e0e4eaee5975aeb0b74e009086fe4a837bad5b
Gravitar64 <gravitar@web.de>
2019-02-10 15:56:11 +0100

    Version auf Youtube veröffentlicht

--- Teil_13_Doppelpendel.py
+++ Teil_13_Doppelpendel.py
@@ -1,11 +1,6 @@
+import pygame as pg
 from dataclasses import dataclass
 import math
-import pygame as pg
-
-PI = math.pi
-g = 1
-BREITE = 900
-HÖHE = 600
 
 
 @dataclass
@@ -14,73 +9,64 @@ class Pendel():
   θ: float
   L: int
   m: int
-  θ1 : float = 0
-  θ2 : float = 0
-  endPos : tuple = (0,0)
+  θ1: float = 0
+  θ2: float = 0
+  endPos: tuple = (0, 0)
 
   def endPosBerechnen(self):
-    x = int(self.anfPos[0] + self.L*math.sin(self.θ))
-    y = int(self.anfPos[1] + self.L*math.cos(self.θ))
-    self.endPos = (x,y)
+    x1, y1 = self.anfPos
+    x2 = int(x1 + self.L*math.sin(self.θ))
+    y2 = int(y1 + self.L*math.cos(self.θ))
+    self.endPos = (x2, y2)
+
+  def winkelAktualisieren(self, acc):
+    self.θ1 += acc
+    self.θ += self.θ1
+    self.endPosBerechnen()
 
   def show(self):
     pg.draw.line(screen, (255, 255, 255), self.anfPos, self.endPos, 3)
     pg.draw.circle(screen, (255, 0, 0), self.endPos, self.m)
 
-  def updateWinkel(self,acceleration):
-    self.θ2 = acceleration
-    self.θ1 += self.θ2
-    self.θ += self.θ1
-    self.endPosBerechnen()  
 
-
+pi = 3.14159265
+g = 1
+breite, höhe = 900, 600
 pg.init()
-#für das Pendel, wird nach jedem Frame wieder gelöscht
-screen = pg.display.set_mode([BREITE, HÖHE])
-#für die Linie, die der Position des 2ten Pendels folgt
-screen2 = pg.Surface([BREITE, HÖHE])
+screen = pg.display.set_mode([breite, höhe])
+screen2 = pg.Surface([breite,höhe])
 
-p1 = Pendel((BREITE//2, HÖHE//3), PI/2, HÖHE//3, 10)
+p1 = Pendel((breite//2, höhe//3), pi/2, höhe//3, 10)
 p1.endPosBerechnen()
-p2 = Pendel(p1.endPos, PI/2, HÖHE//3, 10)
+p2 = Pendel((p1.endPos), pi/2, höhe//4, 10)
 p2.endPosBerechnen()
 oldPos = p2.endPos
 
-# Hauptschleife zum Bildschirmzeichnen und zur Auswertung der Ereignisse
 clock = pg.time.Clock()
 weitermachen = True
 while weitermachen:
-  # Frames per second setzen
-  clock.tick(60)
-  screen.fill((0, 0, 0))
-  #Linie wird über BLIT auf den screen gestanzt
-  screen.blit(screen2, (0, 0))
-  # Events auswerten
   for event in pg.event.get():
-    # wenn Fenster geschlossen wird
     if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
       weitermachen = False
-  
-  #Quelle der Formeln = https://myphysicslab.com/pendulum/double-pendulum-en.html
-  acc1 = (-g*(2*p1.m+p2.m)*math.sin(p1.θ)-p2.m \
-    * g * math.sin(p1.θ - 2*p2.θ) \
-    - 2 * math.sin(p1.θ - p2.θ) * p2.m \
-    * (p2.θ1**2*p2.L + p1.θ1**2*p1.L*math.cos(p1.θ-p2.θ))) \
-    / (p1.L*(2*p1.m+p2.m - p2.m*math.cos(2*p1.θ-2*p2.θ)))
+  clock.tick(60)
+  screen.fill((0, 0, 0))
+  screen.blit(screen2,(0,0))
 
+  acc1 = (-g*(2*p1.m + p2.m)*math.sin(p1.θ)-p2.m \
+          *g*math.sin(p1.θ - 2*p2.θ) - 2*math.sin(p1.θ - p2.θ)*p2.m \
+          *(p2.θ1**2*p2.L+p1.θ1**2*p1.L*math.cos(p1.θ-p2.θ))) \
+          /(p1.L*(2*p1.m+p2.m-p2.m*math.cos(2*p1.θ-2*p2.θ)))
   acc2 = (2*math.sin(p1.θ-p2.θ)*(p1.θ1**2*p1.L*(p1.m+p2.m) \
-    + g * (p1.m+p2.m) * math.cos(p1.θ) \
-    + p2.θ1**2*p2.L*p2.m* math.cos(p1.θ - p2.θ))) \
-    / (p1.L*(2*p1.m+p2.m - p2.m*math.cos(2*p1.θ-2*p2.θ)))
+          +g*(p1.m+p2.m)*math.cos(p1.θ)+p2.θ1**2*p2.L*p2.m \
+          *math.cos(p1.θ-p2.θ))) \
+          /(p2.L*(2*p1.m+p2.m-p2.m*math.cos(2*p1.θ-2*p2.θ)))
 
-  p1.updateWinkel(acc1)
-  p2.updateWinkel(acc2)
+  p1.winkelAktualisieren(acc1)
   p2.anfPos = p1.endPos
+  p2.winkelAktualisieren(acc2)
 
   p1.show()
   p2.show()
-  pg.draw.line(screen2, (0, 150, 255), oldPos, p2.endPos, 2)
+  pg.draw.line(screen2,(0,255,0),oldPos, p2.endPos, 2)
   oldPos = p2.endPos
-
   pg.display.flip()
-pg.quit()
