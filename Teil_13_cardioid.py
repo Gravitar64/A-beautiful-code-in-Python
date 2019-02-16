fcaae055745d9aa1addbea24fca3c4cb5bc558e2
Gravitar64 <gravitar@web.de>
2019-02-16 15:39:52 +0100

    def i2Pos um die Position auf dem Kreis zu berechnen

--- Teil_13_cardioid.py
+++ Teil_13_cardioid.py
@@ -1,14 +1,24 @@
 import pygame as pg
 from math import *
 
+
+def i2Point(i):
+  x, y = center
+  # tau = 2* pi, +pi um das bild um 180° zu drehen
+  w = tau / total * i + pi
+  x1 = int(x + r * cos(w))
+  y1 = int(y + r * sin(w))
+  return (x1, y1)
+
+
 total = 200
 faktor = 0
-breite, höhe = 900, 900
+breite, höhe = 1150, 1150
 center = (breite//2, höhe//2)
 r = breite//2-16
 
 pg.init()
-screen = pg.display.set_mode([breite, höhe])
+screen = pg.display.set_mode((breite, höhe))
 screen2 = pg.Surface([breite, höhe])
 farbe = pg.Color(150, 150, 150)
 
@@ -16,11 +26,9 @@ points = []
 pg.draw.circle(screen2, (255, 255, 255), center, r, 2)
 x, y = center
 for i in range(total):
-  w = 2*pi / total * i
-  x1 = int(x + r*cos(w))
-  y1 = int(y + r*sin(w))
-  pg.draw.circle(screen2, (0, 150, 255), (x1, y1), 4,)
-  points.append((x1, y1))
+  point = i2Point(i)
+  pg.draw.circle(screen2, (0, 150, 255), point, 4,)
+  points.append(point)
 
 
 clock = pg.time.Clock()
@@ -33,15 +41,14 @@ while weitermachen:
     if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
       weitermachen = False
 
-  x, y = center
   for i, p in enumerate(points):
-    x1, y1 = p
     z = (i * faktor) % total
-    w = 2 * pi / total * z
-    x2 = int(x + r*cos(w))
-    y2 = int(y + r*sin(w))
-    farbe.hsva = (360/(2*pi)*w, 100, 100)
-    pg.draw.line(screen, farbe, (x1, y1), (x2, y2))
+    farbe.hsva = (360 / total * z, 100, 100)
+    pg.draw.line(screen, farbe, p, (i2Point(z)))
+
+  textsurface = pg.font.SysFont('cour', 82).render(
+      f'{faktor:6.1f}', False, (farbe))
+  screen.blit(textsurface, (3, 10))
 
   pg.display.flip()
 
