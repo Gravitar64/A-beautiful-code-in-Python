58d76e9afa0fac9ed3e4f0a0b3ca20a9c5b7ad30
Gravitar64 <gravitar@web.de>
2019-04-23 18:50:52 +0200

    Teil_15 Beispielaufgaben

--- Teil_15_Sudoku_Solver.py
+++ Teil_15_Sudoku_Solver.py
@@ -1,24 +1,25 @@
 import time
 
+
 def i2pos(i):
   """Nimmt eine Zahl zwischen 0 und 80 entgegen und liefert ein Tuple 
   mit (spalte, zeile und quadrant) zurück """
-  zeile =  i // 9
+  zeile = i // 9
   spalte = i % 9
   quadrant = zeile//3*3+spalte//3
-  return (spalte,zeile,quadrant)
+  return (spalte, zeile, quadrant)
+
 
 def print_board(nr):
   print(f'Position Nr. {nr}')
   for i in range(81):
     pos = i2pos(i)
-    print(board[pos],' ',end='')  
-    print () if (i+1) % 9 == 0 else None  
+    print(board[pos], ' ', end='')
+    print() if (i+1) % 9 == 0 else None
   print()
-  
 
 
-def change_board(pos,zahl,setzen):
+def change_board(pos, zahl, setzen):
   '''für (pos,zahl,setzen) wird auf der pos die 
   zahl gesetzt (setzen = True) oder gelöscht (setzen = False)'''
   spalte, zeile, quadrant = pos
@@ -28,11 +29,12 @@ def change_board(pos,zahl,setzen):
     quadrate[quadrant].remove(zahl)
     freie_stellen.remove(pos)
     board[pos] = zahl
-  else:  
+  else:
     spalten[spalte].add(zahl)
     zeilen[zeile].add(zahl)
     quadrate[quadrant].add(zahl)
     freie_stellen.add(pos)
+
 
 def bestes_Feld():
   '''liefert ein Tuple mit (Position, Liste der möglichen Einträge) von dem Feld zurück, 
@@ -43,12 +45,12 @@ def bestes_Feld():
     möglichkeiten = spalten[spalte] & zeilen[zeile] & quadrate[quadrant]
     länge = len(möglichkeiten)
     if länge == 1 or not möglichkeiten:
-      return (pos ,möglichkeiten)
+      return (pos, möglichkeiten)
     if länge < best:
       best = länge
       best_feld = (pos, möglichkeiten)
-  
-  return best_felld
+  return best_feld
+
 
 def solve():
   '''Löst das Sudoku rekursiv indem es in dem Feld mit der geringsten Anzahl an möglichen Einträgen
@@ -64,8 +66,8 @@ def solve():
     change_board(pos, zahl, False)
 
 
-#globale Variablen
-full_set = {1,2,3,4,5,6,7,8,9}
+# globale Variablen
+full_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 sudokus = []
 board = {}
 freie_stellen = set()
@@ -73,14 +75,13 @@ spalten = []
 zeilen = []
 quadrate = []
 
+
 def init(sudoku):
   '''generiert aus einem übergebenen String mit der Anfangs-Zahlenverteilung
   ein board-Dictionary mit Key = Tuple(spalte,zeile,quadrant) und Value = Zahl des Feldes
   und die Liste der möglichen Zahlen für jede Spalte, Zeile und jeden Quadranten als Set(). Zusätzlich
   wird ein Set der freien Stellen mit den Tuple für (spalte,zeile,quadrant) angelegt'''
-  for i, char in enumerate(sudoku):
-    board[i2pos(i)] = int(char)
-  
+
   spalten.clear()
   zeilen.clear()
   quadrate.clear()
@@ -89,26 +90,27 @@ def init(sudoku):
   for _ in range(9):
     spalten.append(full_set.copy())
     zeilen.append(full_set.copy())
-    quadrate.append(full_set.copy())     
+    quadrate.append(full_set.copy())
 
-  for i in range(81):
-    pos = i2pos(i)     
+  for i, char in enumerate(sudoku):
+    pos = i2pos(i)
     freie_stellen.add(pos)
-    if board[pos] == 0:
-      continue
-    change_board(pos,board[pos],True)
+    if char not in ('.', '0'):
+      board[pos] = int(char)
+      change_board(pos, int(char), True)
 
-with open ('Sudoku_2635_hard.txt') as f:
+
+with open('Teil_15_Sudoku_top100_hard.txt') as f:
   for nr, sudoku in enumerate(f):
     start = time.perf_counter()
     init(sudoku.rstrip())
     solve()
     zeit = (time.perf_counter() - start)*1_000
     sudokus.append((zeit, nr + 1))
-    
-    
-summe_zeit = sum([x for x,y in sudokus])
-sudokus.sort(reverse=True)    
+
+
+summe_zeit = sum([x for x, y in sudokus])
+sudokus.sort(reverse=True)
 print(f'{nr+1} Sudokus gelöst in {summe_zeit:.0f} Millisek. (ø {summe_zeit/(nr+1):.0f} ms)')
 for i in range(10):
   print(f'{sudokus[i][0]:5,.0f} Millisek. für Nr. {sudokus[i][1]:5d}')
