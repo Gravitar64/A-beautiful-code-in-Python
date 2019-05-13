c354bac26136c082ddd702570ee30b60f36fc3e9
Gravitar64 <gravitar@web.de>
2019-05-13 19:46:42 +0200

    Performanceoptimierung

--- Teil_16_Vier_gewinnt.py
+++ Teil_16_Vier_gewinnt.py
@@ -1,5 +1,4 @@
 # 4-gewinnt
-import random as rnd
 from collections import defaultdict
 import time
 
@@ -18,46 +17,42 @@ def posGültig(pos):
 
 
 def quadGültig(pos, vec):
-  pos = (pos[0]+vec[0]*3, pos[1]+vec[1]*3)
-  return posGültig(pos)
+  return posGültig((pos[0]+vec[0]*3, pos[1]+vec[1]*3))
 
 
 def quadPositionen(pos, vec):
-  positionen = [pos]
+  positionen = {pos}
   for i in range(3):
     pos = (pos[0]+vec[0], pos[1]+vec[1])
-    positionen.append(pos)
+    positionen.add(pos)
   return positionen
 
 
 def findeQuads():
-  quads = []
+  quads = {}
   zähler = 0
   for i in range(ZELLEN):
     pos = i2pos(i)
     for richtung in RICHTUNGEN:
-      if quadGültig(pos, richtung):
-        quads.append([True, True, 0, 0])
-        for position in quadPositionen(pos, richtung):
-          pos_zu_quadindex[position].append(zähler)
-        zähler += 1
+      if not quadGültig(pos, richtung): continue
+      quads[zähler] = [0, 0, quadPositionen(pos, richtung)]
+      for position in quadPositionen(pos, richtung):
+        pos_zu_quadindex[position].append(zähler)
+      zähler += 1
   return quads
 
 
 def i2pos(i):
-  spalte = i % SPALTEN
-  zeile = i // SPALTEN
-  return (spalte, zeile)
+  return (i % SPALTEN, i // SPALTEN)
 
 
-def finde_tiefste_stelle(spalte):
-  for zeile in range(ZEILEN):
-    if (spalte, zeile) in board:
-      return zeile-1
-  return zeile
+def lieferZeile(spalte):
+  for zeile in reversed(range(ZEILEN)):
+    if (spalte, zeile) not in board:
+      return zeile
 
 
-def liefer_gültige_spalten():
+def lieferSpalten():
   gültige_spalten = []
   for spalte in range(SPALTEN):
     gültige_spalten.append(spalte) if (spalte, 0) not in board else None
@@ -65,47 +60,36 @@ def liefer_gültige_spalten():
 
 
 def feld_ändern(player, pos, setzen):
+  win = False
   if setzen:
-    board[pos] = 1 if player else 2
+    board[pos] = 'O' if player else 'X'
   else:
     del board[pos]
 
   for index in pos_zu_quadindex[pos]:
     quad = quads[index]
-    anz_index = 2 if player else 3
-    if setzen and not quad[0] and not quad[1]:
-      continue
+    anz_index = 0 if player else 1
     if setzen:
       quad[anz_index] += 1
+      if quad[anz_index] == 4:
+        win = True
     else:
       quad[anz_index] -= 1
-
-  anz1 = quad[2]
-  anz2 = quad[3]
-
-  if anz1 > 0 and anz2 > 0:
-    quad[0] = quad[1] = False
-  if anz1 >= 0 and anz2 == 0:
-    quad[0] = True
-    quad[1] = False
-  if anz2 >= 0 and anz1 == 0:
-    quad[1] = True
-    quad[0] = False
-
+  return win
 
 def gültigeZüge():
   gültig = []
-  for spalte in liefer_gültige_spalten():
-    zeile = finde_tiefste_stelle(spalte)
+  for spalte in lieferSpalten():
+    zeile = lieferZeile(spalte)
     gültig.append((spalte, zeile))
   return gültig
 
 
-def print_board():
+def printBoard():
   for i in range(ZELLEN):
     print(board[i2pos(i)], end=' ') if i2pos(
-        i) in board else print('_', end=' ')
-    print() if (i+1) % SPALTEN == 0 else 0
+        i) in board else print('.', end=' ')
+    print() if (i+1) % SPALTEN == 0 else None
 
 
 def bewertung():
@@ -113,20 +97,14 @@ def bewertung():
   for pos in board:
     for index in pos_zu_quadindex[pos]:
       quad = quads[index]
-      if not quad[0] and not quad[1]:
+      if quad[0] > 0 and quad[1] > 0:
         continue
-      if quad[0]:
-        score += 1+quad[2]**2
-        if quad[2] == 4:
-          return 99999
-      if quad[1]:
-        score -= 1+quad[3]**2
-        if quad[3] == 4:
-          return -99999
+      score += 1+quad[0]**3
+      score -= 1+quad[1]**3
   return score
 
 
-def bewerteteZüge():
+def bewerteteZüge(player):
   zugliste = gültigeZüge()
   bewerteteZüge = []
   for pos in zugliste:
@@ -137,20 +115,16 @@ def bewerteteZüge():
   return bewerteteZüge
 
 
-def win():
-  for quad in quads:
-    if quad[2] == 4 or quad[3] == 4:
-      return True
-
-
-def alphabeta(depth, α, β, player):
-  if depth == 0 or win():
+def alphabeta(depth, α, β, player, win):
+  if win:
+    return -99999 if player else 99999
+  if depth == 0 or len(board) == ZELLEN:
     return bewertung()
   if player:
     value = -999999
     for child in gültigeZüge():
-      feld_ändern(player, child, True)
-      value = max(value, alphabeta(depth-1, α, β, False))
+      win = feld_ändern(player, child, True)
+      value = max(value, alphabeta(depth-1, α, β, False, win))
       feld_ändern(player, child, False)
       α = max(α, value)
       if α >= β:
@@ -159,36 +133,65 @@ def alphabeta(depth, α, β, player):
   else:
     value = 999999
     for child in gültigeZüge():
-      feld_ändern(player, child, True)
-      value = min(value, alphabeta(depth-1, α, β, True))
+      win = feld_ändern(player, child, True)
+      value = min(value, alphabeta(depth-1, α, β, True, win))
       feld_ändern(player, child, False)
       β = min(β, value)
       if α >= β:
         break  # α cut-off
     return value
 
+def löscheQuads():
+  to_del = []
+  for index, values in quads.items():
+    if values[0] == 0 or values[1] == 0: continue
+    to_del.append(index)
+    for position in values[2]:
+      pos_zu_quadindex[position].remove(index)
+  for index in to_del:
+    del quads[index]      
+
+def computer():
+  zugliste = sorted(bewerteteZüge(player), reverse=player)
+  for pos in zugliste:
+    win = feld_ändern(player, pos[1], True)
+    pos[0] = alphabeta(5, -999999, 999999, not player, win)
+    feld_ändern(player, pos[1], False)
+  zugliste.sort(reverse=player)
+  best_Zug = zugliste[0][1]
+  win=feld_ändern(player, best_Zug, True)
+  print(zugliste)
+  print(f'Spieler {1 if player else 2}: {best_Zug}')
+  return win
+
+def human():
+  spalte = int(input('Ihr Zug (0-6): '))
+  zeile = lieferZeile(spalte)
+  win = feld_ändern(player, (spalte, zeile), True)
+  return win
+
+def spielende(win):
+  if win:
+    print(f'Spieler {1 if player else 2} hat gewonnen)')
+    return True
+  if len(board) == ZELLEN:
+    print('UNENTSCHIEDEN!!!!')
+    return True
 
 quads = findeQuads()
 start = time.perf_counter()
+
 player = True
-
 while True:
-  zugliste = sorted(bewerteteZüge(), reverse=player)
-  for pos in zugliste:
-    feld_ändern(player, pos[1], True)
-    pos[0] = alphabeta(4, -999999, 999999, not player)
-    feld_ändern(player, pos[1], False)
-
-  zugliste.sort(reverse=player)
-  best_Zug = zugliste[0][1]
-  feld_ändern(player, best_Zug, True)
-  print(f'Mein Zug : {best_Zug}')
-  if win():
-    print(f'Spieler {1 if player else 2} hat gewonnen (Zug: {best_Zug})')
+  if player:
+    win = computer()
+  else:
+    win = computer()  
+  printBoard()
+  print('\n')
+  if spielende(win):
     break
-  print_board()
-  print('\n'*2)
-
+  löscheQuads()
   player = not player
 
 print(time.perf_counter()-start)
