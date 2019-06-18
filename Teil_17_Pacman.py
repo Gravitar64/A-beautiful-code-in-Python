ea5c907980742203d8861453f013c285e5f8a1bd
Gravitar64 <gravitar@web.de>
2019-06-18 20:48:50 +0200

    Ziel für Gheister hinterlegt

--- Teil_17_Pacman.py
+++ Teil_17_Pacman.py
@@ -4,83 +4,87 @@ import threading
 from Teil_17_Vector import Vector
 
 
-grid = [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
-        9,1,1,1,1,1,1,1,1,1,1,1,1,9,9,1,1,1,1,1,1,1,1,1,1,1,1,9,
-        9,1,9,9,9,9,1,9,9,9,9,9,1,9,9,1,9,9,9,9,9,1,9,9,9,9,1,9,
-        9,2,9,9,9,9,1,9,9,9,9,9,1,9,9,1,9,9,9,9,9,1,9,9,9,9,2,9,
-        9,1,9,9,9,9,1,9,9,9,9,9,1,9,9,1,9,9,9,9,9,1,9,9,9,9,1,9,
-        9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,
-        9,1,9,9,9,9,1,9,9,1,9,9,9,9,9,9,9,9,1,9,9,1,9,9,9,9,1,9,
-        9,1,9,9,9,9,1,9,9,1,9,9,9,9,9,9,9,9,1,9,9,1,9,9,9,9,1,9,
-        9,1,1,1,1,1,1,9,9,1,1,1,1,9,9,1,1,1,1,9,9,1,1,1,1,1,1,9,
-        9,9,9,9,9,9,1,9,9,9,9,9,0,9,9,0,9,9,9,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,9,9,9,0,9,9,0,9,9,9,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,0,0,0,0,0,0,0,0,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,9,9,9,7,7,9,9,9,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,9,0,0,0,0,0,0,9,0,9,9,1,9,9,9,9,9,9,
-        5,0,0,0,0,0,1,0,0,0,9,0,0,0,0,0,0,9,0,0,0,1,0,0,0,0,0,6,
-        9,9,9,9,9,9,1,9,9,0,9,0,0,0,0,0,0,9,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,9,9,9,9,9,9,9,9,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,0,0,0,0,0,0,0,0,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,9,9,9,9,9,9,9,9,0,9,9,1,9,9,9,9,9,9,
-        9,9,9,9,9,9,1,9,9,0,9,9,9,9,9,9,9,9,0,9,9,1,9,9,9,9,9,9,
-        9,1,1,1,1,1,1,1,1,1,1,1,1,9,9,1,1,1,1,1,1,1,1,1,1,1,1,9,
-        9,1,9,9,9,9,1,9,9,9,9,9,1,9,9,1,9,9,9,9,9,1,9,9,9,9,1,9,
-        9,1,9,9,9,9,1,9,9,9,9,9,1,9,9,1,9,9,9,9,9,1,9,9,9,9,1,9,
-        9,2,1,1,9,9,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,9,9,1,1,2,9,
-        9,9,9,1,9,9,1,9,9,1,9,9,9,9,9,9,9,9,1,9,9,1,9,9,1,9,9,9,
-        9,9,9,1,9,9,1,9,9,1,9,9,9,9,9,9,9,9,1,9,9,1,9,9,1,9,9,9,
-        9,1,1,1,1,1,1,9,9,1,1,1,1,9,9,1,1,1,1,9,9,1,1,1,1,1,1,9,
-        9,1,9,9,9,9,9,9,9,9,9,9,1,9,9,1,9,9,9,9,9,9,9,9,9,9,1,9,
-        9,1,9,9,9,9,9,9,9,9,9,9,1,9,9,1,9,9,9,9,9,9,9,9,9,9,1,9,
-        9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,
-        9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]
+grid = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+        9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 2, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 2, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 1, 1, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 0, 9, 9, 0, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 0, 9, 9, 0, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 9, 9, 7, 7, 9, 9, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 0, 0, 0, 0, 0, 0, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        5, 0, 0, 0, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 1, 0, 0, 0, 0, 0, 6,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 0, 0, 0, 0, 0, 0, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 1, 9, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 1, 9, 9, 9, 9, 9, 9,
+        9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 1, 9, 9, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 1, 9, 9, 9, 9, 1, 9,
+        9, 2, 1, 1, 9, 9, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 2, 9,
+        9, 9, 9, 1, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 1, 9, 9, 9,
+        9, 9, 9, 1, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 1, 9, 9, 9,
+        9, 1, 1, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 9, 9, 1, 1, 1, 1, 1, 1, 9,
+        9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9,
+        9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 9,
+        9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+        9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
 
-directions = {0:(1,0), 1:(-1,0), 2:(0,-1), 3:(0,1)}
-dir_invers = {v:k for (k,v) in directions.items()}
+directions = {0: (1, 0), 1: (-1, 0), 2: (0, -1), 3: (0, 1)}
+abbiegungen = {(1, 0): [(1, 0), (0, -1), (0, 1)],
+               (-1, 0): [(-1, 0), (0, -1), (0, 1)],
+               (0, -1): [(0, -1), (-1, 0), (1, 0)],
+               (0, 1): [(0, 1), (-1, 0), (1, 0)]}
+dir_invers = {v: k for (k, v) in directions.items()}
 grid_save = grid.copy()
 dots = {}
+
 
 class Dot:
   def __init__(self, pos, image):
     self.pos = pos
-    self.grid = pos2grid(self.pos)
-    self.i = grid2i(self.grid)
     self.sprite = makeSprite(image)
-    moveSprite(self.sprite,pos,centre=True)
+    moveSprite(self.sprite, pos, centre=True)
+
 
 class Actor:
-  def __init__(self,pos):
+  def __init__(self, pos):
     self.pos = pos
-    self.richtung = Vector(1,0)
+    self.richtung = Vector(1, 0)
     self.frame = 0
     self.dir = 0
     self.dir_buffer = None
     self.grid = pos2grid(self.pos)
     self.i = grid2i(self.grid)
-  
+
   def changeAnimationFrame(self):
     sprite, animations, je_richtung = self.sprites[self.modus]
     if je_richtung:
       self.frame = (self.frame+1) % animations + self.dir * animations
     else:
-      self.frame = (self.frame+1) % animations  
-    changeSpriteImage(sprite,self.frame)
+      self.frame = (self.frame+1) % animations
+    changeSpriteImage(sprite, self.frame)
 
   def show(self):
     showSprite(self.sprites[self.modus][0])
-  
-  def dirGültig(self,dir):
+
+  def dirGültig(self, dir):
     richtung = Vector(*directions[dir])
     sp, ze = richtung + self.grid
     i = ze * spalten + sp
     return grid[i] != 9
 
-  def inSync(self,pos):
+  def inSync(self, pos):
     pos2 = i2xy(xy2i(pos))
     return pos == pos2
-  
-  def changeDir(self,i):
+
+  def changeDir(self, i):
     self.dir = i
     self.richtung = Vector(*directions[i])
 
@@ -88,58 +92,57 @@ class Actor:
     sprite, _, _ = self.sprites[self.modus]
     hideSprite(sprite)
     self.modus = modus
-    self.frame = 0 
+    self.frame = 0
     sprite, _, _ = self.sprites[self.modus]
-    moveSprite(sprite,self.pos, centre= True)
+    moveSprite(sprite, self.pos, centre=True)
 
-    
 
 class Ghosts(Actor):
-  def __init__(self,tileset,pos):
-    Actor.__init__(self,pos)
-    self.sprites = {'jagd':[makeSprite(tileset,8),2,True],
-                    'flucht':[makeSprite("ghost_flucht.png",2),2,False],
-                    'blink':[makeSprite("ghost_blink.png",4),4,False],
-                    'die':[makeSprite("ghost_die.png",4),1, True]
+  def __init__(self, tileset, pos):
+    Actor.__init__(self, pos)
+    self.sprites = {'jagd': [makeSprite(tileset, 8), 2, True],
+                    'flucht': [makeSprite("ghost_flucht.png", 2), 2, False],
+                    'blink': [makeSprite("ghost_blink.png", 4), 4, False],
+                    'die': [makeSprite("ghost_die.png", 4), 1, True]
                     }
     self.modus = "jagd"
-  
+    self.target = Vector(14,14)
+
   def update(self):
     if self.inSync(self.pos):
-      if grid[self.i] in (5,6):
+      if grid[self.i] in (5, 6):
         self.i = self.i+27 if grid[self.i] == 5 else self.i-27
         self.pos = i2xy(self.i)
         self.pos += self.richtung
         self.grid = pos2grid(self.pos)
         return
-      while True:
-        dir = rnd.randrange(3)
-        vx, vy = self.richtung
-        if dir == 0:
-          vx, vy = vy, -vx
-        elif dir == 2:
-          vx, vy = -vy, vx  
-        dir = dir_invers[(vx, vy)]
-        if self.dirGültig(dir):
-          self.changeDir(dir)
-          break
+      mögliche_abbiegungen = []
+      self.target = pacman.grid
+      for r in abbiegungen[self.richtung.werte]:
+        if self.dirGültig(dir_invers[r]):
+          newPos = self.grid + Vector(*r)
+          abst = self.target.abstand(newPos)
+          mögliche_abbiegungen.append((abst,r))
+      if mögliche_abbiegungen:
+        r = sorted(mögliche_abbiegungen)[0][1]
+        dir = dir_invers[r]
+        self.changeDir(dir)
     self.pos += self.richtung
     self.grid = pos2grid(self.pos)
     self.i = grid2i(self.grid)
-    moveSprite(self.sprites[self.modus][0],self.pos,centre=True)
-    
-  
+    moveSprite(self.sprites[self.modus][0], self.pos, centre=True)
+
+
 class Pacman(Actor):
-  def __init__(self,pos):
-    Actor.__init__(self,pos)
-    self.sprites = {'run':[makeSprite("pacman_tileset2.png",12),3,True],
-                    'die':[makeSprite("pacman_die.png",12),12,False]}
+  def __init__(self, pos):
+    Actor.__init__(self, pos)
+    self.sprites = {'run': [makeSprite("pacman_tileset2.png", 12), 3, True],
+                    'die': [makeSprite("pacman_die.png", 12), 12, False]}
     self.modus = 'run'
-    
 
   def update(self):
     if self.inSync(self.pos):
-      if grid[self.i] in (5,6):
+      if grid[self.i] in (5, 6):
         self.i = self.i+27 if grid[self.i] == 5 else self.i-27
         self.pos = i2xy(self.i)
         self.pos += self.richtung
@@ -154,11 +157,11 @@ class Pacman(Actor):
     self.pos += self.richtung
     self.grid = pos2grid(self.pos)
     self.i = grid2i(self.grid)
-    moveSprite(self.sprites[self.modus][0],self.pos,centre=True)
-    
+    moveSprite(self.sprites[self.modus][0], self.pos, centre=True)
+
   def eatDot(self):
     global grid
-    if grid[self.i] in (1,2):
+    if grid[self.i] in (1, 2):
       if grid[self.i] == 2:
         changeGhostMode('flucht')
       grid[self.i] = 0
@@ -172,20 +175,19 @@ class Pacman(Actor):
       dotsAufbauen()
 
 
-
 def changeGhostMode(modus):
   for ghost in ghosts:
     if ghost.modus != 'die':
       ghost.changeMode(modus)
   if modus == 'flucht':
-    timer1 = threading.Timer(5.0, changeGhostMode, ('blink',)) 
+    timer1 = threading.Timer(5.0, changeGhostMode, ('blink',))
     timer2 = threading.Timer(8.0, changeGhostMode, ('jagd',))
     timer1.start()
     timer2.start()
-    
+
 
 def xy2i(pos):
-  x,y = pos
+  x, y = pos
   sp = round((x - raster_w / 2) / raster_w)
   ze = round((y - raster_h / 2) / raster_h)
   i = ze*spalten+sp
@@ -196,40 +198,44 @@ def i2xy(i):
   sp, ze = i % spalten, i // spalten
   x = sp * raster_w + raster_w // 2
   y = ze * raster_h + raster_h // 2
-  return Vector(x,y)
+  return Vector(x, y)
+
 
 def sync(pos):
   return i2xy(xy2i(pos))
+
 
 def pos2grid(pos):
   i = xy2i(pos)
   return Vector(i % spalten, i // spalten)
 
+
 def grid2i(pos):
   sp, ze = pos
-  return ze * spalten + sp  
+  return ze * spalten + sp
+
 
 def nextPacman():
-  pacman.pos = sync(Vector(321,420))
-  blinky.pos = sync(Vector(335,276))
-  pinky.pos = sync(Vector(300,348))
-  inky.pos = sync(Vector(348,348))
-  clyde.pos = sync(Vector(396,348))
+  pacman.pos = sync(Vector(321, 420))
+  blinky.pos = sync(Vector(335, 276))
+  pinky.pos = sync(Vector(300, 348))
+  inky.pos = sync(Vector(348, 348))
+  clyde.pos = sync(Vector(396, 348))
   for ghost in ghosts:
     sprite = ghost.sprites[ghost.modus][0]
     hideSprite(sprite)
     ghost.modus = 'jagd'
     sprite = ghost.sprites[ghost.modus][0]
-    moveSprite(sprite,ghost.pos,centre=True)
+    moveSprite(sprite, ghost.pos, centre=True)
   pacman.changeMode('run')
   pacman.changeDir(0)
   timer1 = threading.Timer(1.5, changeGameStatus, ('run',))
   timer1.start()
 
+
 def changeGameStatus(status):
   global game_status
   game_status = status
-
 
 
 setAutoUpdate(False)
@@ -240,16 +246,14 @@ raster_w = 24
 raster_h = 24
 zellen = spalten * zeilen
 
-screenSize(w,h)
+screenSize(w, h)
 setBackgroundImage("Teil_17_pacman.png")
-pacman = Pacman(sync(Vector(321,420)))
-blinky = Ghosts("blinky_tileset2.png",sync(Vector(348,276)))
-pinky = Ghosts("pinky_tileset2.png",sync(Vector(300,348)))
-inky = Ghosts("inky_tileset2.png", sync(Vector(348,348)))
-clyde = Ghosts("clyde_tileset2.png", sync(Vector(396,348)))
+pacman = Pacman(sync(Vector(321, 420)))
+blinky = Ghosts("blinky_tileset2.png", sync(Vector(348, 276)))
+pinky = Ghosts("pinky_tileset2.png", sync(Vector(300, 348)))
+inky = Ghosts("inky_tileset2.png", sync(Vector(348, 348)))
+clyde = Ghosts("clyde_tileset2.png", sync(Vector(396, 348)))
 ghosts = [blinky, pinky, inky, clyde]
-
-
 
 
 def dotsAufbauen():
@@ -257,10 +261,10 @@ def dotsAufbauen():
   dots = {}
   for i, zahl in enumerate(grid):
     pos = i2xy(i)
-    if zahl == 1: 
-      dots[i] = Dot(pos,"dot.png")
+    if zahl == 1:
+      dots[i] = Dot(pos, "dot.png")
     elif zahl == 2:
-      dots[i] = Dot(pos,"bit_dot.png")  
+      dots[i] = Dot(pos, "bit_dot.png")
 
 
 nextFrame = clock()
@@ -274,7 +278,7 @@ while True:
     pacman.changeAnimationFrame()
 
   fps = tick(120)
-  
+
   if game_status == "run":
     if keyPressed("right"):
       pacman.dir_buffer = 0
@@ -283,14 +287,14 @@ while True:
     elif keyPressed("up"):
       pacman.dir_buffer = 2
     elif keyPressed("down"):
-      pacman.dir_buffer = 3   
-  
+      pacman.dir_buffer = 3
+
     for dot in dots.values():
       showSprite(dot.sprite)
 
     pacman.eatDot()
     pacman.update()
-    
+
     for ghost in ghosts:
       ghost.update()
       if ghost.grid == pacman.grid:
@@ -302,8 +306,10 @@ while True:
 
         if ghost.modus in ("flucht", "blink"):
           ghost.changeMode('die')
-    
-  for ghost in ghosts: ghost.show()
-  pacman.show()  
+
+  for ghost in ghosts:
+    ghost.show()
+  pacman.show()
   updateDisplay()
-  if keyPressed("ESC"): break
+  if keyPressed("ESC"):
+    break
