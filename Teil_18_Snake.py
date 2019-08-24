eae7263c5f5e2ddbcb1a27e29b62871319ef3542
Gravitar64 <gravitar@web.de>
2019-08-24 19:21:01 +0200

    Highscore ergänzt

--- Teil_18_Snake.py
+++ Teil_18_Snake.py
@@ -7,6 +7,8 @@ snake = [(BREITE//2, HÖHE//2)]
 richt_x, richt_y = 1, 0
 bonus_x, bonus_y = 300, 300
 richtungen = {pg.K_DOWN:(0,1), pg.K_UP: (0,-1), pg.K_LEFT: (-1,0), pg.K_RIGHT: (1,0)}
+with open('Teil_18_Highscore.txt','r') as f:
+  high = int(f.readline())
 
 pg.init()
 screen = pg.display.set_mode([BREITE, HÖHE])
@@ -42,10 +44,22 @@ while weitermachen:
     pg.draw.rect(screen,(0,255,0),(x,y,GRÖßE, GRÖßE))
   pg.draw.rect(screen,(255,0,0),(bonus_x,bonus_y,GRÖßE, GRÖßE))
 
-  textfläche = pg.font.SysFont('impact', 28).render(f'Score: {score}', False, (255,255,255))
+  farbe = (255,255,0) if score > high else (128,128,128)
+  textfläche = pg.font.SysFont('impact', 24).render(f'Score {score}', False, farbe)
   screen.blit(textfläche, (BREITE - textfläche.get_width(), 5))
 
+  textfläche = pg.font.SysFont('impact', 28).render(f'High {high}', False, farbe)
+  screen.blit(textfläche, (BREITE // 2 - textfläche.get_width() // 2, 5))
+
+  textfläche = pg.font.SysFont('impact', 24).render(f'fps {tempo}', False, farbe)
+  screen.blit(textfläche, (5, 5))  
+  
   pg.display.flip()    
 
 pg.quit()      
-print(score)
+if score > high:
+  print(f'New High Score {score} (old {high})')
+  with open('teil_18_highscore.txt','w') as f:
+    f.write(str(score))
+else:
+  print(score)    
