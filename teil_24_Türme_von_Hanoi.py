02fe39c172b2b59b6b9caaa13d47680316c83bc3
Gravitar64 <gravitar@web.de>
2019-10-12 11:08:06 +0200

    Erstes Commit

---
+++ teil_24_Türme_von_Hanoi.py
@@ -0,0 +1,7 @@
+def hanoi(anzahl, von, temp, ziel):
+  if anzahl > 0:
+    hanoi(anzahl - 1, von, ziel, temp)
+    print(f'Scheibe {anzahl} von {von} nach {ziel}')
+    hanoi(anzahl - 1, temp, von, ziel)
+
+hanoi(3, 'A', 'B', 'C')    
