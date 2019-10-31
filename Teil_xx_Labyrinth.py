2126245263b33c0c1700c03083926cdda4bcf202
Gravitar64 <gravitar@web.de>
2019-10-31 18:34:11 +0100

    von s,z auf pos umgestiegen

--- Teil_xx_Labyrinth.py
+++ Teil_xx_Labyrinth.py
@@ -4,32 +4,39 @@ import sys
 
 sys.setrecursionlimit(4000)
 
+BREITE, HÖHE = 1001, 1001
+ZELLEN_GRÖßE = 100
+FPS = 5
+SPALTEN, ZEILEN = BREITE // ZELLEN_GRÖßE, HÖHE // ZELLEN_GRÖßE
+ANZ_ZELLEN = SPALTEN * ZEILEN
+richt = {'l': (-1, 0), 'r': (1, 0), 'o': (0, -1), 'u': (0, 1)}
 
 class Zelle:
-  def __init__(self, spalte, zeile):
-    self.spalte = spalte
-    self.zeile = zeile
+  def __init__(self, pos):
+    self.pos = pos
     self.besucht = False
     self.wände = {'l': True, 'r': True, 'o': True, 'u': True}
 
   def show(self):
-    x, y = self.spalte * ZELLEN_GRÖßE, self.zeile * ZELLEN_GRÖßE
+    s, z = self.pos
+    x, y = s * ZELLEN_GRÖßE, z * ZELLEN_GRÖßE
     for key, val in self.wände.items():
       if not val:
         continue
       if key == 'l':
-        pg.draw.line(screen, f_linie, (x, y), (x, y+ZELLEN_GRÖßE))
+        pg.draw.line(screen, f_linie, (x, y), (x, y+ZELLEN_GRÖßE),2)
       if key == 'r':
         pg.draw.line(screen, f_linie, (x+ZELLEN_GRÖßE, y),
-                     (x+ZELLEN_GRÖßE, y+ZELLEN_GRÖßE))
+                     (x+ZELLEN_GRÖßE, y+ZELLEN_GRÖßE),2)
       if key == 'o':
-        pg.draw.line(screen, f_linie, (x, y), (x+ZELLEN_GRÖßE, y))
+        pg.draw.line(screen, f_linie, (x, y), (x+ZELLEN_GRÖßE, y),2)
       if key == 'u':
         pg.draw.line(screen, f_linie, (x, y+ZELLEN_GRÖßE),
-                     (x+ZELLEN_GRÖßE, y+ZELLEN_GRÖßE))
+                     (x+ZELLEN_GRÖßE, y+ZELLEN_GRÖßE),2)
 
   def mark(self):
-    x, y = self.spalte * ZELLEN_GRÖßE, self.zeile * ZELLEN_GRÖßE
+    s,z = self.pos
+    x, y = s * ZELLEN_GRÖßE, z * ZELLEN_GRÖßE
     x, y = x + ZELLEN_GRÖßE/2, y + ZELLEN_GRÖßE / 2
     pg.draw.circle(screen, f_markierung, (x, y), ZELLEN_GRÖßE // 3)
 
@@ -43,15 +50,16 @@ def ereignis_quit():
 def i2sz(i):
   return i % SPALTEN, i // SPALTEN
 
+def add_pos(pos1,pos2):
+  return pos1[0]+pos2[0], pos1[1]+pos2[1]
+
 
 def nachbarn(zelle):
   nachb = []
   for key, val in richt.items():
-    ds, dz = val
-    s, z = zelle.spalte, zelle.zeile
-    s1, z1 = s + ds, z + dz
-    if (s1, z1) in raster:
-      nachb.append((key, s1, z1))
+   pos = add_pos(zelle.pos, val)
+   if pos in raster:
+      nachb.append((key, pos))
   rnd.shuffle(nachb)
   return nachb
 
@@ -67,11 +75,11 @@ def labyrinth_erstellen(zelle, richtung):
   nachb = nachbarn(zelle)
   if not nachb:
     return
-  for richt, s, z in nachb:
-    if raster[(s, z)].besucht:
+  for richt, pos in nachb:
+    if raster[pos].besucht:
       continue
     zelle.wände[richt] = False
-    labyrinth_erstellen(raster[(s, z)], richt_invers(richt))
+    labyrinth_erstellen(raster[pos], richt_invers(richt))
 
 
 def mögliche_richtungen(zelle):
@@ -85,30 +93,19 @@ def mögliche_richtungen(zelle):
 
 
 weg = []
-
-
-def finde_weg(zelle, found):
+def finde_weg(zelle):
   zelle.besucht = True
-  s, z = zelle.spalte, zelle.zeile
-  if (s, z) == (SPALTEN-1, ZEILEN-1):
-    weg.append((raster[(s, z)]))
+  if zelle.pos == (SPALTEN-1, ZEILEN-1):
+    weg.append((raster[zelle.pos]))
     return True
   for r in mögliche_richtungen(zelle):
-    ds, dz = richt[r]
-    s1, z1 = s+ds, z+dz
-    if (s1, z1) in raster and not raster[(s1, z1)].besucht:
-      found = finde_weg(raster[(s1, z1)], found)
-    if found:
-      weg.append((raster[(s, z)]))
+    pos = add_pos(zelle.pos, richt[r])
+    if pos not in raster or raster[pos].besucht: continue
+    if finde_weg(raster[pos]):
+      weg.append((raster[zelle.pos]))
       return True
-  return found
+  
 
-
-BREITE, HÖHE = 1001, 1001
-ZELLEN_GRÖßE = 20
-SPALTEN, ZEILEN = BREITE // ZELLEN_GRÖßE, HÖHE // ZELLEN_GRÖßE
-ANZ_ZELLEN = SPALTEN * ZEILEN
-richt = {'l': (-1, 0), 'r': (1, 0), 'o': (0, -1), 'u': (0, 1)}
 
 
 pg.init()
@@ -121,8 +118,8 @@ f_markierung = pg.Color('gold2')
 rnd.seed()
 raster = {}
 for i in range(ANZ_ZELLEN):
-  s, z = i2sz(i)
-  raster[(s, z)] = Zelle(s, z)
+  pos = i2sz(i)
+  raster[pos] = Zelle(pos)
 
 screen.fill(f_hintergrund)
 labyrinth_erstellen(raster[(0, 0)], 'l')
@@ -133,16 +130,16 @@ while not ereignis_quit():
   pg.display.flip()
 
 for i in range(ANZ_ZELLEN):
-  s, z = i2sz(i)
-  raster[(s, z)].besucht = False
+  pos = i2sz(i)
+  raster[pos].besucht = False
 
-finde_weg(raster[(0, 0)], False)
+finde_weg(raster[(0, 0)])
 weg.reverse()
 
 clock = pg.time.Clock()
 i = 0
 while not ereignis_quit():
-  clock.tick(5)
+  clock.tick(FPS)
   weg[i].mark()
   i = min(i+1, len(weg)-1)
   pg.display.flip()
