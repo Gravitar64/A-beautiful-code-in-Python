e8f2c29d26ff50fef0e82bb85219423597969ed9
Gravitar64 <gravitar@web.de>
2019-11-11 15:40:56 +0100

    Klasse eleminiert

--- Teil_23_Labyrinth.py
+++ Teil_23_Labyrinth.py
@@ -4,21 +4,22 @@ import sys
 
 sys.setrecursionlimit(4000)
 
-
 BREITE = HÖHE = 1000
-SPALTEN = ZEILEN = 30
-ZELLE_BH = BREITE // SPALTEN
+SPALTEN = ZEILEN = 90
+ZE_BH = BREITE // SPALTEN
 
 ri_inv = {'l': 'r', 'r': 'l', 'o': 'u', 'u': 'o'}
-ri_sz = {'l': (-1, 0), 'r': (1, 0), 'o': (0, -1), 'u': (0, 1)}
-ri_xy = {'l': [(0, 0), (0, ZELLE_BH)],
-         'r': [(ZELLE_BH, 0), (ZELLE_BH, ZELLE_BH)],
-         'o': [(0, 0), (ZELLE_BH, 0)],
-         'u': [(0, ZELLE_BH), (ZELLE_BH, ZELLE_BH)]}
+delta_nachbarn = {'l': (-ZE_BH, 0), 'r': (ZE_BH, 0), 'o': (0, -ZE_BH), 'u': (0, ZE_BH)}
+delta_linien = {
+    'l': [(0, 0), (0, ZE_BH)],
+    'r': [(ZE_BH, 0), (ZE_BH, ZE_BH)],
+    'o': [(0, 0), (ZE_BH, 0)],
+    'u': [(0, ZE_BH), (ZE_BH, ZE_BH)]
+}
 
 
 def add_pos(pos1, pos2):
-  return pos1[0]+pos2[0], pos1[1]+pos2[1]
+  return pos1[0] + pos2[0], pos1[1] + pos2[1]
 
 
 def pg_quit():
@@ -27,19 +28,32 @@ def pg_quit():
       (ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE):
       return True
 
+def zelle_zeichnen(pos, wände):
+  for wand in wände:
+    delta_von, delta_bis = delta_linien[wand]
+    von = add_pos(pos, delta_von)
+    bis = add_pos(pos, delta_bis)
+    pg.draw.line(screen, farbe_wand, von, bis, 2)
 
-class Zelle:
-  def __init__(self, pos):
-    self.posXY = pos[0]*ZELLE_BH, pos[1] * ZELLE_BH
-    self.besucht = False
-    self.wände = {c for c in 'lrou'}
+def nachbarn(pos):
+  nachb = []
+  for richtung, delta in delta_nachbarn.items():
+    neue_pos = add_pos(pos, delta)
+    if neue_pos not in raster:
+      continue
+    nachb.append((richtung, neue_pos))
+  rnd.shuffle(nachb)
+  return nachb
 
-  def anzeigen(self):
-    for wand in self.wände:
-      delta_von, delta_bis = ri_xy[wand]
-      von = add_pos(self.posXY, delta_von)
-      bis = add_pos(self.posXY, delta_bis)
-      pg.draw.line(screen, farbe_wand, von, bis, 2)
+def labyrinth_erstellen(pos_aktuell, richtung_von):
+  besucht.add(pos_aktuell)
+  raster[pos_aktuell].remove(richtung_von)
+  nachb = nachbarn(pos_aktuell)
+  for richtung_nach, pos_neu in nachb:
+    if pos_neu in besucht:
+      continue
+    raster[pos_aktuell].remove(richtung_nach)
+    labyrinth_erstellen(pos_neu, ri_inv[richtung_nach])
 
 
 pg.init()
@@ -49,37 +63,16 @@ farbe_hintergrund = pg.Color('Black')
 
 raster = {}
 for i in range(SPALTEN * ZEILEN):
-  posSZ = i % SPALTEN, i // SPALTEN
-  raster[posSZ] = Zelle(posSZ)
+  pos = i % SPALTEN * ZE_BH, i // SPALTEN * ZE_BH
+  raster[pos] = {c for c in 'lrou'}
 
-
-def nachbarn(posSZ):
-  nachb = []
-  for richtung, deltaSZ in ri_sz.items():
-    neue_posSZ = add_pos(posSZ, deltaSZ)
-    if neue_posSZ not in raster: continue
-    nachb.append((richtung, neue_posSZ))
-  rnd.shuffle(nachb)
-  return nachb
-
-
-def labyrinth_erstellen(posSZ_aktuell, richtung_von):
-  akt_zelle = raster[posSZ_aktuell]
-  akt_zelle.besucht = True
-  akt_zelle.wände.remove(richtung_von)
-  nachb = nachbarn(posSZ_aktuell)
-  for richtung_nach, posSZ_neu in nachb:
-    if raster[posSZ_neu].besucht: continue
-    akt_zelle.wände.remove(richtung_nach)
-    labyrinth_erstellen(posSZ_neu, ri_inv[richtung_nach])
-
-
+besucht = set()
 labyrinth_erstellen((0, 0), 'l')
 
 while not pg_quit():
   screen.fill(farbe_hintergrund)
-  for zelle in raster.values():
-    zelle.anzeigen()
+  for pos, wände in raster.items():
+    zelle_zeichnen(pos, wände)
   pg.display.flip()
 
 pg.quit()
