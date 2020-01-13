6d311df4d8f50a8752f5496afa7d5e4dcef0e766
Gravitar64 <gravitar@web.de>
2020-01-13 22:04:07 +0100

    youtube version 2

--- Teil_26_Conways_Game_of_Life.py
+++ Teil_26_Conways_Game_of_Life.py
@@ -1,39 +1,37 @@
-import pygame as pg 
-import random as rnd
-from collections import Counter 
-
-def zellGröße(spielfeld, breite, höhe):
-  Xs, Ys = zip(*spielfeld)
-  minx, maxx, miny, maxy = min(Xs), max(Xs), min(Ys), max(Ys)
-  spalten, zeilen = maxx-minx + 1, maxy-miny + 1
-  return breite / spalten, höhe / zeilen
+from collections import Counter
+import pygame as pg
+import random as rnd 
 
 def nachbarn(pos):
-  for x1, y1 in [(-1,-1), (0,-1), (1,-1),
-                 (-1,0),          (1,0),
-                 (-1,1),  (0,1),  (1,1)]:
-    yield pos[0]+x1, pos[1] + y1               
+  for dx, dy in [(-1,-1), (0,-1), (1,-1),
+                 (-1, 0),         (1, 0),
+                 (-1, 1), (0, 1), (1, 1)]:
+    yield pos[0] + dx, pos[1] + dy
 
+def nächsteGeneration(spielfeld):
+  nachb = Counter([pos for zelle in spielfeld for pos in nachbarn(zelle)]) 
+  return {pos for pos,anz in nachb.items() if anz == 3 or (anz==2 and pos in spielfeld)}
 
-def neueGeneration(spielfeld):
-  anzNachbarn =  Counter([p for pos in spielfeld for p in nachbarn(pos)])
-  spielfeld = {pos for pos, anz in anzNachbarn.items() if anz == 3 or (anz == 2 and pos in spielfeld)}
-  return spielfeld 
+def größeErmitteln(spielfeld, breite, höhe):
+  Xs, Ys = zip(*spielfeld)
+  minX, maxX, minY, maxY = min(Xs), max(Xs), min(Ys), max(Ys)
+  spalten, zeilen = maxX - minX + 1, maxY - minY + 1
+  return breite / spalten, höhe/ zeilen, -minX, -minY                   
 
-
-spielfeld = {(rnd.randrange(200), rnd.randrange(200)) for _ in range(6000)}
+spielfeld = {(rnd.randrange(200), rnd.randrange(200)) for i in range(6000)}
 
 pg.init()
 sc_b = sc_h = 1000
 screen = pg.display.set_mode([sc_b, sc_h])
-zb, zh = zellGröße(spielfeld, sc_b, sc_h)
 
 
 while True:
   screen.fill((0,0,0))
-  spielfeld = neueGeneration(spielfeld)
-  for zelle in spielfeld:
-    pg.draw.rect(screen, (0,0,255), (zelle[0]*zb, zelle[1]*zh, zb, zh))
+  spielfeld = nächsteGeneration(spielfeld)
+  zb, zh, offsX, offsY = größeErmitteln(spielfeld, sc_b, sc_h)
+  for x,y in spielfeld:
+    pg.draw.rect(screen,(0,0,255), ((x+offsX)*zb,(y+offsY)*zh, zb, zh))
   pg.display.flip()
+
 pg.quit()
 
