36b2bc8888bb255123ff9b4b2e210998efa72958
Gravitar64 <gravitar@web.de>
2020-06-20 17:44:06 +0200

    youtube-Version

--- Teil_31_Mastermind lösen.py
+++ Teil_31_Mastermind lösen.py
@@ -1,20 +1,22 @@
 from itertools import product
-import random as rnd
+import random as rnd 
 
-lösungsmenge = list(product(range(6), repeat=4))
+lösungsmenge = list(product(range(7), repeat = 4))
 GEHEIM = rnd.choice(lösungsmenge)
+versuch = ''
 
-
-def treffer(v, g):
-  farbe_position = sum(a == b for a, b in zip(v, g))
+def treffer(v,g):
+  farbe_position = sum(a==b for a,b in zip(v,g))
   farbe = sum(min(v.count(a), g.count(a)) for a in set(g)) - farbe_position
   return '+'*farbe_position + 'o'*farbe
 
-
-anz, versuch = 0, []
+anz = 0
 while versuch != GEHEIM:
   anz += 1
   versuch = rnd.choice(lösungsmenge)
   erg = treffer(versuch, GEHEIM)
-  print(f'{anz:2}. Lösungsmenge = {len(lösungsmenge):5} Elemente, {versuch}, {erg} ')
-  lösungsmenge = [l for l in lösungsmenge if treffer(l, versuch) == erg]
+  print(f'Versuch Nr. {anz} noch {len(lösungsmenge):4} {versuch} {erg}')
+  lösungsmenge = [denkbare_lösung for denkbare_lösung in lösungsmenge if treffer(versuch, denkbare_lösung) == erg]
+
+
+print(f'Lösung in {anz} Versuchen gefunden!')
