15d2cf1e367cc6c8b41b09c4d57a3d349873cfe6
Gravitar64 <gravitar@web.de>
2020-07-02 14:37:32 +0200

    kommentare überarbeitet

--- pygame_grundgerüst.py
+++ pygame_grundgerüst.py
@@ -1,9 +1,11 @@
 #pygame ab Version 2.0 wird benötigt
 #Installation im Terminal mit --> pip install pygame==2.0.0.dev10 
-import pygame as pg
+
 #Vektor-Modul aus meiner YouTube-Reihe wird benötigt
 #kann hier über dem Browser kopiert werden 
 # https://github.com/Gravitar64/A-beautiful-code-in-Python/blob/master/Teil_25_Vektor.py
+
+import pygame as pg
 from Teil_25_Vektor import Vec, pol2cart
 
 pg.init()
