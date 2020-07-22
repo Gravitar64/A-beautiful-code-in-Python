8322208befb73fbc05f0f29a406ab48fd6799f24
Gravitar64 <gravitar@web.de>
2020-07-22 20:56:24 +0200

    Fehler bei Straight Flush korrigiert (karten5[:1])

--- Teil_34_Poker_Best_Hand.py
+++ Teil_34_Poker_Best_Hand.py
@@ -1,6 +1,7 @@
 import random as rnd
 from itertools import combinations
-from collections import defaultdict 
+from collections import defaultdict
+
 
 class Karte(object):
   def __init__(self, wert, farbe) -> None:
@@ -8,16 +9,17 @@ class Karte(object):
     self.farbe = farbe
 
   def __repr__(self):
-    karten_namen = {10:'T', 11:'J', 12:'Q', 13:'K', 14:'A'}
+    karten_namen = {10: 'T', 11: 'J', 12: 'Q', 13: 'K', 14: 'A'}
     name = str(self.wert) if self.wert < 10 else karten_namen[self.wert]
-    return name + self.farbe  
+    return name + self.farbe
+
 
 class Kartendeck(object):
   def __init__(self):
     self.karten = self._neu_gemischt()
 
   def _neu_gemischt(self):
-    karten = [Karte(w,f) for f in '♤♡♧♢' for w in range(2,15)]
+    karten = [Karte(w, f) for f in '♤♡♧♢' for w in range(2, 15)]
     rnd.shuffle(karten)
     return karten
 
@@ -27,15 +29,15 @@ class Kartendeck(object):
 
 def beste_5_aus_7_ermitteln(pocket, board):
   karten7 = pocket + board
-  karten7 = sorted(karten7, key=lambda k:k.wert, reverse = True)
+  karten7 = sorted(karten7, key=lambda k: k.wert, reverse=True)
   bester_rang = -1
-  for karten5 in combinations(karten7,5):
-    rang,karten = bewerte5(karten5)
+  for karten5 in combinations(karten7, 5):
+    rang, karten = bewerte5(karten5)
     if rang > bester_rang:
       bester_rang = rang
       beste_karten = karten
-  return bester_rang, beste_karten    
-  
+  return bester_rang, beste_karten
+
 
 def bewerte5(karten5):
   wert2karten = defaultdict(list)
@@ -44,50 +46,48 @@ def bewerte5(karten5):
     wert2karten[k.wert].append(k)
   for v in wert2karten.values():
     anz2karten[len(v)] += v
-  werte = {k.wert for k in karten5}  
+  werte = {k.wert for k in karten5}
   straight = (len(werte) == 5 and max(werte) - min(werte) == 4) or \
-             werte == {5,4,3,2,14}
+      werte == {5, 4, 3, 2, 14}
   farben = {k.farbe for k in karten5}
-  flush = len(farben) == 1            
-  
-  #Royal Flush
+  flush = len(farben) == 1
+
+  # Royal Flush
   if straight and flush and max(werte) == 14:
     return 9, karten5
-  #Straight Flush
+  # Straight Flush
   if straight and flush:
-    if werte == {5,4,3,2,14}:
-      karten5 = karten5[1:]+karten[:1]
-    return 8, karten5  
-  #Four of a Kind
+    if werte == {5, 4, 3, 2, 14}:
+      karten5 = karten5[1:]+karten5[:1]
+    return 8, karten5
+  # Four of a Kind
   if 4 in anz2karten:
-    return 7, anz2karten[4] + anz2karten[1]  
-  #Full House
+    return 7, anz2karten[4] + anz2karten[1]
+  # Full House
   if 3 in anz2karten and 2 in anz2karten:
-    return 6, anz2karten[3] + anz2karten[2]  
-  #Flush
+    return 6, anz2karten[3] + anz2karten[2]
+  # Flush
   if flush:
     return 5, karten5
-  #Straight
+  # Straight
   if straight:
-    if werte == {5,4,3,2,14}:
+    if werte == {5, 4, 3, 2, 14}:
       karten5 = karten5[1:]+karten[:1]
     return 4, karten5
-  #Tree of a Kind
+  # Tree of a Kind
   if 3 in anz2karten:
-    return 3, anz2karten[3] + anz2karten[1]  
-  #Two Pair
+    return 3, anz2karten[3] + anz2karten[1]
+  # Two Pair
   if 2 in anz2karten and len(anz2karten[2]) == 4:
-    return 2, anz2karten[2] + anz2karten[1]  
-  #One Pair
+    return 2, anz2karten[2] + anz2karten[1]
+  # One Pair
   if 2 in anz2karten:
-    return 1, anz2karten[2] + anz2karten[1]  
-  #High Card
+    return 1, anz2karten[2] + anz2karten[1]
+  # High Card
   return 0, karten5
+
 
 deck = Kartendeck()
 pocket = deck.gib(2)
 board = deck.gib(5)
-rang, karten = beste_5_aus_7_ermitteln(pocket,board)
-
-print(pocket, board)
-print(rang,karten)
+rang, karten = beste_5_aus_7_ermitteln(pocket, board)
