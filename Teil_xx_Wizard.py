7ceb7c7f99c3c763eb8edc7d0f2baf3e6960d373
Gravitar64 <gravitar@web.de>
2020-08-29 22:09:46 +0200

    Funktion ermittle_Stich_gewinner eingefügt

--- Teil_xx_Wizard.py
+++ Teil_xx_Wizard.py
@@ -18,6 +18,21 @@ def ermittle_mögliche_karte(karten):
     else:
       return rnd.choice(karten)
 
+def ermittle_stich_gewinner(stich):
+  if all([k[0] == 14 for n,k in stich]): return stich[0][0]
+  höchste_karte = höchste_trumpf_karte = -1
+  for name, (wert, farbe) in stich:
+    if wert == 14: continue
+    if wert == 15: return name
+    if farbe == trumpf and wert > höchste_trumpf_karte:
+      höchste_trumpf_karte = wert
+      stich_gewinner = name  
+    if wert > höchste_karte and farbe == aktuelle_farbe and höchste_trumpf_karte == -1:
+      höchste_karte = wert
+      stich_gewinner = name
+  return stich_gewinner    
+
+
 def input2karten(input_string):
   return [(int(k[:-1]), k[-1]) for k in input_string.split()]
 
@@ -29,7 +44,7 @@ while True:
   trumpf = trumpf_karte[0][1]
   wer_startet = input('Wer startet: ')
   spieler_stich = {name:0 for name in spieler_namen}
-  simulationsläufe = 1
+  simulationsläufe = 10_000
   DECK = [(wert, farbe) for wert in range(1,16) for farbe in 'rgby' if (wert,farbe) not in (ich_karten + trumpf_karte)]
   
   for _ in range(simulationsläufe):
@@ -40,22 +55,15 @@ while True:
     stich_gewinner = wer_startet
     spieler_namen.rotate(-spieler_namen.index(stich_gewinner))
     while spieler_karten[spieler_namen[-1]]:
-      höchste_karte = höchste_trumpf_karte = aktuelle_farbe = -1
-      for spieler in spieler_namen:
-        wert,farbe = ermittle_mögliche_karte(spieler_karten[spieler])
-        spieler_karten[spieler].remove((wert,farbe))
-        if wert == 14: continue
+      aktuelle_farbe = -1
+      stich = []
+      for name in spieler_namen:
+        wert, farbe = ermittle_mögliche_karte(spieler_karten[name])
+        stich.append((name, (wert, farbe)))
+        spieler_karten[name].remove((wert, farbe))
         if wert < 14 and aktuelle_farbe == -1: aktuelle_farbe = farbe
-        if höchste_karte < 15 and wert == 15:
-          höchste_karte = höchste_trumpf_karte = 15
-          stich_gewinner = spieler
-        elif farbe == trumpf and wert > höchste_trumpf_karte:
-          höchste_trumpf_karte = wert
-          stich_gewinner = spieler  
-        elif wert > höchste_karte and farbe == aktuelle_farbe and höchste_trumpf_karte == -1:
-          höchste_karte = wert
-          stich_gewinner = spieler
+      stich_gewinner = ermittle_stich_gewinner(stich)
       spieler_stich[stich_gewinner] += 1
-      
       spieler_namen.rotate(-spieler_namen.index(stich_gewinner))
+  
   print(f'{ich_spieler} mit durchschnittlich {spieler_stich[ich_spieler]/simulationsläufe:.2f}')
