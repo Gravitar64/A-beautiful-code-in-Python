18bb66f5a9aa792c78428009987718a5ef6a6a79
Gravitar64 <gravitar@web.de>
2020-08-31 20:17:55 +0200

    Regelungen Narr, Zauberer erste Karte

--- Teil_xx_Wizard.py
+++ Teil_xx_Wizard.py
@@ -6,7 +6,7 @@ SIMULATIONEN = 10_000
 
 def ermittle_mögliche_karte(karten):
   farbe_bedienen = [k for k in karten if k[1] == aktuelle_farbe and k[0] < 14]
-  if aktuelle_farbe == -1 or len(karten) == 1 or not farbe_bedienen:
+  if not farbe_bedienen: 
     return rnd.choice(karten)
   return rnd.choice(farbe_bedienen + [k for k in karten if k[0] > 13])
 
@@ -17,8 +17,8 @@ def ermittle_stich_gewinner(stich, höchste_karte=-1):
   for name, (wert, farbe) in stich:
     if wert == 14: continue
     if wert == 15: return name
-    if farbe == trumpf: wert += 20
-    if wert > höchste_karte and farbe in (aktuelle_farbe, trumpf):
+    if farbe == t_farbe: wert += 20
+    if wert > höchste_karte and farbe in (aktuelle_farbe, t_farbe):
       höchste_karte = wert
       stich_gewinner = name
   return stich_gewinner
@@ -34,8 +34,12 @@ while True:
   ich_karten = input2karten(
       input('Eigene Karten (1-13, Narr=14, Zauberer=15, ygbr=Farben): '))
   trumpf_karte = input2karten(input('Trumpf Karte: '))
-  trumpf = trumpf_karte[0][1]
+  t_wert, t_farbe = trumpf_karte[0]
   wer_startet = input('Wer startet: ')
+  if t_wert == 15:
+    t_farbe = input(f'{wer_startet} bestimmt den Trumpf (rgby): ')
+  if t_wert == 14:
+    t_farbe = None   
   DECK = [(wert, farbe) for wert in range(1, 16)
           for farbe in 'rgby' if (wert, farbe) not in (ich_karten + trumpf_karte)]
   anz_stiche_spieler = {name: 0 for name in spieler_namen}
@@ -51,9 +55,9 @@ while True:
       aktuelle_farbe, stich = -1, []
       for name in spieler_namen:
         wert, farbe = ermittle_mögliche_karte(spieler_karten[name])
-        if wert < 14 and aktuelle_farbe == -1: aktuelle_farbe = farbe
-        spieler_karten[name].remove((wert, farbe))
         stich.append((name, (wert, farbe)))
+        if aktuelle_farbe == -1 and wert < 14 and stich[0][1][0] != 15: aktuelle_farbe = farbe
+        spieler_karten[name].remove((wert, farbe))
       stich_gewinner = ermittle_stich_gewinner(stich)
       anz_stiche_spieler[stich_gewinner] += 1
       spieler_namen.rotate(-spieler_namen.index(stich_gewinner))
