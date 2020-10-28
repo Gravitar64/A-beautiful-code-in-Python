9daab44cb4f1d3e5bcf04535086c85438e610774
Gravitar64 <gravitar@web.de>
2020-10-28 14:28:54 +0100

    formatiert

--- Teil_xx_Flappy_Bird.py
+++ Teil_xx_Flappy_Bird.py
@@ -1,7 +1,5 @@
-import pygame as pg
-import random as rnd
-import requests
-import io
+import pygame as pg, random as rnd
+import requests, io
 
 
 def lade_bild(url):
@@ -30,28 +28,25 @@ def generiere_röhre():
 
 
 def zeichne_Text(text, pos):
-  textsurface = pg.font.SysFont('impact', 40).render(
-      text, False, (255, 255, 255))
+  textsurface = pg.font.SysFont('impact', 40).render(text, False, (255, 255, 255))
   screen.blit(textsurface, pos)
-
-
-URL = 'https://raw.githubusercontent.com/samuelcust/flappy-bird-assets/master/'
-b_basis = lade_bild(URL+'sprites/base.png')
-b_hintergrund = lade_bild(URL+'sprites/background-day.png')
-b_röhre_unten = lade_bild(URL+'sprites/pipe-green.png')
-b_röhre_oben = pg.transform.flip(b_röhre_unten, False, True)
-b_ende = lade_bild(URL+'sprites/gameover.png')
-vögel = []
-vögel.append(lade_bild(URL+'sprites/redbird-downflap.png'))
-vögel.append(lade_bild(URL+'sprites/redbird-midflap.png'))
-vögel.append(lade_bild(URL+'sprites/redbird-upflap.png'))
-s_flug = lade_sound(URL+'audio/wing.wav')
-s_punkt = lade_sound(URL+'audio/point.wav')
-s_wand = lade_sound(URL+'audio/hit.wav')
 
 pg.init()
 BREITE, HÖHE = 576, 1024
 screen = pg.display.set_mode((BREITE, HÖHE))
+
+URL           = 'https://raw.githubusercontent.com/samuelcust/flappy-bird-assets/master/'
+b_basis       = lade_bild(URL+'sprites/base.png')
+b_hintergrund = lade_bild(URL+'sprites/background-day.png')
+b_röhre_unten = lade_bild(URL+'sprites/pipe-green.png')
+b_röhre_oben  = pg.transform.flip(b_röhre_unten, False, True)
+b_ende        = lade_bild(URL+'sprites/gameover.png')
+b_vögel       = [lade_bild(URL+'sprites/redbird-downflap.png'), 
+                 lade_bild(URL+'sprites/redbird-midflap.png'), 
+                 lade_bild(URL+'sprites/redbird-upflap.png')]
+s_flug        = lade_sound(URL+'audio/wing.wav')
+s_punkt       = lade_sound(URL+'audio/point.wav')
+s_wand        = lade_sound(URL+'audio/hit.wav')
 
 score = highscore = basis_x = hintergrund_x = 0
 vogel_y, vogel_grav, vogel_geschw, vogel_anim = HÖHE//2, 1, 0, 0
@@ -76,12 +71,13 @@ while weitermachen:
       vogel_anim = (vogel_anim + 1) % 3
     if ereignis.type == pg.MOUSEBUTTONDOWN and pg.mouse.get_pressed()[0]:
       vogel_geschw = -15
+      s_flug.play()
       if spielende:
         spielende = False
         vogel_y = HÖHE//2
         röhren.clear()
         score = 0
-
+  
   screen.fill((0, 0, 0))
   hintergrund_x = scrolling(b_hintergrund, hintergrund_x, 0, 1, True)
   if not spielende:
@@ -93,13 +89,15 @@ while weitermachen:
         s_punkt.play()
       if rect.x < -100:
         röhren.remove(rect)
+    
     vogel_geschw += vogel_grav
     vogel_y += vogel_geschw
-    screen.blit(vögel[vogel_anim], (100, vogel_y))
-    vogel_rect = vögel[vogel_anim].get_rect(topleft=(100, vogel_y))
+    screen.blit(b_vögel[vogel_anim], (100, vogel_y))
+    vogel_rect = b_vögel[vogel_anim].get_rect(topleft=(100, vogel_y))
+    
     if vogel_rect.collidelist(röhren) > -1 or \
-       vogel_rect.bottom > 900 or \
-       vogel_rect.top < 0:
+        vogel_rect.bottom > 900 or \
+        vogel_rect.top < 0:
       s_wand.play()
       spielende = True
   else:
