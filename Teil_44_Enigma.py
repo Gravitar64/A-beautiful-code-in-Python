a71a1782ad37de801085e703a48b12d3a831b207
Gravitar64 <gravitar@web.de>
2020-11-19 17:12:27 +0100

    youtube

--- Teil_44_Enigma.py
+++ Teil_44_Enigma.py
@@ -1,83 +1,103 @@
 from collections import deque
-import string
 
-walzenDict = dict(I    = deque('EKMFLGDQVZNTOWYHXUSPAIBRCJ'),
-                  II   = deque('AJDKSIRUXBLHWTMCQGZNPYFVOE'),
-                  III  = deque('BDFHJLCPRTXVZNYEIWGAKMUSQO'),
-                  IV   = deque('ESOVPZJAYQUIRHXLNFTGKDCMWB'),
-                  V    = deque('VZBRGITYUPSDNHLXAWMJQOFECK'),
-                  VI   = deque('JPGVOUMFYQBENHZRDKASXLICTW'),
-                  VII  = deque('NZJHGRCXMYSWBOUFAIVLPEKQDT'),
-                  VIII = deque('FKQHTLXOCBJSPDZRAMEWNIUYGV'))
-walzenNamen = walzenDict.keys()
+def str2num(zeichenkette):
+  return [ord(c)-65 for c in zeichenkette]
 
-reflDict = dict(B = deque('YRUHQSLDPXNGOKMIEBFZCWVJAT'),
-                A = deque('EJMZALYXVBWFCRQUONTSPIKHGD'),
-                C = deque('FVPJIAOYEDRZXWGCTKUQSBNMHL'))        
-reflNamen = reflDict.keys()
+walzen_r = ['EKMFLGDQVZNTOWYHXUSPAIBRCJ',  # I 
+            'AJDKSIRUXBLHWTMCQGZNPYFVOE',  # II 
+            'BDFHJLCPRTXVZNYEIWGAKMUSQO',  # III 
+            'ESOVPZJAYQUIRHXLNFTGKDCMWB',  # IV 
+            'VZBRGITYUPSDNHLXAWMJQOFECK',  # V 
+            'JPGVOUMFYQBENHZRDKASXLICTW',  # VI 
+            'NZJHGRCXMYSWBOUFAIVLPEKQDT',  # VII 
+            'FKQHTLXOCBJSPDZRAMEWNIUYGV']  # VIII
+walzen_r = [deque(str2num(zeile)) for zeile in walzen_r]
+walzen_l = deque(range(26))             
 
-kerbenDict = dict(I='Q', II='E', III='V', IV='J', V='Z', VI='ZM', VII='ZM', VIII='ZM')
-alphabet_num = deque(string.ascii_uppercase)          
+UKWs = ['EJMZALYXVBWFCRQUONTSPIKHGD',  # UKW A 
+        'YRUHQSLDPXNGOKMIEBFZCWVJAT',  # UKW B 
+        'FVPJIAOYEDRZXWGCTKUQSBNMHL']  # UKW C 
+UKWs = [str2num(zeile) for zeile in UKWs]             
 
-def enigma_setup(refl, walzen, walzen_pos, ring_pos, steckerbrett):
-  enigma_reflektor = reflDict[refl]
-  enigma_verdr_r, enigma_verdr_l = [], []
-  for name in walzen.split():
-    enigma_verdr_r.append(walzenDict[name].copy())
-    enigma_verdr_l.append(alphabet_num.copy())
-  enigma_pos = [x for x in walzen_pos]
-  enigma_ring = [int(x)-1 for x in ring_pos.split()]
-  enigma_kerben = [kerbenDict[name] for name in walzen.split()]
-  enigma_stecker = {}
-  for paar in steckerbrett.split():
-    enigma_stecker[paar[0]] = paar[1]
-    enigma_stecker[paar[1]] = paar[0]
-  for i in range(len(enigma_verdr_r)):
-    offset = -ord(walzen_pos[i])-65+enigma_ring[i]
-    enigma_verdr_r[i].rotate(offset)
-    enigma_verdr_l[i].rotate(offset)
-  return enigma_reflektor, enigma_verdr_r, enigma_verdr_l, enigma_pos, enigma_kerben, enigma_stecker
+kerbenKat = "Q E V J Z ZM ZM ZM"
+kerbenKat = [str2num(zeile) for zeile in kerbenKat.split()]
+            
+        
+class Walze():
+  def __init__(self, nr, w_pos, r_pos):
+    self.w_pos = w_pos
+    self.r_pos = r_pos
+    self.verdr_r = walzen_r[nr].copy()
+    self.verdr_l = walzen_l.copy()
+    self.kerben = kerbenKat[nr]
+    self.setup()
 
-def click(nr,v_r, v_l, positionen):
-  v_r[nr].rotate(-1)
-  v_l[nr].rotate(-1)
-  positionen[nr] = chr((ord(positionen[nr]) + 1) % 65)
+  def setup(self):
+    offset = self.r_pos-self.w_pos
+    self.verdr_l.rotate(offset)  
+    self.verdr_r.rotate(offset)
+    self.kerben = [(k-self.r_pos) % 26 for k in self.kerben]
+
+  def click(self):
+    self.verdr_l.rotate(-1)  
+    self.verdr_r.rotate(-1)
+
+  def schaltung(self):
+    return self.verdr_l[0] in self.kerben  
+
+class Enigma():
+  def __init__(self):
+    self.walzen = []
+    self.ukw = []
+    self.steckerbr = {}
+
+  def setup(self, nr_ukw, nr_walzen, w_pos, r_pos, paare_steckerbr):
+    for i,nr in enumerate(nr_walzen):
+      wpos = ord(w_pos[i])-65
+      rpos = r_pos[i]-1
+      self.walzen.append(Walze(nr-1, wpos, rpos))
+    self.ukw = UKWs[nr_ukw-1]
+    for a,b in paare_steckerbr.split():
+      self.steckerbr[ord(a)-65] = ord(b)-65
+      self.steckerbr[ord(b)-65] = ord(a)-65
+
+  def rotiere(self):
+    links, mitte, rechts = self.walzen
+    if mitte.schaltung():
+      mitte.click()
+      links.click()
+    elif rechts.schaltung():
+      mitte.click()
+    rechts.click()
+
+def umwandeln(e, text):
+  u_text = ""
+  text = text.upper()
+  for c in text:
+    c = ord(c)-65
+    if c <0 or c> 26: continue
+    e.rotiere()
+    c = e.steckerbr.get(c,c)
+    for w in reversed(e.walzen):
+      c = w.verdr_r[c]
+      c = w.verdr_l.index(c)
+    c = e.ukw[c]
+    for w in e.walzen:
+      c = w.verdr_l[c]
+      c = w.verdr_r.index(c)
+    c = e.steckerbr.get(c,c)
+    u_text += chr(c+65)
+  return u_text  
 
 
-def rotiere(v_r, v_l, positionen, kerben):
-  if positionen[1] == kerben[1]:
-    click(1, v_r, v_l, positionen)
-    click(0, v_r, v_l, positionen)
-  elif positionen[2] == kerben[2]:
-    click(1, v_r, v_l, positionen)
-  click(2, v_r, v_l, positionen)
+enigma = Enigma()
+enigma.setup(2, [2,4,5], "BLA", [2,21,12], "AV BS CG DL FU HZ IN KM OW RX")
+
+print('##### ENIGMA SIMULATOR #####')
+u_text = umwandeln(enigma, "EDPUD NRGYS ZRCXN UYTPO MRMBO FKTBZ REZKM LXLVE FGUEY SIOZV EQMIK UBPMM YLKLT TDEIS MDICA GYKUA CTCDO MOHWX MUUIA UBSTS LRNBZ SZWNR FXWFY SSXJZ VIJHI DISHP RKLKA YUPAD TXQSP INQMA TLPIF SVKDA SCTAC DPBOP VHJK-")
+u_text = u_text.replace('X', ' ')
+u_text = u_text.replace('Q', 'CH')
+
+print(f'Umgewandelter Text: {u_text}')
 
 
-def encode(text, v_r, v_l, p, k, refl, steckbr, crib=''):
-  ciphertext = ""
-  text=text.upper()
-  for n,c in enumerate(text):
-    if c not in string.ascii_uppercase: continue
-    rotiere(v_r, v_l, p, k)
-    if c in steckbr:
-      c = steckbr[ord(c)-65]
-    for i in reversed(range(3)):
-      c = v_r[i][ord(c)-65]
-      c = chr(v_l[i].index(c)+65)
-    c = refl[ord(c)-65]
-    for i in range(3):
-      c = v_l[i][ord(c)-65]
-      c = chr(v_r[i].index(c)+65)
-    if c in steckbr:
-      c =steckbr[ord(c)-65]
-    ciphertext += c
-    if crib:
-      if c != crib[n]: return ''
-  return ciphertext
-
-
-if __name__ == "__main__":
-  refl, v_r, v_l, p, k, steckbr = enigma_setup("B", "III II I", "CAT", "01 01 01","") 
-  print("  ##### Enigma Simlator #####\n")
-  text = input("Texteingabe: \n")
-  print(f'Umgewandelter Text: \n{encode(text, v_r, v_l, p, k, refl, steckbr)}')
