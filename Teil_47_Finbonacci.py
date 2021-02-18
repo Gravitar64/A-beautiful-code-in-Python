555a6af3935d861e4298c87b3ac6548d9592937e
Gravitar64 <gravitar@web.de>
2021-02-18 10:11:43 +0100

    Sequenz + Erläuterungen Rekursiv

--- Teil_47_Finbonacci.py
+++ Teil_47_Finbonacci.py
@@ -1,12 +1,19 @@
 import turtle
 
 
-def fib(n):
+def fib(n,tiefe=0):
   if n < 2: return n
-  return fib(n-1) + fib(n-2)
+  print(' '*tiefe, n)
+  a = fib(n-1, tiefe +1)
+  print(' '*tiefe + f'fib({n-1}) = ',a)
+  b = fib(n-2, tiefe +1)
+  print(' '*tiefe + f'fib({n-2}) = ',b)
+  return a + b
+  
 
 
 def draw_quadrat(size):
+  
   for _ in range(4):
     x.forward(size)
     x.right(90)
@@ -16,9 +23,16 @@ def draw_quadrat(size):
 
 
 def fiboPlot(n):
+  fibseq = ''
+  t1 = turtle.Turtle()
+  t1.penup()
+  t1.setposition(-600,400)
+  t1.pendown()
   x.pensize(3)
   x.pencolor("grey")
   for i in range(n):
+    fibseq += str(fib(i))+', '
+    t1.write(fibseq,font=('Arial',48,'normal'))
     draw_quadrat(fib(i)*SCALE)
 
   x.penup()
@@ -30,6 +44,7 @@ def fiboPlot(n):
   for i in range(n):
     x.circle(-SCALE*fib(i), 90)
 
+print(fib(10))
 
 START, SCALE = (-400, -200), 7
 
