9589ad782715832c926854563f17b7bca64402cd
Gravitar64 <gravitar@web.de>
2021-03-05 11:35:36 +0100

    Vector-Modul entfernt, Kommentare angepasst

--- pygame_grundgerüst.py
+++ pygame_grundgerüst.py
@@ -1,23 +1,23 @@
 #pygame ab Version 2.0 wird benötigt
-#Installation im Terminal mit --> pip install pygame==2.0.0.dev10 
-
-#Vektor-Modul aus meiner YouTube-Reihe wird benötigt
-#kann hier über dem Browser kopiert werden 
-# https://github.com/Gravitar64/A-beautiful-code-in-Python/blob/master/Teil_25_Vektor.py
+#Installation im Terminal mit 
+#   --> pip install pygame (windows) 
+#   --> pip3 install pygame (mac)
+#   --> sudo apt-get install python3-pygame (Linux Debian/Ubuntu/Mint)
 
 import pygame as pg
-from Teil_25_Vektor import Vec, pol2cart
+
 
 pg.init()
-auflösung = Vec(1000,1000)
-screen = pg.display.set_mode(auflösung)
-zentrum = auflösung / 2
+BREITE, HÖHE = 1000,1000
+FPS = 40
+zentrum = (BREITE / 2, HÖHE / 2)
+screen = pg.display.set_mode((BREITE, HÖHE))
 
 weitermachen = True
 clock = pg.time.Clock()
 
 while weitermachen:
-  clock.tick(40)
+  clock.tick(FPS)
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT:
       weitermachen = False
