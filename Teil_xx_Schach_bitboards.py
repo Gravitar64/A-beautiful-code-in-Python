e6dd2cb976114454d336634685b5e8a5b2386cda
Gravitar64 <gravitar@web.de>
2021-03-13 15:43:46 +0100

    zugausführung inkl. updates bitboards fehlerfrei

--- Teil_xx_Schach_bitboards.py
+++ Teil_xx_Schach_bitboards.py
@@ -57,37 +57,47 @@ def zuggenerator(weiss, position):
   pseudo = pseudo_züge(weiss, position)
   for von, zus, fig in pseudo:
     for zu in [i for i, char in enumerate(bin(zus)[:1:-1]) if char == '1']:
-      capture = ziehe(von,zu)
+      capture = ziehe(weiss, von,zu)
       if not imSchach(weiss):
         züge.append((fig, von, zu))
-      else:
-        print(f'SCHACH durch {fig} {i2A8(von)} - {i2A8(zu)}')
-      ziehe_rückgängig(von,zu,capture)
+      ziehe_rückgängig(weiss, von,zu,capture)
   return züge
 
 
-def ziehe(von, zu, capture=None):
-  mask = SQUARE_BB[von] | SQUARE_BB[zu]
-  pieces_bb[position[von]] ^= mask
-  if zu in position: 
-    pieces_bb[position[zu]] &= ~mask
-    capture = position[zu]
+def ziehe(weiss, von, zu):
+  global occupied_bb
+  capture = position.get(zu,None)
+  vonzu = SQUARE_BB[von] ^ SQUARE_BB[zu]
+  pieces_bb[position[von]] ^= vonzu
+  all_pieces_bb[weiss] ^= vonzu
+  if capture:
+    pieces_bb[capture] ^= SQUARE_BB[zu]
+    all_pieces_bb[not weiss] ^= SQUARE_BB[zu]
+    occupied_bb ^= SQUARE_BB[von]
+  else:
+    occupied_bb ^= vonzu
+  
   position[zu] = position[von]
   del position[von]
   return capture
   
 
 
-def ziehe_rückgängig(von, zu, capture):
-  mask = SQUARE_BB[von] | SQUARE_BB[zu]
-  pieces_bb[position[zu]] ^= mask
-  if von in position: 
-    pieces_bb[position[von]] &= ~mask
-  position[von] = position[zu]
+def ziehe_rückgängig(weiss, von, zu, capture):
+  global occupied_bb
+  vonzu = SQUARE_BB[von] ^ SQUARE_BB[zu]
+  pieces_bb[position[zu]] ^= vonzu
+  all_pieces_bb[weiss] ^= vonzu
   if capture:
-    position[zu] = capture
+    pieces_bb[capture] ^= SQUARE_BB[zu]
+    all_pieces_bb[not weiss] ^= SQUARE_BB[zu]
+    occupied_bb ^= SQUARE_BB[von]
   else:
-    del position[zu]
+    occupied_bb ^= vonzu
+  
+  position[von], position[zu] = position[zu], capture
+  if not capture: 
+    del position[zu]  
 
 def imSchach(weiss):
   i = ONLY1POS[pieces_bb['K' if weiss else 'k']]
@@ -107,7 +117,8 @@ def imSchach(weiss):
 
 NON_SLIDING_PIECES = {'K', 'k', 'p', 'P', 'n', 'N'}
 #fen = chess.get_random_daily_puzzle().json['fen']
-fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+#fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+fen = 'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w  KQkq - 0 1'
 weiss, position, kingspos = fen2pos(fen)
 
 move_bb = bitboards.create_move_bitboards()
@@ -116,13 +127,40 @@ blockerboards = bitboards.gen_blockerboards(move_bb)
 sliding_move_bb = bitboards.gen_sliding_move_bb(blockerboards)
 SQUARE_BB = bitboards.gen_square_bb()
 ONLY1POS = {mask:i for i,mask in enumerate(SQUARE_BB)}
-
+occ1 = occupied_bb
+all_copy = all_pieces_bb.copy()
 
 start = pfc()
 for i in range(10_000):
   züge = zuggenerator(weiss, position)
 print(pfc()-start)
 
-# züge = zuggenerator(weiss, position)
+if occupied_bb != occ1: 
+  b1 = bin(occ1)[:1:-1]
+  b2 = bin(occupied_bb)[:1:-1]
+  for i in range(64):
+    if b1[i] != b2[i]:
+      fig = position[i]
+      print(fig, i2A8(i))
+      print(bitboards.pretty(pieces_bb[fig]))
+      print(bitboards.pretty(all_pieces_bb[weiss]))
+for p1,p2 in zip(all_copy, all_pieces_bb):
+  if p1 == p2: continue
+  print(bitboards.pretty(p1))
+  print(bitboards.pretty(p2))
+
+
+
+#züge = zuggenerator(weiss, position)
 # for fig, von, zu in züge:
 #   print(f'{fig} {i2A8(von)} - {i2A8(zu)}')
+
+# for pos, fig in position.items():     
+#   print(bitboards.pretty(pieces_bb[fig]))
+#   print(fig)
+
+# print(bitboards.pretty(all_pieces_bb[True]))
+# print(bitboards.pretty(all_pieces_bb[False]))
+# print(bitboards.pretty(occupied_bb))
+
+
