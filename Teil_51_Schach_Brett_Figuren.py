d2e67d9e20f33f97df1df1f2737042a69fdf41ba
Gravitar64 <gravitar@web.de>
2021-03-20 14:16:01 +0100

    youTube

---
+++ Teil_51_Schach_Brett_Figuren.py
@@ -0,0 +1,95 @@
+import pygame as pg
+import chessdotcom as chess
+import Teil_51_Schach_Zuggenerator as zuggen
+
+
+def sz2xy(sz):
+  return sz[0]*FELD, sz[1]*FELD
+
+def xy2sz(xy):
+  return xy[0]//FELD, xy[1]//FELD
+
+def zeichneBrett(BRETT):
+  for sz, feld in BRETT.items():
+    farbe = '#DFBF93' if feld else '#C5844E'
+    pg.draw.rect(screen, farbe, (*sz2xy(sz), FELD, FELD))
+
+def fen2position(fen):
+  position, s, z = {}, 0, 0
+  figurenstellung, zugrecht, rochaderechte, enpassant, zug50, zugnr = fen.split()
+  for char in figurenstellung:
+    if char.isalpha():
+      position[(s,z)] = char
+      s += 1
+    elif char.isnumeric():
+      s += int(char)
+    else:
+      s,z = 0, z+1
+  return position, zugrecht
+
+def ladeFiguren():
+  bilder = {}
+  fig2datei = dict(r='br', n='bn', b='bb', q='bq', k='bk', p='bp',
+                   R='wr', N='wn', B='wb', Q='wq', K='wk', P='wp')
+  for fig, datei in fig2datei.items():
+    bild = pg.image.load(f'Teil_49_Figuren/{datei}.png')
+    bilder[fig] = pg.transform.smoothscale(bild, (FELD, FELD))
+  return bilder  
+
+def zeichneFiguren(p):
+  for sz, fig in p.items():
+    screen.blit(FIGUREN[fig], sz2xy(sz))
+
+
+
+
+pg.init()
+BREITE, HÖHE = 800,800
+FELD = BREITE // 8
+FPS = 40
+screen = pg.display.set_mode((BREITE, HÖHE))
+FIGUREN = ladeFiguren()
+#fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+#fen = chess.get_random_daily_puzzle().json['fen']
+fen = '1q2r1k1/P4ppp/3n4/4P3/8/2N3b1/1PPP1PPP/R5K1 w - - 0 1'
+position,zugrecht = fen2position(fen)
+print(zugrecht)
+weiss = zugrecht == 'w'
+züge = zuggen.zugGenerator(weiss, position)
+for zug in züge:
+  print(zug)
+
+weitermachen = True
+clock = pg.time.Clock()
+drag = None
+
+while weitermachen:
+  clock.tick(FPS)
+  for ereignis in pg.event.get():
+    if ereignis.type == pg.QUIT:
+      weitermachen = False
+    elif ereignis.type == pg.MOUSEBUTTONDOWN and not drag:
+      von = xy2sz(pg.mouse.get_pos())
+      if von in position:
+        fig = position[von]
+        drag = FIGUREN[fig]
+        del position[von]
+    elif ereignis.type == pg.MOUSEBUTTONUP and drag:
+      zu = xy2sz(pg.mouse.get_pos())
+      position[zu] = fig
+      drag = None
+      
+           
+  screen.fill((0,0,0))
+  zeichneBrett(zuggen.BRETT)
+  zeichneFiguren(position)
+  if drag:
+    rect = drag.get_rect(center=pg.mouse.get_pos())
+    screen.blit(drag, rect)
+
+  
+
+  #und hier wird das im Hintergrund gezeichnete Bild angezeigt
+  pg.display.flip()
+
+pg.quit()
---
+++ Teil_51_Schach_Zuggenerator.py
@@ -0,0 +1,67 @@
+_M_HV = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+_M_DI = [(-1, -1), (1, -1), (-1, 1), (1, 1)]
+
+_MOVES = {'r': [7, *_M_HV],
+          'b': [7, *_M_DI],
+          'k': [1, *_M_HV, *_M_DI],
+          'q': [7, *_M_HV, *_M_DI],
+          'n': [1, (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (-1, 2), (1, 2)],
+          'p': [2, (0, 1)],
+          'P': [2, (0, -1)],
+          'pc': [1, (-1, 1), (1, 1)],
+          'Pc': [1, (-1, -1), (1, -1)]}
+
+_GRUNDLINIE = [1,6]          
+
+BRETT = {(s,z): s % 2 == z % 2 for s in range(8) for z in range(8)}          
+
+def zugGenerator(weiss, position):
+  züge = []
+  pseudo = _pseudoZugGenerator(weiss, position)
+  #return züge
+  return pseudo
+
+def _pseudoZugGenerator(weiss, position):
+  pseudo = []
+  for von, fig in position.items():
+    if fig.isupper() != weiss: continue
+    if fig in 'pP':
+      _zügeBauern(weiss, fig, von, position, pseudo)
+      continue
+    f = fig.lower()
+    richtungen = _MOVES[f][1:]
+    multiplikator = _MOVES[f][0]
+    for ds, dz in richtungen:
+      for m in range(1, multiplikator + 1):
+        zu = von[0] + ds * m, von[1] + dz * m
+        if zu not in BRETT: break
+        if zu in position and position[zu].isupper() == weiss: break
+        if zu in position and position[zu].isupper() != weiss:
+          pseudo.append((fig, von, zu, position[zu]))
+          break
+        else:
+          pseudo.append((fig, von, zu, False))
+  return pseudo 
+
+def _zügeBauern(weiss, fig, von, position, pseudo):
+  # Stiller Zug
+  for ds, dz in _MOVES[fig][1:]:
+    for m in range(1,_MOVES[fig][0] + 1):
+      zu = von[0], von[1] + dz * m
+      if zu not in BRETT or zu in position: break
+      if m == 2 and von[1] != _GRUNDLINIE[weiss]: break
+      if zu[1] in (0, 7):
+        pseudo.append((fig, von, zu, False, True))
+      else:
+        pseudo.append((fig, von, zu, False, False))         
+  # Schlagzug
+  for ds, dz in _MOVES[fig+'c'][1:]:
+    zu = von[0] + ds, von[1] + dz
+    if zu not in position: continue
+    if position[zu].isupper() == weiss: continue
+    if zu[1] in (0, 7):
+      pseudo.append((fig, von, zu, position[zu], True))
+    else:
+      pseudo.append((fig, von, zu, position[zu], False)) 
+
+        
