a9fd25cabbf2abf010346b9722229e26db3d42b5
Gravitar64 <gravitar@web.de>
2021-04-12 18:02:58 +0200

    youtube

--- Teil_52_Inzidenz.py
+++ Teil_52_Inzidenz.py
@@ -1,18 +1,16 @@
 import requests, json
 from twilio.rest import Client
-import Twilio_credentials as cred
+import Teil_52_Twilio_credentials as cred 
 
-
-URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=%20GEN%20%3D%20'NORDFRIESLAND'OR%20GEN%20%3D%20'FLENSBURG'&outFields=GENGEN%20%3D%20'HANNOVER'%20ORGEN%20%3D%20'CELLE'%20,last_update,cases7_per_100k_txt&returnGeometry=false&outSR=&f=json"
+URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'REGION%20HANNOVER'%20OR%20GEN%20%3D%20'FLENSBURG'&outFields=GEN,cases7_per_100k_txt,last_update&returnGeometry=false&outSR=&f=json"
 
 inzidenzen = requests.get(URL).json()
 last_update = inzidenzen['features'][0]['attributes']['last_update']
-
-whats_app_text = f'Stand: {last_update}\n\n'
+whatsapp = f'Stand: {last_update}\n\n'
 for inzidenz in inzidenzen['features']:
   landkreis = inzidenz['attributes']['GEN']
-  inzidenz = inzidenz['attributes']['cases7_per_100k_txt']
-  whats_app_text += f'{inzidenz}\t {landkreis}\n'
+  inz = inzidenz['attributes']['cases7_per_100k_txt']
+  whatsapp += f'{inz}\t {landkreis}\n'
 
 client = Client(cred.ACCOUNT_SID, cred.AUTH_TOKEN)
-client.messages.create(body=whats_app_text, from_=cred.FROM, to=cred.TO)
+client.messages.create(body=whatsapp, from_= cred.FROM, to=cred.TO)
