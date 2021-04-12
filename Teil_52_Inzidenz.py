df067bfb31afb57fa23e1c706f057ecc14420a54
Gravitar64 <gravitar@web.de>
2021-04-12 15:51:41 +0200

    namensänderung

--- Teil_52_Inzidenz.py
+++ Teil_52_Inzidenz.py
@@ -3,11 +3,12 @@ from twilio.rest import Client
 import Twilio_credentials as cred
 
 
-URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'CELLE'%20OR%20GEN%20%3D%20'NORDFRIESLAND'OR%20GEN%20%3D%20'FLENSBURG'&outFields=GEN,last_update,cases7_per_100k_txt&returnGeometry=false&outSR=&f=json"
+URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=%20GEN%20%3D%20'NORDFRIESLAND'OR%20GEN%20%3D%20'FLENSBURG'&outFields=GENGEN%20%3D%20'HANNOVER'%20ORGEN%20%3D%20'CELLE'%20,last_update,cases7_per_100k_txt&returnGeometry=false&outSR=&f=json"
 
 inzidenzen = requests.get(URL).json()
 last_update = inzidenzen['features'][0]['attributes']['last_update']
-whats_app_text = ''
+
+whats_app_text = f'Stand: {last_update}\n\n'
 for inzidenz in inzidenzen['features']:
   landkreis = inzidenz['attributes']['GEN']
   inzidenz = inzidenz['attributes']['cases7_per_100k_txt']
