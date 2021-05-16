df21db71f00f4f7b53e711343b62740adcb493d6
Gravitar64 <gravitar@web.de>
2021-05-16 11:31:11 +0200

    Ausgabe optimiert

--- Teil_57_12_Kugeln.py
+++ Teil_57_12_Kugeln.py
@@ -1,83 +1,78 @@
 import random as rnd
-from itertools import combinations 
-
-stati = {True:  {'?':'+', '+':'+', '=':'=', '-':'='},
-         False: {'?':'-', '-':'-', '=':'=', '+':'='}}
-
-class Kugel():
-  def __init__(self, nr, status):
-    self.nr = nr
-    self.status = status
-    self.seite = ''
-
-  def __repr__(self):
-    return f'{self.nr}{self.status}'
-
-  def _statusänderung(self, wiegung):
-    if wiegung == 'm' and not self.seite:
-      return
-    if (wiegung == 'm' and self.seite) or (wiegung in 'lr' and not self.seite):
-      self.status = '='
-      return
-    self.status = stati[self.seite == wiegung][self.status]
-
-class Kugeln():
-  def __init__(self, gesucht):
-    self.kugeln = [Kugel(nr, '?') for nr in range(anz_kugeln)]      
-    self.gesucht = gesucht
-
-  def auswerten(self, auf_waage):
-    mitte = len(auf_waage)//2
-    nicht_auf_waage = set(range(anz_kugeln)) - set(auf_waage)
-    self.lrn = {'l': auf_waage[:mitte], 'r': auf_waage[mitte:], '': nicht_auf_waage}
-    for seite, nrn in self.lrn.items():
-      for nr in nrn:
-        self.kugeln[nr].seite = seite
-    wiegung = self._wiegen()
-    for kugel in self.kugeln:
-      kugel._statusänderung(wiegung)
-    l = ' '.join(map(str, [k for k in self.kugeln if k.nr in self.lrn['l']]))
-    r = ' '.join(map(str, [k for k in self.kugeln if k.nr in self.lrn['r']]))
-    n = ' '.join(map(str, [k for k in self.kugeln if k.nr in self.lrn['']]))
-    text = f'Gesucht = {self.gesucht}, {wiegung}, [{l} <-> {r}] ({n})\n'
-    return wiegung, text  
-
-  def _wiegen(self):
-    if self.gesucht.nr in self.lrn['l']:
-      return 'l' if self.gesucht.status == '+' else 'r'
-    if self.gesucht.nr in self.lrn['r']:
-      return 'r' if self.gesucht.status == '+' else 'l'
-    return 'm'              
+from itertools import combinations
+from time import perf_counter as pfc
 
 
-def prüfung(v2lr):
+def seite_ermitteln(auf_waage):
+  seite = [0]*anz_kugeln
+  mitte = len(auf_waage)//2
+  for nr in auf_waage:
+    seite[nr] = -1 if nr in auf_waage[:mitte] else 1
+  return seite
+
+
+def wiegen(gesucht, seite):
+  return gesucht[1] * seite[gesucht[0]]
+
+
+def statusänderung(wiegung, seite):
+  for nr, status in kugeln:
+    if wiegung == 0 and seite[nr] == 0: continue
+    if (wiegung == 0 and seite[nr] != 0) or (wiegung != 0 and seite[nr] == 0):
+      kugeln[nr][1] = '='
+    else:
+      kugeln[nr][1] = stati[wiegung == seite[nr]].get(status, status)
+
+
+def kugeln2str(liste):
+  return ' '.join([f'{nr}{kugeln[nr][1]}' for nr in liste])
+  
+
+def prüfung(v1, v2m, v2lr):
   prüfergebnisse = []
   text = ''
   for nr in range(anz_kugeln):
-    gesucht = Kugel(nr,rnd.choice('+-'))
-    kugeln = Kugeln(gesucht)
-    wiegung, t1 = kugeln.auswerten(range(8))
-    if wiegung == 'm':
-      _, t2 = kugeln.auswerten([8,9,10,0,1,2])
-    else:
-      _, t2 = kugeln.auswerten(v2lr)
-    kandidaten = [k for k in kugeln.kugeln if k.status != '=']
+    gesucht = (nr, rnd.choice((-1, 1)))
+    text += f'Gesucht wird {gesucht[0]}{"+" if gesucht[1] == 1 else "-"}\n'
+    for k in kugeln: k[1] = '?'
+    for n in range(2):
+      v = v1 if n == 0 else v2m if wiegung == 0 else v2lr
+      seite = seite_ermitteln(v)
+      wiegung = wiegen(gesucht, seite)
+      statusänderung(wiegung, seite)
+      text += f'{wiegung} {kugeln2str(v)}\n' 
+      kandidaten = [k[0] for k in kugeln if k[1] != '=']
+    text += f'Kandidaten = {kugeln2str(kandidaten)}\n\n'
     prüfergebnisse.append(len(kandidaten) < 4)
-    text += t1 + t2 + f'Kandidaten = {kandidaten}\n\n'
-  return all(prüfergebnisse), text   
+  return all(prüfergebnisse), text
 
-def alle_varianten():
+def alle_varianten(modus):
   anz_lösungen = 0
-  for v2l in combinations(range(anz_kugeln), 4):
-    for v2r in combinations(range(anz_kugeln), 4):
-      if set(v2l) & set(v2r): continue
-      e, text = prüfung(v2l+v2r)
-      if e:
-        anz_lösungen += 1
-        print(text)
-  print(anz_lösungen//2)      
+  for anz in range(1, anz_kugeln//2+1):
+    for v2l in combinations(range(anz_kugeln), anz):
+      for v2r in combinations(range(anz_kugeln), anz):
+        if set(v2l) & set(v2r): continue
+        e, text = prüfung(v1,v2m,v2l+v2r)
+        if e:
+          anz_lösungen += 1
+          if modus > 0:
+            print(f'Lösung Nr. {anz_lösungen}  {v2l} <-> {v2r}')
+          if modus > 1:
+            print(text+'\n\n')
+          if modus > 2:
+            return
+  print(anz_lösungen)        
 
 
-
+start = pfc()
 anz_kugeln = 12
-alle_varianten()
+kugeln = [[nr, '?'] for nr in range(anz_kugeln)]
+
+v1 = range(8)
+v2m = [8,9,10,0,1,2]
+
+stati = {True:  {'?': '+', '-': '='},
+         False: {'?': '-', '+': '='}}
+
+alle_varianten(3)
+print(pfc()-start)
