bdc9370235629b44f81a956b2aab22cb7d2de28d
Gravitar64 <gravitar@web.de>
2021-05-21 21:57:16 +0200

    Youtube

--- Teil_58_Hangman.py
+++ Teil_58_Hangman.py
@@ -1,17 +1,17 @@
-import random as rnd
+import random as rnd 
 
 with open("Teil_58_wörter.txt") as f:
-  wort = rnd.choice([w.strip().upper() for w in f])
+  wort =  rnd.choice([w.strip().upper() for w in f])
 
-versuche, gesucht, geraten = 8, set(c for c in wort), set()
+versuche, gesucht, geraten = 8, set(b for b in wort), set()
 
 while versuche > 0:
   text = f'Noch {versuche:>2} Versuche: '
-  text += " ".join([f'{b if b in geraten else "_"}' for b in wort])
+  text += " ".join([f"{b if b in geraten else '_'}" for b in wort])
   versuch = input(text+" Ihr Buchstabe? ").upper()
   geraten.add(versuch)
-  if versuch not in wort: versuche -= 1
-  if gesucht.issubset(geraten): break  
+  if versuch not in gesucht: versuche -= 1
+  if gesucht.issubset(geraten): break
 
-print("GEWONNEN! ", end = "") if gesucht.issubset(geraten) else print("VERLOREN! ", end = "")
-print(f"Das gesuchte Wort war {wort}")
+text = "GEWONNEN! " if versuche > 0 else "VERLOREN! "
+print(text + "Das gesuchte Wort war", wort)  
