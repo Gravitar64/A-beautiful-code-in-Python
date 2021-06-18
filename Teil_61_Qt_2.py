b29f5fe3a737146f2024851500f15288e747244c
Gravitar64 <gravitar@web.de>
2021-06-18 07:22:28 +0200

    Qt-Teil 2

---
+++ Teil_61_Qt_2.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6 import QtSql
+from Teil_61_Qt.frm_main import Ui_frm_main
+
+class Frm_main(QMainWindow, Ui_frm_main):
+  def __init__(self):
+    super().__init__()
+    self.setupUi(self)
+    mod_offene_Leistungen = QtSql.QSqlRelationalTableModel()
+    mod_offene_Leistungen.setTable("Leistungen")
+    mod_offene_Leistungen.setRelation(1, QtSql.QSqlRelation("Kunden", "id", "Firma"))
+    self.tbl_offene_leitsungen.setItemDelegate(QtSql.QSqlRelationalDelegate())
+    mod_offene_Leistungen.select()
+    self.tbl_offene_leitsungen.setModel(mod_offene_Leistungen)
+
+db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+db.setDatabaseName("Teil_61_Qt/Rechnungen.sqlite")
+
+app = QApplication()
+frm_main = Frm_main()
+frm_main.show()
+app.exec()   
