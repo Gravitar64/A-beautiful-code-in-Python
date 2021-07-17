d8ea98fa7a4abd4c2870b18194a6c37d3c0578f1
Gravitar64 <gravitar@web.de>
2021-07-17 22:26:06 +0200

    YouTube2

--- Teil_64_5_würfel_Teil_2.py
+++ Teil_64_5_würfel_Teil_2.py
@@ -2,6 +2,7 @@ import pygame as pg
 import random as rnd
 from itertools import groupby 
 
+
 class Element(pg.sprite.Sprite):
   def __init__(self, id, typ, image, pos, enabled):
     super().__init__()
@@ -17,11 +18,11 @@ class Element(pg.sprite.Sprite):
     self.mögl_punkte = 0
     self.punkte = 0
 
-
   def change_enabled(self, t):
     self.enabled = t
     self.image = self.image_e.copy() if self.enabled else self.image_d.copy()
-  
+
+
 def clicked_button(e):
   global würfel_zähler
   if e.id == 0:
@@ -32,35 +33,38 @@ def clicked_button(e):
         e2.punkte += e2.mögl_punkte
         e2.selektiert = False
         e2.change_enabled(False)
-        render_punkte(e2,e2.punkte)
+        render_punkte(e2, e2.punkte)
+        #Freischalten extra 5er nach Einbuchen 5er
         if e2.id == 14 and e2.punkte:
           group_elemente.sprites()[16].change_enabled(True)
+        #Extra 5er bleiben nach Buchung von Punkten weiterhin freigeschaltet
         if e2.id == 16 and e2.mögl_punkte:
           e2.change_enabled(True)
-          render_punkte(e2,e2.punkte)  
+          render_punkte(e2, e2.punkte)  
       if e2.typ == 'Würfel' and e2.selektiert:
         e2.selektiert = False
-        e.image = e.image_e 
+        e2.image = e2.image_e  
     e.change_enabled(False)
     group_elemente.sprites()[-3].change_enabled(True)
-    render_summen()
     würfel_zähler = 0
     start_würfeln(e)
+    render_summen()      
+
 
 def clicked_würfel(e):
   e.selektiert = not e.selektiert
   e.image = e.image_d if e.selektiert else e.image_e
 
 def clicked_feld(e):
-  e.image = e.image_e.copy()
   e.selektiert = not e.selektiert
+  e.image = e.image_e.copy()
   group_elemente.sprites()[-2].change_enabled(e.selektiert)
   if e.selektiert:
-    pg.draw.rect(e.image, '#B7FF7A', (4,4,59,62), 5)
-    for e2 in group_elemente:
+    pg.draw.rect(e.image, '#B7FF7A', (4,4,59,62),5)
+    for e2 in group_elemente:     
       if e2.typ == 'Feld' and e2.selektiert and e2 != e:
         e2.selektiert = False
-        e2.image = e2.image_e.copy()  
+        e2.image = e2.image_e.copy()
   e.mögl_punkte = punkte_ermitteln(e.id)
   render_punkte(e, e.mögl_punkte)
 
@@ -74,7 +78,7 @@ def render_summen():
   global gesamt_summe
   summen = {6:0, 7:0, 8:0, 17:0}
   for e in group_elemente:
-    if e.typ != 'Feld': continue
+    if e.typ != 'Feld':continue 
     if e.id < 6:
       summen[6] += e.punkte
     elif e.id in range(9,17):
@@ -87,31 +91,7 @@ def render_summen():
     e = group_elemente.sprites()[i]
     e.punkte = punkte
     e.image = e.image_d.copy()
-    render_punkte(e, punkte)
-          
-
-def punkte_ermitteln(i):
-  
-  def punkte_straße(wurf,i):
-    wurf = sorted(set(wurf))
-    abst = [b-a for a,b in zip(wurf, wurf[1:])]
-    if 1 not in abst: return 0
-    max_seq = max(len(list(g)) for k,g in groupby(abst) if k == 1)
-    return (max_seq == 4) * 40 if i == 13 else (max_seq > 2) * 30
-  
-  wurf = sorted([e.id for e in group_elemente if e.typ == "Würfel"])
-  set_wurf, set_länge = set(wurf), len(set(wurf))
-  max_pasch = max(len(list(g)) for _,g in groupby(wurf))
-  if i < 6: return wurf.count(i+1) * (i+1)
-  if i == 9: return (max_pasch > 2) * sum(wurf)
-  if i == 10: return (max_pasch > 3) * sum(wurf)
-  if i == 11:
-    c = {wurf.count(a) for a in set_wurf}
-    return (c == {2,3}) * 25
-  if i in (12,13): return punkte_straße(wurf, i)
-  if i == 14: return (set_länge == 1) * 50
-  if i == 15: return sum(wurf)
-  if i == 16: return (set_länge == 1) * (50 + sum(wurf))  
+    render_punkte(e, e.punkte)                
 
 def start_würfeln(e):
   global anim_würfeln, würfel_zähler
@@ -128,7 +108,31 @@ def würfeln():
     e.id = rnd.randint(1,6)
     e.image_e = bild_würfel_e[e.id-1]    
     e.image_d = bild_würfel_d[e.id-1]
-    e.image = e.image_e    
+    e.image = e.image_e  
+
+def punkte_ermitteln(i):
+  
+  def punkte_straße(wurf,i):
+    wurf = sorted(set(wurf))
+    abst = [b-a for a,b in zip(wurf, wurf[1:])]
+    if 1 not in abst: return 0
+    max_seq = max(len(list(g)) for k,g in groupby(abst) if k == 1)
+    return (max_seq == 4) * 40 if i == 13 else (max_seq > 2) * 30
+  
+  wurf = sorted([e.id for e in group_elemente if e.typ == "Würfel"])
+  set_wurf, set_länge = set(wurf), len(set(wurf))
+  max_pasch = max(len(list(g)) for _,g in groupby(wurf))
+  if i < 6: return wurf.count(i+1) * (i+1)
+  if i==9: return (max_pasch >= 3) * sum(wurf)
+  if i==10: return (max_pasch >= 4) * sum(wurf)
+  if i==11:
+    s = {wurf.count(a) for a in set_wurf}
+    return (s == {2,3}) * 25
+  if i in (12,13): return punkte_straße(wurf, i)  
+  if i==14: return (set_länge == 1) * 50
+  if i==15: return sum(wurf)
+  if i==16: return (set_länge == 1) * (50 +sum(wurf))  
+
 
 pg.init()
 BREITE, HÖHE = 1280, 720
@@ -137,8 +141,8 @@ zentrum = (BREITE / 2, HÖHE / 2)
 screen = pg.display.set_mode((BREITE, HÖHE))
 pg.time.set_timer(pg.USEREVENT, 1000, True)
 anim_würfeln = True
-würfel_zähler = 1   
-gesamt_summe = 0
+würfel_zähler = 1
+gesamt_summe = 0   
 
 pfad = 'Teil_63_Bilder/'
 bild_gui = pg.image.load(f'{pfad}gui.png')
@@ -158,9 +162,7 @@ for n in range(1,6):
 for n in range(3):
   pos = [(400,615), (815,615), (506,450)]
   enabled = n == 0
-  group_elemente.add(Element(n, 'Button', f'{pfad}Button{n}', pos[n], enabled))
-
-
+  group_elemente.add(Element(n, 'Button', f'{pfad}Button{n}', pos[n], enabled)) 
 
 
 weitermachen = True
@@ -179,17 +181,17 @@ while weitermachen:
         if not e.enabled or not e.rect.collidepoint(point): continue
         if e.typ == "Button": clicked_button(e)
         if e.typ == "Würfel": clicked_würfel(e)
-        if e.typ == "Feld"  : clicked_feld(e)
+        if e.typ == "Feld": clicked_feld(e)
 
   screen.blit(bild_gui, (0,0))
   group_elemente.draw(screen)
   if anim_würfeln:
     würfeln()
-  
-  text = pg.font.SysFont('impact',60).render(str(gesamt_summe), True, '#B7FF7A')
-  rect = text.get_rect(center=zentrum)
+
+  text = pg.font.SysFont('impact', 60).render(str(gesamt_summe), True, '#B7FF7A')
+  rect = text.get_rect(center = zentrum)
   rect[1] = 5
-  screen.blit(text,rect)
+  screen.blit(text, rect)  
   
   pg.display.flip()
 
