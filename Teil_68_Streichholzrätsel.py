72b8fbf63575935df0ed27925098b55d8a56f3bf
Gravitar64 <gravitar@web.de>
2021-10-28 19:50:45 +0200

    Fehlerberichtigung Durch konsequentes Runden auf 1 Nachkommastelle

--- Teil_68_Streichholzrätsel.py
+++ Teil_68_Streichholzrätsel.py
@@ -25,32 +25,34 @@ def genKoordinaten(listen):
   return linien
 
 
+def addVec(pos, delta):
+  return tuple(round(a+b, 1) for a, b in zip(pos, delta))
+
+
 def pol2cart(w, von):
-  r = 1
-  x, y = von
-  w_rad = math.radians(w)
-  dx, dy = r * math.cos(w_rad), r * math.sin(w_rad)
-  return round(x+dx, 1), round(y+dy, 1)
+  r, w_rad = 1, math.radians(w)
+  delta = r * math.cos(w_rad), r * math.sin(w_rad)
+  return addVec(von, delta)
 
 
 def verschiebe(nachher):
   for (x, y), *args in vorher:
     for (x1, y1), *args in nachher:
-      dx, dy = x-x1, y-y1
-      yield {frozenset(((vx+dx, vy+dy), (zx+dx, zy+dy))) for (vx, vy), (zx, zy) in nachher}
+      delta = x-x1, y-y1
+      yield {frozenset((addVec(von, delta), addVec(zu, delta))) for von, zu in nachher}
 
 
 def solve():
   maxÜbereinst = 0
   for ziel in verschiebe(nachher):
-    if (l :=len(vorher & ziel)) > maxÜbereinst:
+    if (l := len(vorher & ziel)) > maxÜbereinst:
       maxÜbereinst, bestZiel = l, ziel
   return bestZiel
 
 
-l = dateiLesen('Teil_68_streichhoelzer0.txt')
-vorher, nachher = genKoordinaten(l)
-ziel = solve()
-gleich, löschen, setzen = vorher & ziel, vorher - ziel, ziel - vorher
-print(f'Anzahl übereinstimmender Streichhölzer = {len(gleich)}')
-print(f'Anzahl zu verschiebender Streichhölzer = {len(löschen)}')
+for n in range(6):
+  l = dateiLesen(f'Teil_68_streichhoelzer{n}.txt')
+  vorher, nachher = genKoordinaten(l)
+  ziel = solve()
+  gleich, löschen, setzen = vorher & ziel, vorher - ziel, ziel - vorher
+  print(f'Anzahl zu verschiebender Streichhölzer = {len(löschen)}')
