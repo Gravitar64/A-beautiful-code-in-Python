8042db6181364bfafb457c9e275b8121e94eb412
Gravitar64 <gravitar@web.de>
2022-02-04 19:38:56 +0100

    Optimierungen

--- Teil_71_Verschiebe_Puzzle.py
+++ Teil_71_Verschiebe_Puzzle.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import random as rnd 
+import random as rnd
 
 
 def i2pos(i):
@@ -10,57 +10,55 @@ def pos2i(x, y):
   return int(x//breite + y//höhe*spalten)
 
 
-def vertausche(leer, kachel):
-  kacheln[leer], kacheln[kachel] = kacheln[kachel], kacheln[leer]
+def vertausche(feld):
+  global leer
+  felder[leer], felder[feld] = felder[feld], felder[leer]
+  leer = feld
 
-
-def get_nachbarn(empty):
-  x, y = i2pos(empty)
+def get_nachbarn():
+  x, y = i2pos(leer)
   n = [pos for pos in ((x+breite, y), (x-breite, y), (x, y+höhe), (x, y-höhe))]
   return [pos2i(x, y) for x, y in n if 0 <= x < screen_breite and 0 <= y < screen_höhe]
 
 
 def mischen():
-  global leer
-  for _ in range(100):
-    kachel = rnd.choice(get_nachbarn(leer))
-    vertausche(leer, kachel)
-    leer = kachel
+  for _ in range(spalten*zeilen*10):
+    feld = rnd.choice(get_nachbarn())
+    vertausche(feld)
 
 
 spalten = zeilen = 3
 screen_breite = screen_höhe = 800
 breite, höhe = screen_breite / spalten, screen_höhe / zeilen
-screen  = pg.display.set_mode((screen_breite, screen_höhe))
+screen = pg.display.set_mode((screen_breite, screen_höhe))
+
 
 bild = pg.image.load('Teil_71_katze.jpg')
 bild = pg.transform.smoothscale(bild, (screen_breite, screen_höhe))
-kacheln = [bild.subsurface((i*breite, j*höhe, breite, höhe))
-           for j in range(zeilen) for i in range(spalten)]
-pg.draw.rect(kacheln[-1], '#000000', (0, 0, breite, höhe))
-leer = len(kacheln)-1
+felder = [bild.subsurface((s*breite, z*höhe, breite, höhe))
+           for z in range(zeilen) for s in range(spalten)]
+pg.draw.rect(felder[-1], '#1BA6A6', (0, 0, breite, höhe))
+leer = len(felder)-1
 
 clock = pg.time.Clock()
-FPS = 40
+FPS = 20
 
 while True:
   clock.tick(FPS)
+
   for ereignis in pg.event.get():
-    if ereignis.type == pg.QUIT: quit()
+    if ereignis.type == pg.QUIT:
+      quit()
     if ereignis.type == pg.MOUSEBUTTONDOWN:
       if ereignis.button == 1:
-        pos = pg.mouse.get_pos()
-        i = pos2i(*pos)
-        if i in get_nachbarn(leer):
-          vertausche(leer, i)
-          leer = i
+        feld = pos2i(*pg.mouse.get_pos())
+        if feld in get_nachbarn():
+          vertausche(feld)
       if ereignis.button == 3:
         mischen()
 
-  for i,kachel in enumerate(kacheln):
+  for i, feld in enumerate(felder):
     pos = i2pos(i)
-    screen.blit(kachel, pos)
-  for i in range(spalten):
-    pg.draw.rect(screen,'#ffffff',(i*breite,0,breite,screen_höhe),1)  
-    pg.draw.rect(screen,'#ffffff',(0,i*höhe,screen_breite,höhe),1)  
+    screen.blit(feld, pos)
+    pg.draw.rect(screen, '#000000', (*pos, breite, höhe), 1)
   pg.display.flip()
