a171b2d00993ef899d895ebe89051042e8808ac2
Gravitar64 <gravitar@web.de>
2022-02-12 14:32:43 +0100

    V6

--- Teil_72_Wordle.py
+++ Teil_72_Wordle.py
@@ -4,14 +4,14 @@ import random as rnd
 
 class Feld:
 
-  FARBEN = {-1: ('#ffffff', '#000000'),
-            0: ('#787C7E', '#ffffff'),
-            1: ('#C9B458', '#ffffff'),
-            2: ('#6AAA64', '#ffffff'),
-            3: ('#D3D6DA', '#000000')}
+  STATES = {0: ('#ffffff', '#000000'),
+            1: ('#787C7E', '#ffffff'),
+            2: ('#C9B458', '#ffffff'),
+            3: ('#6AAA64', '#ffffff'),
+            4: ('#D3D6DA', '#000000')}
 
-  def __init__(self, rect, char, state):
-    self.rect = pg.Rect(rect)
+  def __init__(self, x, y, char, state):
+    self.rect = pg.Rect(x,y,45,44)
     self.char = char
     self.state = state
 
@@ -19,13 +19,13 @@ class Feld:
   def img(self):
     bild = pg.Surface(self.rect.size)
     bild_rect = bild.get_rect()
-    farbe_hg, farbe_txt = self.FARBEN[self.state]
-    pg.draw.rect(bild, farbe_hg, bild_rect)
-    if self.state == -1:
+    bg, fg = self.STATES[self.state]
+    pg.draw.rect(bild, bg, bild_rect)
+    if self.state == 0:
       pg.draw.rect(bild, '#D3D6DA', bild_rect, 3)
     if self.char != '':
       text = pg.font.SysFont('Arial_bold', 32).render(
-          self.char, True, farbe_txt)
+          self.char, True, fg)
       text_rect = text.get_rect(center=bild_rect.center)
       bild.blit(text, text_rect)
     return bild
@@ -35,24 +35,24 @@ def generiere_felder():
   felder = []
   for i in range(26):
     x, y = i % 13 * 50 + 152, i // 13 * 50 + 500
-    felder.append(Feld((x, y, 45, 44), chr(65+i), 3))
+    felder.append(Feld(x, y, chr(65+i), 4))
   for i in range(30):
     x, y = i % 5 * 50 + 352, i // 5 * 50 + 80
-    felder.append(Feld((x, y, 45, 44), '', -1))
+    felder.append(Feld(x, y, '', 0))
   return felder
 
 
 def vergleich(geheim, versuch):
   for i in range(5):
     if versuch[i] == geheim[i]:
-      versuch[i] = geheim[i] = 2
+      versuch[i] = geheim[i] = 3
   for i, b in enumerate(versuch):
-    if b == 2: continue
+    if b == 3: continue
     if b in geheim:
-      versuch[i] = 1
-      geheim[geheim.index(b)] = 1
+      versuch[i] = 2
+      geheim[geheim.index(b)] = 2
     else:
-      versuch[i] = 0
+      versuch[i] = 1
   return versuch
 
 
@@ -72,10 +72,10 @@ def eingabe(key):
       tasten_pos = ord(felder[feld_pos].char)-65
       felder[feld_pos].state = ergebnis[i]
       felder[tasten_pos].char = ''
-      felder[tasten_pos].state = -1
+      felder[tasten_pos].state = 0
     cursor_min, cursor_max = cursor, cursor + 5
     if cursor_max > 56: print(geheim)
-  elif cursor < cursor_max:
+  elif cursor < cursor_max and key in range(65,123):
     felder[cursor].char = chr(key).upper()
     cursor += 1
 
@@ -93,6 +93,7 @@ bild = pg.image.load('Teil_72_Wordle.jpg')
 with open('Teil_72_wörter.txt') as f:
   wörter = [w.strip() for w in f]
 geheim = rnd.choice(wörter)
+geheim = 'HALLE'
 felder = generiere_felder()
 cursor, cursor_min, cursor_max = 26, 26, 31
 zeichne_felder()
