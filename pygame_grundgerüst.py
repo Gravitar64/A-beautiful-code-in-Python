80a35eb0e0e99e408912748a5b94f3e401df298c
Gravitar64 <gravitar@web.de>
2022-02-24 09:27:54 +0100

    Update für pygame V2

--- pygame_grundgerüst.py
+++ pygame_grundgerüst.py
@@ -5,15 +5,19 @@ import pygame as pg
 #   --> pip3 install pygame (mac)
 #   --> sudo apt-get install python3-pygame (Linux Debian/Ubuntu/Mint)
 
-screen = pg.display.set_mode((1000,1000))
+pg.init()
+screen = pg.display.set_mode((1920,1080))
 zentrum = (screen.get_width() / 2, screen.get_height() / 2)
 
 clock = pg.time.Clock()
 FPS = 40
 
+#Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
   clock.tick(FPS)
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT: quit()
-  screen.fill((0,0,0))
+  
+  screen.fill('#000000')
+  
   pg.display.flip()
