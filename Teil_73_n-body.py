c7b88e50a9b22bfde8b8b74dc0fdcaee3e9da80e
Gravitar64 <gravitar@web.de>
2022-02-24 22:01:41 +0100

    YouTube2

--- Teil_73_n-body.py
+++ Teil_73_n-body.py
@@ -14,13 +14,12 @@ class Body:
     entf = pg.math.Vector2.distance_to(self.pos, other.pos)
     v_richt = pg.math.Vector2.normalize(other.pos - self.pos)
     if entf > (self.rad + other.rad):
-      f = G * (self.masse * other.masse) / entf**2 * v_richt
-      f1, f2 = f / self.masse, -f / other.masse
-      self.geschw += f1      
-      other.geschw += f2
+      f = G * (self.masse * other.masse) / entf**2
+      self.geschw += f * v_richt / self.masse
+      other.geschw += f * -v_richt / other.masse
     else:
       gm = self.masse + other.masse
-      self.geschw = (self.geschw * self.masse + other.geschw * other.masse) / gm
+      self.geschw = (self.geschw * self.masse +  other.geschw * other.masse) / gm
       self.pos = (self.pos * self.masse + other.pos * other.masse) / gm
       self.masse, other.masse = gm, 0
       self.rad = (gm / 3.141) ** (1/3)
@@ -28,17 +27,18 @@ class Body:
   def aktualisiere_pos(self):
     self.pos += self.geschw / FPS
     self.spur.append((self.pos.x, self.pos.y))
-    if len(self.spur) > 50: self.spur.pop(0)
+    if len(self.spur) > 50:
+      self.spur.pop(0)
 
 
 pg.init()
 screen = pg.display.set_mode((1920, 1080))
 zentrum = (screen.get_width() / 2, screen.get_height() / 2)
-bodies = [Body(zentrum, (0,0), 200_000)]
-G = 5
 
 clock = pg.time.Clock()
 FPS = 40
+G = 5
+bodies = [Body(zentrum, (0, 0), 200_000)]
 
 # Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
@@ -55,20 +55,20 @@ while True:
     if ereignis.type == pg.MOUSEBUTTONUP and ereignis.button == 1:
       pos2 = pg.Vector2(pg.mouse.get_pos())
       geschw = pos1 - pos2
-      bodies.append(Body(pos2, geschw*2, 1000))
+      bodies.append(Body(pos2, geschw*2, 10_000))
 
-  screen.fill('#000000')
   for b1, b2 in combinations(bodies, 2):
     if b1.masse == 0 or b2.masse == 0: continue
     b1.gravitation(b2)
 
   bodies = [b for b in bodies if b.masse > 0]
 
+  screen.fill('#000000')
   for body in bodies:
     body.aktualisiere_pos()
-    pg.draw.circle(screen, '#F2cb05', body.pos, body.rad)
+    pg.draw.circle(screen, '#f2cb05', body.pos, body.rad)
     if len(body.spur) > 2:
-      pg.draw.lines(screen,'#303AF2',False,body.spur,2)
+      pg.draw.lines(screen, '#303af2', False, body.spur, 2)
 
   if pg.mouse.get_pressed()[0]:
     pos2 = pg.Vector2(pg.mouse.get_pos())
