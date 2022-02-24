cf41e67f9ac87735a83effb17858a438149e5137
Gravitar64 <gravitar@web.de>
2022-02-24 10:42:08 +0100

    YouTube2

--- Teil_73_n-body.py
+++ Teil_73_n-body.py
@@ -16,23 +16,25 @@ class Body:
       self.geschw += G * (self.masse * other.masse) / entf**2 * v_richt / self.masse
     else:
       gm = self.masse + other.masse
-      self.geschw = (self.masse * self.geschw + other.masse * other.geschw) / gm
-      self.pos = (self.masse * self.pos + other.masse * other.pos) / gm
+      self.geschw = (self.geschw * self.masse + other.geschw * other.masse) / gm
+      self.pos = (self.pos * self.masse + other.pos * other.masse) / gm
       self.masse, other.masse = gm, 0
-      self.rad = (gm/3.141)**(1/3)
+      self.rad = (gm / 3.141) ** (1/3)
 
   def aktualisiere_pos(self):
-    self.pos += self.geschw / FPS    
+    self.pos += self.geschw / FPS
 
 
+pg.init()
 screen = pg.display.set_mode((1920, 1080))
 zentrum = (screen.get_width() / 2, screen.get_height() / 2)
+bodies = [Body(zentrum, (0, 0), 200_000)]
+G = 5
 
 clock = pg.time.Clock()
 FPS = 40
-G = 5
-bodies = [Body(zentrum, (0, 0), 200_000)]
 
+# Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
   clock.tick(FPS)
   for ereignis in pg.event.get():
@@ -43,24 +45,25 @@ while True:
         m, i = max([(b.masse, i) for i, b in enumerate(bodies)])
         bodies[i].pos = pos1
         bodies[i].geschw = 0, 0
+
     if ereignis.type == pg.MOUSEBUTTONUP and ereignis.button == 1:
       pos2 = pg.Vector2(pg.mouse.get_pos())
       geschw = pos1 - pos2
       bodies.append(Body(pos2, geschw*2, 1000))
 
-  for b1,b2 in permutations(bodies,2):
+  screen.fill('#000000')
+  for b1, b2 in permutations(bodies, 2):
     if b1.masse == 0 or b2.masse == 0: continue
     b1.wird_angezogen_von(b2)
-  
+
   bodies = [b for b in bodies if b.masse > 0]
-  
-  screen.fill('#000000')
+
   for body in bodies:
     body.aktualisiere_pos()
-    pg.draw.circle(screen, '#F2CB05', body.pos, body.rad)
-  
+    pg.draw.circle(screen, '#F2cb05', body.pos, body.rad)
+
   if pg.mouse.get_pressed()[0]:
     pos2 = pg.Vector2(pg.mouse.get_pos())
-    pg.draw.line(screen, '#419FD9', pos1, pos2, 1)
-  
+    pg.draw.line(screen, '#419fd9', pos1, pos2, 1)
+
   pg.display.flip()
