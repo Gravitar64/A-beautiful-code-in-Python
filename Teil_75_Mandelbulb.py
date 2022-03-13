b6fe623a6466b7ba9592950a1655bec82be8ac77
Gravitar64 <gravitar@web.de>
2022-03-13 11:52:10 +0100

    Unicode für Theta und Phi

--- Teil_75_Mandelbulb.py
+++ Teil_75_Mandelbulb.py
@@ -1,33 +1,30 @@
 import math
-from itertools import product
 from ursina import *
-from time import process_time as pfc
+from time import perf_counter as pfc
 
 
-def punkt_in_mandelbrot(x,y,z) -> bool:
+def punkt_in_mandelbrot(x, y, z) -> bool:
   global hülle
-  if z == 0: 
+  if z == 0:
     hülle = True
     return False
-  x,y,z = x/BEREICH, y/BEREICH, z/BEREICH
+  x, y, z = x/BEREICH, y/BEREICH, z/BEREICH
   for _ in range(MAX_ITER):
-    x2, y2, z2 = x*x, y*y, z*z
-    r = (x2 + y2 + z2)**0.5
-    if r > 2: 
+    r = (x*x + y*y + z*z)**0.5
+    if r > 2:
       hülle = False
-      return False 
-    thetaN = math.atan2((x2 + y2)**0.5, z) * N
-    phiN = math.atan2(y,x) * N
-    r2N = r**N
-    rNsinTethaN = r2N * math.sin(thetaN)
-    x += rNsinTethaN * math.cos(phiN)
-    y += rNsinTethaN * math.sin(phiN)
-    z += r2N * math.cos(thetaN)
+      return False
+    θ = math.atan2((x*x + y*y)**0.5, z)
+    φ = math.atan2(y, x)
+    rn = r**N
+    x += rn * math.sin(θ * N) * math.cos(φ * N)
+    y += rn * math.sin(θ * N) * math.sin(φ * N)
+    z += rn * math.cos(θ * N)
   if not hülle:
     hülle = True
     return True
-      
-  
+
+
 start = pfc()
 N, MAX_ITER, BEREICH = 8, 20, 64
 punkte = []
@@ -35,23 +32,23 @@ for x in range(BEREICH):
   for y in range(BEREICH):
     hülle = False
     for z in range(BEREICH):
-      if not punkt_in_mandelbrot(x, y, z): continue
-      punkte.append((x,y,z))
-      punkte.append((-x,y,z))
-      punkte.append((x,-y,z))
-      punkte.append((-x,-y,z))
-      punkte.append((x,y,-z))
-      punkte.append((-x,y,-z))
-      punkte.append((x,-y,-z))
-      punkte.append((-x,-y,-z))
-
-print(f'{len(punkte):,.0f} Punkte ermittelt in {pfc()-start:.2f} Sek.')      
-      
+      if not punkt_in_mandelbrot(x, y, z):
+        continue
+      punkte.append((x, y, z))
+      punkte.append((-x, y, z))
+      punkte.append((x, -y, z))
+      punkte.append((-x, -y, z))
+      punkte.append((x, y, -z))
+      punkte.append((-x, y, -z))
+      punkte.append((x, -y, -z))
+      punkte.append((-x, -y, -z))
+print(pfc()-start)
 app = Ursina()
 window.borderless = False
 window.fps_counter.enabled = True
-window.size = (1920,1080)
+window.size = (1920, 1080)
 window.color = color.black
-Entity(model=Mesh(vertices=punkte, mode='point', thickness=0.1),color=color.green)
+Entity(model=Mesh(vertices=punkte, mode='point',
+                  thickness=0.01), color=color.green)
 EditorCamera()
 app.run()
