63cedebf06401d6b8a032089bebbbad8ecb3a160
Gravitar64 <gravitar@web.de>
2022-03-13 13:41:04 +0100

    YouTube

--- Teil_75_Mandelbulb.py
+++ Teil_75_Mandelbulb.py
@@ -1,46 +1,47 @@
+import ursina as urs
 import math
-from ursina import *
-from itertools import product
+import itertools as it
 
 
-def punkt_in_mandelbrot(x, y, z) -> bool:
+def punkt_in_fraktal(x, y, z) -> bool:
   global hülle
   if z == 0:
     hülle = True
     return False
   x, y, z = x/BEREICH, y/BEREICH, z/BEREICH
   for _ in range(MAX_ITER):
-    r = (x*x + y*y + z*z)**0.5
+    r = (x*x + y*y + z*z) ** 0.5
     if r > 2:
       hülle = False
       return False
-    θ = math.atan2((x*x + y*y)**0.5, z)
-    φ = math.atan2(y, x)
-    rn = r**N
-    x += rn * math.sin(θ * N) * math.cos(φ * N)
-    y += rn * math.sin(θ * N) * math.sin(φ * N)
-    z += rn * math.cos(θ * N)
+    θN = math.atan2((x*x + y*y) ** 0.5, z) * N
+    φN = math.atan2(y, x) * N
+    rN = r**N
+    x += rN * math.sin(θN) * math.cos(φN)
+    y += rN * math.sin(θN) * math.sin(φN)
+    z += rN * math.cos(θN)
   if not hülle:
     hülle = True
     return True
 
 
-N, MAX_ITER, BEREICH = 8, 20, 64
+BEREICH, N, MAX_ITER = 64, 8, 16
 punkte = []
 for x in range(BEREICH):
   for y in range(BEREICH):
     hülle = False
     for z in range(BEREICH):
-      if not punkt_in_mandelbrot(x, y, z): continue
-      for s1,s2,s3 in product([-1,1],repeat=3):
-        punkte.append((x*s1, y*s2, z*s3))
+      if not punkt_in_fraktal(x, y, z):
+        continue
+      for m1, m2, m3 in it.product([-1, 1], repeat=3):
+        punkte.append((x*m1, y*m2, z*m3))
 
-app = Ursina()
-window.borderless = False
-window.fps_counter.enabled = True
-window.size = (1920, 1080)
-window.color = color.black
-Entity(model=Mesh(vertices=punkte, mode='point',
-                  thickness=0.01), color=color.green)
-EditorCamera()
+
+app = urs.Ursina()
+urs.window.borderless = False
+urs.window.size = (1920, 1080)
+urs.window.color = urs.color.black
+urs.Entity(model=urs.Mesh(vertices=punkte, mode='point',
+                          thickness=0.01), color=urs.color.green)
+urs.EditorCamera()
 app.run()
