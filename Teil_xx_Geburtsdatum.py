d752dff6a0ccfbcfcc214cf035579b5963c24812
Gravitar64 <gravitar@web.de>
2022-05-06 11:41:44 +0200

    Problemdarstellung arrow

--- Teil_xx_Geburtsdatum.py
+++ Teil_xx_Geburtsdatum.py
@@ -1,14 +1,22 @@
 import datetime as dt
-from tkinter import N
+import arrow
 from dateutil.relativedelta import relativedelta
 import locale
 
 locale.setlocale(locale.LC_ALL, 'German')
 dat_format = '%a %d.%m.%Y %H:%M'
 
-geburtsdatum = dt.datetime(1995,05,08,12,59)
+
+geburtsdatum = dt.datetime(1964,10,21,12,59)
 jetzt = dt.datetime.now()
 alter = jetzt-geburtsdatum
+
+start = arrow.now()
+start.replace(tzinfo='Europe/Berlin')
+end = arrow.get('30.04.2022 13:00','DD.MM.YYYY HH:mm')
+print(f'Start-Datetime: {start}')
+print(f'End-Datetime : {end}')
+print(f'Time-Difference: {end-start}')
 
 jubiläen = []
 for k in (500,1_000,22_222,5_000,10_000):
