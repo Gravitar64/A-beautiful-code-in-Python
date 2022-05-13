00db54cfaadcd5ca927f978d2d9d465e786e9929
Gravitar64 <gravitar@web.de>
2022-05-13 22:08:21 +0200

    ohne Zeitmessung

--- Teil_76_Monty_Hall_Problem.py
+++ Teil_76_Monty_Hall_Problem.py
@@ -1,7 +1,5 @@
 import random as rnd
-from time import perf_counter as pfc
 
-start = pfc()
 gew_ow = gew_mw = 0
 sim = 1_000_000
 türen = {0,1,2}
@@ -16,6 +14,5 @@ for _ in range(sim):
   if auto == wahl:
     gew_mw += 1
 
-print(f'{sim:,.0f} Simulationen in {pfc()-start:.2f} Sek.')
 print(f'Gewinnwahrscheinlichkeit ohne Wechsel = {gew_ow/sim*100:.2f} %')
 print(f'Gewinnwahrscheinlichkeit mit Wechsel  = {gew_mw/sim*100:.2f} %')
