d0f5b709216afa62369a98991b9984a185465fe6
Gravitar64 <gravitar@web.de>
2022-05-13 13:31:08 +0200

    YouTube2

--- Teil_76_Monty_Hall_Problem.py
+++ Teil_76_Monty_Hall_Problem.py
@@ -2,19 +2,20 @@ import random as rnd
 from time import perf_counter as pfc
 
 start = pfc()
-gew_ow = gew_mw = 0
+gew_ohne_wechsel = gew_mit_wechsel = 0
 anz_simulationen = 1_000_000
-türen = {0,1,2}
 
 for _ in range(anz_simulationen):
-  auto, wahl  = rnd.randrange(3), rnd.randrange(3)
+  auto = rnd.randrange(3)
+  wahl = rnd.randrange(3)
   if auto == wahl:
-    gew_ow += 1
-  ziege = (türen - {auto, wahl}).pop()
-  wahl  = (türen - {wahl, ziege}).pop()
+    gew_ohne_wechsel += 1
+  ziege = ({0, 1, 2} - {auto, wahl}).pop()
+  wahl  = ({0, 1, 2} - {wahl, ziege}).pop()
   if auto == wahl:
-    gew_mw += 1
+    gew_mit_wechsel += 1
+
 
 print(f'{anz_simulationen:,.0f} Simulationen in {pfc()-start:.2f} Sek.')
-print(f'Gewinnquote ohne Wechsel = {gew_ow/anz_simulationen*100:.2f} %')    
-print(f'Gewinnquote mit Wechsel  = {gew_mw/anz_simulationen*100:.2f} %')
+print(f'Gewinnwahrscheinlichkeit ohne Wechsel = {gew_ohne_wechsel/anz_simulationen*100:.2f} %')
+print(f'Gewinnwahrscheinlichkeit mit Wechsel  = {gew_mit_wechsel/anz_simulationen*100:.2f} %')
