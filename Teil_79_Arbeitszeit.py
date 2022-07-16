75e910d83973e120e7feaa19f9f720d16f0252ed
Gravitar64 <gravitar@web.de>
2022-07-16 12:39:30 +0200

    youtube3

--- Teil_79_Arbeitszeit.py
+++ Teil_79_Arbeitszeit.py
@@ -1,48 +1,38 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import time
 import curses
+import playsound
 
 
-def zeitstatus(start, soll, pause_nach_6h, pause_nach_9h):
-  jetzt = datetime.now()
-  jetzt = timedelta(hours=jetzt.hour, minutes=jetzt.minute,
-                    seconds=jetzt.second)
-  ist = jetzt - start
-  if ist > timedelta(hours=6):
-    soll = soll + pause_nach_6h
-  if ist > timedelta(hours=9):
-    soll = soll + pause_nach_9h
-  arbeitsende = start + soll
-  saldo = arbeitsende-jetzt if arbeitsende > jetzt else jetzt-arbeitsende
-  max_arbeitszeit = timedelta(hours=10, minutes=45)
-
-  stdscr.addstr(0, 0, f'  Start                           :  {start}')
-  stdscr.addstr(1, 0, f'+ Arbeitszeit inkl. Pausen        :  {soll}')
-  stdscr.addstr(2, 0, f'= Reguläres Arbeitsende           : {start+soll}')
-  stdscr.addstr(3, 0, f'- Aktuelle Uhrzeit                : {jetzt}')
-  stdscr.addstr(4, 0, f'= Saldo jetzt                     : {"-" if jetzt < arbeitsende else "+"}{saldo}', COLOR_RED if ist < soll else COLOR_GREEN | curses.A_BOLD)
-
-  stdscr.addstr(
-      6, 0, f'Spätester Feierabend              : {start+max_arbeitszeit}')
-  stdscr.refresh()
+def td(z):
+  if type(z) == str:
+    HH, MM = map(int, z.split(':'))
+    return timedelta(hours=HH, minutes=MM)
+  else:
+    return timedelta(hours=z.hour, minutes=z.minute, seconds=z.second)
 
 
-H, M = map(int, input('Arbeitsbeginn (HH:MM): ').split(':'))
-start = timedelta(hours=H, minutes=M)
-soll = timedelta(hours=7, minutes=45)
-pause_nach_6h = timedelta(minutes=30)
-pause_nach_9h = timedelta(minutes=15)
-
-stdscr = curses.initscr()
-curses.curs_set(False)
-curses.start_color()
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-COLOR_RED = curses.color_pair(1)
-COLOR_GREEN = curses.color_pair(2)
+start = td(input('Arbeitsbeginn (HH:MM): '))
+soll = td('7:45')
+max_soll = td('10:45')
+pausen = td('0:00')
+std = curses.initscr()
 
 while True:
-  zeitstatus(start, soll, pause_nach_6h, pause_nach_9h)
-  time.sleep(1)
+  jetzt = td(datetime.now())
+  ist = jetzt - start
+  pausen = td('0:45') if ist > td('9:00') else td(
+      '0:30') if ist > td('6:00') else pausen
+  saldo = '+' + str(ist - (soll + pausen)) if ist >= (soll +
+                                                      pausen) else '-' + str(soll+pausen-ist)
 
-curses.endwin()
+  std.addstr(0, 0, f'  Arbeitszeit inkl. Pausen          =  {soll + pausen}')
+  std.addstr(1, 0, f'- IST                               =  {ist}')
+  std.addstr(2, 0, f'= akt. Saldo                        = {saldo}')
+  std.addstr(4, 0, f'  Feierabend bei start {str(start)[:-3]:0>5}        = {start + soll + pausen} (max. {start + max_soll})')
+  
+  if jetzt == (start + soll + pausen):
+    playsound.playsound('Teil_79_yabba_dabba_doo.mp3')
+  
+  time.sleep(1)
+  std.refresh()
