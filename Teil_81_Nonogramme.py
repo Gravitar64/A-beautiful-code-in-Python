0b4a3b4f0d7c37ee6ebfbd2954003531e438f6d5
Gravitar64 <gravitar@web.de>
2022-08-25 19:03:23 +0200

    YouTube 8

--- Teil_81_Nonogramme.py
+++ Teil_81_Nonogramme.py
@@ -1,5 +1,5 @@
-import itertools as itt
 from time import perf_counter as pfc
+import itertools as itt
 
 
 def datei_einlesen(datei):
@@ -14,29 +14,28 @@ def nonogramm_fehlerhaft(nonogramm):
   return sum(map(sum, nonogramm[ZEILEN])) != sum(map(sum, nonogramm[SPALTEN]))
 
 
-def gen_perm(zf, l):
-  perm = []
-  for v in itt.combinations(range(l-sum(zf)+1), len(zf)):
-    v = [v[0]] + [b-a for a, b in zip(v, v[1:])]
-    perm.append(''.join([' '*leer + '#'*zf[i] for i, leer in enumerate(v)]).ljust(l, ' '))
-  return perm
-
-
-def prüfe_gültige(perm, sicht, zs):
-  vergleich = spielfeld[zs] if sicht == ZEILEN else [
-      spielfeld[z][zs] for z in range(höhe)]
-  if all(e == '?' for e in vergleich): return perm
-  gültige = []
-  for p in perm:
-    if not all(a == '?' or a == b for a, b in zip(vergleich, p)): continue
-    gültige.append(p)
-  return gültige
-
-
 def zeige_spielfeld():
   for zeile in spielfeld:
     print(' '.join(zeile))
   print()
+
+
+def gen_perm(zf, l):
+  perm = []
+  for p in itt.combinations(range(l-sum(zf)+1), len(zf)):
+    p = [p[0]] + [b-a for a,b in zip(p, p[1:])]
+    perm.append(''.join([' '*leer + '#'*zf[i] for i,leer in enumerate(p)]).ljust(l,' '))
+  return perm
+
+
+def prüfe_gültige(perm, sicht, zs):
+  vergleich = spielfeld[zs] if sicht == ZEILEN else [spielfeld[z][zs] for z in range(höhe)]
+  if all(e == '?' for e in vergleich): return perm
+  gültige = []
+  for p in perm:
+    if not all(a=='?' or a == b for a,b in zip(vergleich, p)): continue
+    gültige.append(p)
+  return gültige    
 
 
 def löse_nonogramm(nonogramm):
@@ -52,15 +51,15 @@ def löse_nonogramm(nonogramm):
         speicher[(sicht,zs)] = gültige
         eindeutige = [(i, s[0]) for i, s in enumerate(zip(*gültige)) if len(set(s)) == 1]
         for sz, e in eindeutige:
-          z, s = (zs, sz) if sicht == ZEILEN else (sz, zs)
+          z,s = (zs, sz) if sicht == ZEILEN else (sz, zs)
           if spielfeld[z][s] != e:
             spielfeld[z][s] = e
             änderung = True
 
 
+
 nonogramme = datei_einlesen('Teil_81_Nonogram_problems.txt')
 ZEILEN, SPALTEN = 0, 1
-
 
 for nonogramm in nonogramme:
   start = pfc()
@@ -68,7 +67,8 @@ for nonogramm in nonogramme:
     print('Sorry, das Nonogramm ist fehlerhaft und kann nicht gelöst werden')
     continue
   breite, höhe = len(nonogramm[SPALTEN]), len(nonogramm[ZEILEN])
-  spielfeld = [['?']*breite for _ in range(höhe)]
+  spielfeld = [['?'] * breite for _ in range(höhe)]
   löse_nonogramm(nonogramm)
   zeige_spielfeld()
   print(pfc()-start)
+
