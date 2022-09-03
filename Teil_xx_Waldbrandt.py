fc5b35def24952d16731fcbc5c17b5067ca4952b
Gravitar64 <gravitar@web.de>
2022-09-03 14:07:30 +0200

    realisierung mit sets Halb so schnell wie Lists

--- Teil_xx_Waldbrandt.py
+++ Teil_xx_Waldbrandt.py
@@ -1,80 +1,52 @@
 import random as rnd
-import itertools as itt
 import pygame as pg
-from time import perf_counter as pfc
 
 
-def i2sz(i):
-  return i % ZELLEN, i // ZELLEN
+def zeige_wald(bäume, brennen):
+  for s,z in bäume | brennen:
+    color = '#00ff00' if (s,z) in bäume else '#ff0000'
+    pg.draw.rect(fenster,color,(s*PIXEL,z*PIXEL,PIXEL,PIXEL))
+        
 
-
-def create_new_forest(ZELLEN):
-  return {i2sz(i):[BAUM,1]  for i in range(ZELLEN*ZELLEN) if rnd.random() <= START_BÄUME}
-
-
-def display_forest(forest):
-  for (s,z),(b,alter) in forest.items():
-    farbe.hsva = (122,100,max(100-alter,10))  if b == BAUM else (0,61,100)
-    pg.draw.rect(fenster,farbe,(s*PIXEL,z*PIXEL,PIXEL,PIXEL))    
-      
-
-def feuer_nachbarn(s,z,f):
-  del f[(s,z)]
-  for ds,dz in itt.product(range(-1,2), range(-1,2)):
-    if not (s+ds, z+dz) in f: continue
-    if f[(s+ds, z+dz)][0] != BAUM: continue
-    f[(s+ds, z+dz)] = [BRENNT,1]
+def feuer_nachbarn(bäume, brennen):
+  bäume -= brennen
+  brennen = {(s+ds, z+dz) for s,z in brennen 
+                 for ds in range(-1,2)
+                 for dz in range(-1,2)
+                 if (s+ds, z+dz) in bäume}
+  return bäume-brennen, brennen
     
 
-def sim(forest):
-  forest2 = forest.copy()
-  stichprobe = int(len(KOORD) * WACHSTUM)
-  for _ in range(stichprobe):
-    pos = rnd.choice(KOORD)
-    if (pos) in forest: continue
-    forest2[pos] = [BAUM,1]
+def sim(bäume, brennen):
+  leer = koord - bäume - brennen
+  bäume -= brennen
+  brennen |= set(rnd.sample(bäume,int(len(bäume)*BRAND)))
+  bäume |= set(rnd.sample(leer,int(len(leer)*WACHSTUM)))
+  return feuer_nachbarn(bäume, brennen)
   
-  for pos, b in forest.items():
-    if b[0] == BAUM: 
-      if rnd.random() <= BRAND:
-        forest2[pos] = [BRENNT,1]
-      else:
-        forest2[pos][1] += 1  
-    elif b[0] == BRENNT:
-      feuer_nachbarn(*pos, forest2)
-  return forest2
 
-
-LEER, BAUM, BRENNT  = 0,1,2 
 START_BÄUME = 0.5
 WACHSTUM = 0.01          
 BRAND = 0.001         
-
+FPS = 40
 
 pg.init()
-ZELLEN, PIXEL = 80, 10
-KOORD = [(s,z) for s,z in itt.product(range(ZELLEN), range(ZELLEN))]
-fenster_b, fenster_h = ZELLEN*PIXEL, ZELLEN*PIXEL
+GR, PIXEL = 80, 10
+fenster_b, fenster_h = GR*PIXEL, GR*PIXEL
 fenster = pg.display.set_mode((fenster_b, fenster_h))
 clock = pg.time.Clock()
-farbe = pg.Color(0)
-FPS = 2
 
-forest = create_new_forest(ZELLEN)
+koord = {(s,z) for z in range(GR) for s in range(GR)}
+bäume = set(rnd.sample(koord, int(GR*GR*START_BÄUME)))
+brennen = set() 
 
-frames = gesamt = 0
 while True:
   clock.tick(FPS)
-  frames += 1
-  
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT: quit()
   
   fenster.fill('#000000')
-  
-  start = pfc()
-  forest = sim(forest)
-  gesamt += pfc() - start
-  print(gesamt/frames)
-  display_forest(forest)
+  bäume, brennen = sim(bäume, brennen)
+  zeige_wald(bäume, brennen)
+
   pg.display.flip()
