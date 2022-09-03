1094327c07efc8ac091d3b85bd89b76a5bebb11b
Gravitar64 <gravitar@web.de>
2022-09-03 16:59:16 +0200

    def sim eleminiert

--- Teil_xx_Waldbrandt.py
+++ Teil_xx_Waldbrandt.py
@@ -2,14 +2,13 @@ import random as rnd
 import pygame as pg
 
 
-def zeige_wald(bäume, brennen):
+def zeichne_wald(bäume, brennen):
   for s,z in bäume | brennen:
     color = '#00ff00' if (s,z) in bäume else '#ff0000'
     pg.draw.rect(fenster,color,(s*PIXEL,z*PIXEL,PIXEL,PIXEL))
         
 
 def feuer_nachbarn(bäume, brennen):
-  bäume -= brennen
   brennen = {(s+ds, z+dz) for s,z in brennen 
                  for ds in range(-1,2)
                  for dz in range(-1,2)
@@ -17,27 +16,16 @@ def feuer_nachbarn(bäume, brennen):
   return bäume-brennen, brennen
     
 
-def sim(bäume, brennen):
-  leer = koord - bäume - brennen
-  bäume -= brennen
-  brennen |= set(rnd.sample(bäume,int(len(bäume)*BRAND)))
-  bäume |= set(rnd.sample(leer,int(len(leer)*WACHSTUM)))
-  return feuer_nachbarn(bäume, brennen)
-  
-
-START_BÄUME = 0.5
-WACHSTUM = 0.01          
-BRAND = 0.001         
-FPS = 40
+ANF_BESTAND, WACHSTUM, FEUER = 0.2, 0.01, 0.001
+GR, PIXEL = 80, 10
+FPS = 25
 
 pg.init()
-GR, PIXEL = 80, 10
-fenster_b, fenster_h = GR*PIXEL, GR*PIXEL
-fenster = pg.display.set_mode((fenster_b, fenster_h))
+fenster = pg.display.set_mode((GR*PIXEL, GR*PIXEL))
 clock = pg.time.Clock()
 
 koord = {(s,z) for z in range(GR) for s in range(GR)}
-bäume = set(rnd.sample(koord, int(GR*GR*START_BÄUME)))
+bäume = set(rnd.sample(koord, int(GR*GR*ANF_BESTAND)))
 brennen = set() 
 
 while True:
@@ -45,8 +33,11 @@ while True:
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT: quit()
   
+  leer = koord - bäume - brennen
+  bäume |= set(rnd.sample(leer,int(len(leer)*WACHSTUM)))
+  brennen |= set(rnd.sample(bäume,int(len(bäume)*FEUER)))
+  bäume, brennen = feuer_nachbarn(bäume-brennen, brennen)
+      
   fenster.fill('#000000')
-  bäume, brennen = sim(bäume, brennen)
-  zeige_wald(bäume, brennen)
-
+  zeichne_wald(bäume, brennen)
   pg.display.flip()
