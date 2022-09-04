70e2dc8e2fe14fa686bd8ea8bb15e3427df87935
Gravitar64 <gravitar@web.de>
2022-09-04 21:29:08 +0200

    YouTube

--- Teil_xx_Waldbrand.py
+++ Teil_xx_Waldbrand.py
@@ -1,43 +1,45 @@
-import random as rnd
 import pygame as pg
+import random as rnd
 
 
 def zeichne_wald(wald, brand):
-  for s,z in wald | brand:
-    color = '#00ff00' if (s,z) in wald else '#ff0000'
-    pg.draw.rect(fenster,color,(s*PIXEL,z*PIXEL,PIXEL,PIXEL))
-        
+  for s, z in wald | brand:
+    color = '#00ff00' if (s, z) in wald else '#ff0000'
+    pg.draw.rect(fenster, color, (s*PIXEL, z*PIXEL, PIXEL, PIXEL))
+
 
 def feuer_nachbarn(wald, brand):
-  brand = {(s+ds, z+dz) for s,z in brand 
-                 for ds in range(-1,2)
-                 for dz in range(-1,2)
-                 if (s+ds, z+dz) in wald}
+  brand = {(s+ds, z+dz) for s, z in brand
+           for ds in range(-1, 2)
+           for dz in range(-1, 2)
+           if (s+ds, z+dz) in wald}
   return wald-brand, brand
-    
 
-ANF_BESTAND, WACHSTUM, FEUER = 0.2, 0.01, 0.001
-GR, PIXEL = 80, 10
-FPS = 25
 
 pg.init()
+GR, PIXEL = 80, 10
 fenster = pg.display.set_mode((GR*PIXEL, GR*PIXEL))
-clock = pg.time.Clock()
 
-koord = {(s,z) for z in range(GR) for s in range(GR)}
-wald = set(rnd.sample(koord, int(GR*GR*ANF_BESTAND)))
-brand = set() 
+
+clock = pg.time.Clock()
+FPS = 25
+ANF_BESTAND, WACHSTUM, FEUER = 0.2, 0.02, 0.001
+koord = {(s, z) for z in range(GR) for s in range(GR)}
+wald = set(rnd.sample(koord, int(len(koord)*ANF_BESTAND)))
+brand = set()
+
 
 while True:
   clock.tick(FPS)
   for ereignis in pg.event.get():
-    if ereignis.type == pg.QUIT: quit()
-  
+    if ereignis.type == pg.QUIT:
+      quit()
+
   leer = koord - wald - brand
-  wald |= set(rnd.sample(leer,int(len(leer)*WACHSTUM)))
-  brand |= set(rnd.sample(wald,int(len(wald)*FEUER)))
+  wald |= set(rnd.sample(leer, int(len(leer)*WACHSTUM)))
+  brand |= set(rnd.sample(wald, int(len(wald)*FEUER)))
   wald, brand = feuer_nachbarn(wald-brand, brand)
-      
+
   fenster.fill('#000000')
   zeichne_wald(wald, brand)
   pg.display.flip()
