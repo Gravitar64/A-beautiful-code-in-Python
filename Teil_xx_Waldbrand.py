e294e3193daa23962b6b41bd864aa063901c0a9e
Gravitar64 <gravitar@web.de>
2022-09-05 14:18:39 +0200

    Update Teil_xx_Waldbrand.py

--- Teil_xx_Waldbrand.py
+++ Teil_xx_Waldbrand.py
@@ -1,45 +1,38 @@
-import pygame as pg
 import random as rnd
+import pygame as pg
 
 
-def zeichne_wald(wald, brand):
-  for s, z in wald | brand:
-    color = '#00ff00' if (s, z) in wald else '#ff0000'
-    pg.draw.rect(fenster, color, (s*PIXEL, z*PIXEL, PIXEL, PIXEL))
+def feuerwalze(bäume, brand):
+    return {(s+ds, z+dz) for s,z in brand for ds in range(-1,2) for dz in range(-1,2) if (s+ds, z+dz) in bäume}
+   
+   
+def zeichne_fläche(bäume, brand):
+    for s,z in bäume | brand:
+        farbe = '#00ff00' if (s,z) in bäume else '#ff0000'
+        pg.draw.rect(fenster,farbe,(s*pixel, z*pixel, pixel, pixel))
+    
 
-
-def feuer_nachbarn(wald, brand):
-  brand = {(s+ds, z+dz) for s, z in brand
-           for ds in range(-1, 2)
-           for dz in range(-1, 2)
-           if (s+ds, z+dz) in wald}
-  return wald-brand, brand
-
-
-pg.init()
-GR, PIXEL = 80, 10
-fenster = pg.display.set_mode((GR*PIXEL, GR*PIXEL))
-
-
-clock = pg.time.Clock()
-FPS = 25
-ANF_BESTAND, WACHSTUM, FEUER = 0.2, 0.02, 0.001
-koord = {(s, z) for z in range(GR) for s in range(GR)}
-wald = set(rnd.sample(koord, int(len(koord)*ANF_BESTAND)))
+ANF_BESTAND, WACHSTUM, BRAND = 0.2, 0.01, 0.01
+fläche = {(s,z) for s in range(bh) for z in range(bh)}
+bäume = set(rnd.sample(list(fläche), int(bh*bh*ANF_BESTAND)))
 brand = set()
 
+pg.init()
+bh, pixel, FPS = 80, 10, 25
+fenster = pg.display.set_mode((bh*pixel, bh*pixel))
+clock = pg.time.Clock()
 
 while True:
-  clock.tick(FPS)
-  for ereignis in pg.event.get():
-    if ereignis.type == pg.QUIT:
-      quit()
-
-  leer = koord - wald - brand
-  wald |= set(rnd.sample(leer, int(len(leer)*WACHSTUM)))
-  brand |= set(rnd.sample(wald, int(len(wald)*FEUER)))
-  wald, brand = feuer_nachbarn(wald-brand, brand)
-
-  fenster.fill('#000000')
-  zeichne_wald(wald, brand)
-  pg.display.flip()
+    clock.tick(FPS)
+    for ereignis in pg.event.get():
+        if ereignis.type == pg.QUIT: quit()
+    
+    leer = fläche - bäume - brand
+    bäume |= set(rnd.sample(list(leer), int(len(leer)*WACHSTUM)))
+    brand |= set(rnd.sample(list(bäume), int(len(bäume)*BRAND)))
+    bäume -= brand
+    brand = feuerwalze(bäume, brand)
+    
+    fenster.fill('#000000')
+    zeichne_fläche(bäume-brand, brand)
+    pg.display.flip()
