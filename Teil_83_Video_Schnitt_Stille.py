80620d21ac9d89135ecae70fb9190676c1d1f356
Gravitar64 <gravitar@web.de>
2022-09-09 17:42:54 +0200

    YouTube

--- Teil_83_Video_Schnitt_Stille.py
+++ Teil_83_Video_Schnitt_Stille.py
@@ -1,25 +1,5 @@
-# Basiert auf folgendem Source-Code von Donald Feury
-# https://gitlab.com/dak425/scripts/-/blob/master/trim_silenceV2
 import moviepy.editor as mvpe
 import numpy as np
-
-
-def finde_sprache(audio, abschnitt, min_lautst):
-  schnittliste, sprache = [], False
-   
-  for ts in np.arange(0, video.end, abschnitt):
-    lautst = audio.subclip(ts, ts+abschnitt).max_volume()
-    if lautst >= min_lautst and not sprache:
-      start = ts
-      sprache = True
-    elif lautst < min_lautst and sprache :
-      schnittliste.append([start, ts])
-      sprache = False
-  
-  if sprache:
-    schnittliste.append([start, ts])
-  
-  return schnittliste
 
 
 def lade_datei(datei):
@@ -27,20 +7,35 @@ def lade_datei(datei):
   return mvpe.VideoFileClip(datei), name+'_stille.'+ext
 
 
-video, ausgabe = lade_datei(
-    'y:\\Videos\\OBS Studio\\adventOfCode\\2022-09-05 18-49-31.mp4')
+def finde_sprache(audio, abschnitt, min_lautst):
+  schnittliste, sprache = [], False
+
+  for zeit in np.arange(0, audio.end, abschnitt):
+    lautst = audio.subclip(zeit, zeit+abschnitt).max_volume()
+    if lautst >= min_lautst and not sprache:
+      start = zeit
+      sprache = True
+    elif lautst < min_lautst and sprache:
+      schnittliste.append([start, zeit])
+      sprache = False
+
+  if sprache:
+      schnittliste.append([start, zeit])
+
+  return schnittliste        
+
+video, ausgabedatei = lade_datei('Teil_83_Beispiel.mp4')
 schnittliste = finde_sprache(video.audio, 0.05, 0.03)
-länge = sum(b-a for a, b in schnittliste)
 
-print(f'  Videolänge vor dem Schnitt  : {video.end//60:.0f}:{video.end%60:.0f}')
-print(f'- Videolänge nach dem Schnitt : {länge//60:.0f}:{länge%60:.0f}')
-print(f'= Einsparung in %             : {(1-länge/video.end)*100:.2f}%')
-print(f'  Anz. Schnitte               : {len(schnittliste)}')
+länge_vs, länge_ns = video.end, sum(e-s for s,e in schnittliste)
 
-clips = [video.subclip(s, e) for s, e in schnittliste]
+print(f'  Videolänge vor dem Schnitt    = {länge_vs//60:.0f}:{länge_vs%60:.0f}')
+print(f'  Videolänge nach dem Schnitt   = {länge_ns//60:.0f}:{länge_ns%60:.0f}')
+print(f'= Einsparung in %               = {(1-länge_ns/länge_vs)*100:.2f}')
+print(f'  Anz. Schnitte                 = {len(schnittliste)}')
+
+clips = [video.subclip(s,e) for s,e in schnittliste]
 video = mvpe.concatenate_videoclips(clips)
-video.write_videofile(ausgabe, fps=30, preset='ultrafast', codec='libx264',
-                      temp_audiofile='temp-audio.m4a', remove_temp=True,
-                      audio_codec="aac", threads=4)
-
-video.close()
+video.write_videofile(ausgabedatei, fps=30, preset='ultrafast', codec='libx264',
+                      temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac', threads=4)
+video.close()                      
