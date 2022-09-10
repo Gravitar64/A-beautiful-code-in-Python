452fde6aeb2f7264ec878a289f01b35ceadf4434
Gravitar64 <gravitar@web.de>
2022-09-10 18:22:56 +0200

    Wegfall Schnittliste

--- Teil_83_Video_Schnitt_Stille.py
+++ Teil_83_Video_Schnitt_Stille.py
@@ -1,5 +1,4 @@
 import moviepy.editor as mvpe
-import itertools as it
 
 
 def lade_datei(datei):
@@ -7,21 +6,11 @@ def lade_datei(datei):
   return mvpe.VideoFileClip(datei), name+'_stille.'+ext
 
 
-def finde_sprache(audio, ab, stille):
-  a = [audio.subclip(i*ab, (i+1)*ab).max_volume() >= stille for i in range(int(audio.end/ab))]
-  b = [(k,len(list(g))*ab) for k,g in it.groupby(a)]
-  schnittliste, start = [], 0
-  for sprache, länge in b:
-    if sprache: 
-      schnittliste.append((start,start+länge))
-    start += länge   
-  return schnittliste        
-
-
+ab, stille = 0.05, 0.03
 video, ausgabedatei = lade_datei('Teil_83_Beispiel.mp4')
-schnittliste = finde_sprache(video.audio, 0.05, 0.03)
-clips = [video.subclip(s,e) for s,e in schnittliste]
+clips = [video.subclip(i*ab, (i+1)*ab) for i in range(int(video.end/ab))
+         if video.subclip(i*ab, (i+1)*ab).audio.max_volume() >= stille]
 video = mvpe.concatenate_videoclips(clips)
 video.write_videofile(ausgabedatei, fps=30, preset='ultrafast', codec='libx264',
                       temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac', threads=4)
-video.close()                      
+video.close()              
