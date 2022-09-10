7cc3aa969e0ff045e0f529210a31d00698ed63e5
Gravitar64 <gravitar@web.de>
2022-09-10 18:01:58 +0200

    ohne zusätzliche ausgabe

--- Teil_83_Video_Schnitt_Stille.py
+++ Teil_83_Video_Schnitt_Stille.py
@@ -8,10 +8,8 @@ def lade_datei(datei):
 
 
 def finde_sprache(audio, ab, stille):
-  a = [audio.subclip(i*ab, (i+1)*ab).max_volume() >= stille
-       for i in range(int(audio.end/ab))]
+  a = [audio.subclip(i*ab, (i+1)*ab).max_volume() >= stille for i in range(int(audio.end/ab))]
   b = [(k,len(list(g))*ab) for k,g in it.groupby(a)]
-  
   schnittliste, start = [], 0
   for sprache, länge in b:
     if sprache: 
@@ -22,14 +20,6 @@ def finde_sprache(audio, ab, stille):
 
 video, ausgabedatei = lade_datei('Teil_83_Beispiel.mp4')
 schnittliste = finde_sprache(video.audio, 0.05, 0.03)
-
-länge_vs, länge_ns = video.end, sum(e-s for s,e in schnittliste)
-
-print(f'  Videolänge vor dem Schnitt    = {länge_vs//60:.0f}:{länge_vs%60:.0f}')
-print(f'  Videolänge nach dem Schnitt   = {länge_ns//60:.0f}:{länge_ns%60:.0f}')
-print(f'= Einsparung in %               = {(1-länge_ns/länge_vs)*100:.2f}')
-print(f'  Anz. Schnitte                 = {len(schnittliste)}')
-
 clips = [video.subclip(s,e) for s,e in schnittliste]
 video = mvpe.concatenate_videoclips(clips)
 video.write_videofile(ausgabedatei, fps=30, preset='ultrafast', codec='libx264',
