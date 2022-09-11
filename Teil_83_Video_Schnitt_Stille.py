740f524378e999011e6108a2f819f57f64d64fb8
Gravitar64 <gravitar@web.de>
2022-09-11 12:01:37 +0200

    3 comprehensions

--- Teil_83_Video_Schnitt_Stille.py
+++ Teil_83_Video_Schnitt_Stille.py
@@ -7,20 +7,16 @@ def lade_video(datei):
   return mvpe.VideoFileClip(datei), name+'_kurz.'+ext
 
 
-def gen_clips(video, ab, stille):
+def gen_video(video, ab, stille):
   audio = video.audio
   a = [audio.subclip(i*ab, (i+1)*ab).max_volume() >= stille for i in range(int(audio.end/ab))]
-  b = [(k,len(list(g))*ab) for k,g in it.groupby(a)]
-  clips, start = [], 0
-  for sprache, länge in b:
-    if sprache: 
-      clips.append(video.subclip(start,start+länge))
-    start += länge   
-  return clips       
+  s,l = zip(*[(k,len(list(g))*ab) for k,g in it.groupby(a)])
+  c = [(sum(l[:i]), sum(l[:i+1])) for i in range(len(l)) if s[i]]
+  return mvpe.concatenate_videoclips([video.subclip(s,e) for s,e in c])    
 
 
 video1, ausgabedatei = lade_video('Teil_83_Beispiel.mp4')
-video2 = mvpe.concatenate_videoclips(gen_clips(video1,0.05,0.03))
+video2 = gen_video(video1, 0.05, 0.03)
 video2.write_videofile(ausgabedatei, fps=30, preset='ultrafast', codec='libx264',
                    temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac', threads=4)
 
