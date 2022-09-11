802892592dade9504968c0e49f08fff2a0502f54
Gravitar64 <gravitar@web.de>
2022-09-11 10:51:07 +0200

    ohne Zeitmessung

--- Teil_83_Video_Schnitt_Stille.py
+++ Teil_83_Video_Schnitt_Stille.py
@@ -1,6 +1,5 @@
 import moviepy.editor as mvpe
 import itertools as it
-from time import perf_counter as pfc
 
 
 def lade_video(datei):
@@ -20,11 +19,9 @@ def gen_clips(video, ab, stille):
   return clips       
 
 
-start = pfc()
 video1, ausgabedatei = lade_video('Teil_83_Beispiel.mp4')
 video2 = mvpe.concatenate_videoclips(gen_clips(video1,0.05,0.03))
 video2.write_videofile(ausgabedatei, fps=30, preset='ultrafast', codec='libx264',
                    temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac', threads=4)
 
 print(f'Länge vorher/nacher = {video1.end:.2f} Sek. / {video2.end:.2f} Sek.')
-print(pfc()-start)
