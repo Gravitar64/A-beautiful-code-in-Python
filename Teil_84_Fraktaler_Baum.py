4470f37c69a12d428924baf3b1adef94e0f9e658
Gravitar64 <gravitar@web.de>
2022-09-18 12:27:15 +0200

    youTube2

--- Teil_84_Fraktaler_Baum.py
+++ Teil_84_Fraktaler_Baum.py
@@ -1,23 +1,21 @@
 import pygame as pg
 import math
 
-
 def zeichne_Baum(x, y, θ, tiefe):
   if not tiefe: return
-  x2 = x + math.cos(θ) * tiefe * länge
-  y2 = y + math.sin(θ) * tiefe * länge
+  x2 = x + math.cos(θ) * länge * tiefe
+  y2 = y + math.sin(θ) * länge * tiefe
   farbe.hsva = (120, tiefe*sv, tiefe*sv)
-  pg.draw.line(fenster, farbe, (x, y), (x2, y2), 1)
-  zeichne_Baum(x2, y2, θ - β, tiefe - 1)
-  zeichne_Baum(x2, y2, θ + β, tiefe - 1)
+  pg.draw.line(fenster, farbe, (x,y), (x2,y2), 1)
+  zeichne_Baum(x2, y2, θ-β, tiefe-1)
+  zeichne_Baum(x2, y2, θ+β, tiefe-1)
 
 
 θ, β, länge, tiefe = math.radians(-90), math.radians(20), 10, 14
-clock, BREITE, HÖHE = pg.time.Clock(), 1920, 1080
-fenster = pg.display.set_mode((BREITE, HÖHE))
-farbe, sv = pg.Color(0), 100/tiefe
-
-zeichne_Baum(BREITE/2, HÖHE, θ, tiefe)
+clock, breite, höhe = pg.time.Clock(), 1920, 1080
+fenster = pg.display.set_mode((breite,höhe))
+farbe, sv = pg.Color(0), int(100/tiefe)
+zeichne_Baum(breite/2,höhe,θ, tiefe)
 pg.display.flip()
 
 while True:
