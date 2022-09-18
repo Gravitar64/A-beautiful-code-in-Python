2d72bbdd96def9f6e41ac0b6b72446deb20c0eec
Gravitar64 <gravitar@web.de>
2022-09-18 11:31:15 +0200

    youTube

--- Teil_84_Fraktaler_Baum.py
+++ Teil_84_Fraktaler_Baum.py
@@ -12,12 +12,12 @@ def zeichne_Baum(x, y, θ, tiefe):
   zeichne_Baum(x2, y2, θ + β, tiefe - 1)
 
 
-β, länge, tiefe = math.radians(20), 5, 14
-clock, BREITE, HÖHE = pg.time.Clock(), 800, 600
+θ, β, länge, tiefe = math.radians(-90), math.radians(20), 10, 14
+clock, BREITE, HÖHE = pg.time.Clock(), 1920, 1080
 fenster = pg.display.set_mode((BREITE, HÖHE))
 farbe, sv = pg.Color(0), 100/tiefe
 
-zeichne_Baum(BREITE/2, HÖHE, math.radians(-90), tiefe)
+zeichne_Baum(BREITE/2, HÖHE, θ, tiefe)
 pg.display.flip()
 
 while True:
