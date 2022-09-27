6e7ad4170afd45c5b70bdc5ef7acba43999feb2d
Gravitar64 <gravitar@web.de>
2022-09-27 12:07:23 +0200

    youTube8

--- Teil_84_Fraktaler_Baum.py
+++ Teil_84_Fraktaler_Baum.py
@@ -5,20 +5,22 @@ from pygame import Vector2 as Vec
 def zeichne_baum(von, 𝜙, verzweigungen):
   if not verzweigungen: return
   zu = Vec(); zu.from_polar((r, 𝜙)); zu = von + zu * verzweigungen
-  farbe.hsva = (120, sv * verzweigungen, sv * verzweigungen)
+  farbe.hsva = (120, sv*verzweigungen, sv*verzweigungen)
   pg.draw.line(fenster, farbe, von, zu, 3)
-  zeichne_baum(zu, 𝜙 + 𝛽, verzweigungen - 1)
-  zeichne_baum(zu, 𝜙 - 𝛽, verzweigungen - 1)
+  zeichne_baum(zu, 𝜙+𝛽, verzweigungen-1)
+  zeichne_baum(zu, 𝜙-𝛽, verzweigungen-1)
 
 
 FENSTER_G = Vec(1920, 1080)
 fenster = pg.display.set_mode(FENSTER_G)
-von, 𝜙, 𝛽, r, verzweigungen = Vec(FENSTER_G.x / 2, FENSTER_G.y), 270, 20, 10, 14
-farbe, sv = pg.Color(0), int(100 / verzweigungen)
 clock = pg.time.Clock()
+
+von, r, 𝜙, 𝛽, verzweigungen = Vec(FENSTER_G.x/2, FENSTER_G.y), 10, 270, 20, 12
+farbe, sv = pg.Color(0), int(100/verzweigungen)
 
 zeichne_baum(von, 𝜙, verzweigungen)
 pg.display.flip()
+
 
 while True:
   clock.tick(5)
