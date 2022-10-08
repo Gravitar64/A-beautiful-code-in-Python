d134224983713869af3e158d61eaa61bb1612bb1
Gravitar64 <gravitar@web.de>
2022-10-08 13:50:37 +0200

    youtube2

--- Teil_85_Mandelbrot_Teil_1.py
+++ Teil_85_Mandelbrot_Teil_1.py
@@ -1,28 +1,32 @@
 import pygame as pg
 
-def zeichne_fractal(x1,x2,y1,y2,max_iter):
+
+def zeichne_fractal(x1, x2, y1, y2, max_iter):
   schritt_x = (x2-x1) / breite
   schritt_y = (y2-y1) / höhe
-  for x in range(breite):
-    for y in range(höhe):
-      c = (x * schritt_x + x1) + (y * schritt_y + y1) * 1j
+  for y in range(höhe):
+    for x in range(breite):
+      c = complex(x1 + x*schritt_x, y1 + y*schritt_y)
       z = 0
       for i in range(max_iter):
         z = z**2 + c
-        if abs(z) > 2: break
+        if abs(z) > 2:
+          break
       farbe = 255 / max_iter * i
-      fenster.set_at((x,y), (farbe, farbe, farbe))
-  pg.display.flip()
+      fenster.set_at((x, y), (farbe, farbe, farbe))
 
 
 pg.init()
-größe = breite, höhe = 1200, 800
+größe = breite, höhe = 800, 600
 fenster = pg.display.set_mode(größe)
 clock = pg.time.Clock()
 
-x1,x2 = -2,1
-y1,y2 = -1,1
+x1, x2 = -2, 1
+y1, y2 = -1, 1
 max_iter = 30
+
+zeichne_fractal(x1, x2, y1, y2, max_iter)
+pg.display.flip()
 
 while True:
   clock.tick()
@@ -30,5 +34,3 @@ while True:
     if ereignis.type == pg.QUIT or \
        ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE:
       quit()
-  
-  zeichne_fractal(x1,x2,y1,y2,max_iter)
