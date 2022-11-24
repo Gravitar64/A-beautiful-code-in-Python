6803ed1c524f1c850a2205995d332cedf1e0f92f
Gravitar64 <gravitar@web.de>
2022-11-24 11:19:24 +0100

    Variablennamen angepasst

--- Teil_86_41_bwinf_j1.py
+++ Teil_86_41_bwinf_j1.py
@@ -2,44 +2,43 @@ import requests
 import collections
 import itertools
 
-def gen_maßgebliche_vokalgruppe(wort):
-  vokalgruppe, vokalgruppen = '', []
+def gen_endung(wort):
+  vokalgruppe, endungen = '', []
   for i,buchstabe in enumerate(wort):
     if buchstabe in VOKALE:
       vokalgruppe += buchstabe
     elif vokalgruppe:
-      vokalgruppen.append(vokalgruppe + wort[i:])
+      endungen.append(vokalgruppe + wort[i:])
       vokalgruppe = ''
   if vokalgruppe:
-    vokalgruppen.append(vokalgruppe)
-  if not vokalgruppen: return None  
-  return vokalgruppen[-2] if len(vokalgruppen) > 1 else vokalgruppen[0] 
+    endungen.append(vokalgruppe)
+  l = len(endungen) 
+  return endungen[-2] if l > 1 else endungen[0] if l == 1 else None 
 
 
 URL = 'https://bwinf.de/fileadmin/bundeswettbewerb/41/reimerei'
 VOKALE = set('aeiouöüä')
 
 for n in range(4):
-  mvg2wörter = collections.defaultdict(list)
+  end2wörter = collections.defaultdict(list)
   datei = requests.get(f'{URL}{n}.txt')
   print(f'Datei Nr. {n} wurde geladen, Return-Code {datei}')
   wörter = [wort.strip().lower() for wort in datei.text.split('\n')]
   
   for wort in wörter:
-    mvg = gen_maßgebliche_vokalgruppe(wort)
-    if not mvg: continue
-    mvg2wörter[mvg].append(wort)
+    endung = gen_endung(wort)
+    if not endung: continue
+    end2wörter[endung].append(wort)
 
   treffer = 0
-  for mvg,einträge in mvg2wörter.items():
-    if len(einträge) == 1: continue
-    for w1,w2 in itertools.combinations(einträge,2):
-      if len(w1) / 2 > len(mvg): continue
-      if len(w2) / 2 > len(mvg): continue
+  for endung,wörter in end2wörter.items():
+    for w1,w2 in itertools.combinations(wörter,2):
+      if len(endung) < len(w1) / 2: continue
+      if len(endung) < len(w2) / 2: continue
       if w1 in w2 or w2 in w1: continue
       print(w1,w2)
       treffer += 1
-  print(f'{treffer} gefundene Wortpaare')
+  print(f'Anz. Wortpaare = {treffer}')
   print()    
 
 
