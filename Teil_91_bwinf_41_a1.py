c4fa31f8bdda3beac8c83a82b347c81115c36be6
Gravitar64 <gravitar@web.de>
2023-05-14 22:58:19 +0200

    youTube

--- Teil_91_bwinf_41_a1.py
+++ Teil_91_bwinf_41_a1.py
@@ -1,11 +1,13 @@
 import requests
 import re
 
-ALICE = requests.get(
-    "https://bwinf.de/fileadmin/bundeswettbewerb/41/Alice_im_Wunderland.txt").text.lower()
+
+URL_TEXT    = "https://bwinf.de/fileadmin/bundeswettbewerb/41/Alice_im_Wunderland.txt"
+URL_AUFGABE = "https://bwinf.de/fileadmin/bundeswettbewerb/41/stoerung"
+
+ALICE = requests.get(URL_TEXT).text.lower()
 
 for i in range(6):
-  lückentext = requests.get(
-      f"https://bwinf.de/fileadmin/bundeswettbewerb/41/stoerung{i}.txt").text
+  lückentext = requests.get(f'{URL_AUFGABE}{i}.txt').text
   suchmuster = lückentext.replace('_', '\w+')
   print(lückentext, re.findall(suchmuster, ALICE))
