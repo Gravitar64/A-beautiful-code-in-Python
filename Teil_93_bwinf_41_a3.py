926e4e66d393f8a27130f4b3ccf183226a7cf793
Gravitar64 <gravitar@web.de>
2023-06-01 22:39:48 +0200

    V5

--- Teil_93_bwinf_41_a3.py
+++ Teil_93_bwinf_41_a3.py
@@ -4,48 +4,49 @@ import requests
 import time
 
 
-def mut_zeilen(s, p):
-  s1 = s.copy()
-  for zei_von, zei_zu in enumerate(p):
-    if zei_von == zei_zu: continue
-    for sp in range(9):
-      s1[zei_zu*9+sp] = s[zei_von*9+sp]
-  return s1
+def mut_zeilen(p):
+  return {a*9+i:b*9+i for a,b in enumerate(p) for i in range(9) if a != b}
 
 
-def mut_spalten(s, p):
-  s1 = s.copy()
-  for spa_von, spa_zu in enumerate(p):
-    if spa_von == spa_zu: continue
-    for ze in range(9):
-      s1[ze*9+spa_zu] = s[ze*9+spa_von]
-  return s1
+def mut_spalten(p):
+  return {i*9+a:i*9+b for a,b in enumerate(p) for i in range(9) if a != b}
+  
 
+def mut_blockspalte(p):
+  return {i//3*9+i%3+a*3:i//3*9+i%3+b*3 for a,b in enumerate(p)
+          for i in range(27)}
+  
 
-def mut_blockspalte(s, p):
-  s1 = s.copy()
-  for bsp_von, bsp_zu in enumerate(p):
-    if bsp_von == bsp_zu: continue
-    for i in range(27):
-      s1[i // 3 * 9 + i%3 + (bsp_zu * 3)] = s[i // 3 * 9 + i%3 + (bsp_von * 3)]
-  return s1
+def mut_blockzeile(p):
+  return {i%9+i//9*9+a*3*9:i%9+i//9*9+b*3*9 for a,b in enumerate(p)
+          for i in range(27)}
 
-
-def mut_blockzeile(s, p):
-  s1 = s.copy()
-  for bze_von, bze_zu in enumerate(p):
-    if bze_von == bze_zu: continue
-    for i in range(27):
-      s1[i%9 + i // 9 * 9 + bze_zu * 3 * 9] = s[i%9 + i // 9 * 9 + bze_von * 3 * 9]
-  return s1
-
-
+  
 def rotiere(s):
   ret = s.copy()
   for ze, sp in perm_rotation:
     ret[ze*9+sp] = s[(9-sp-1)*9+ze]
   return ret
 
+
+def mutate(p):
+  ret = s.copy()
+  for von,zu in p.items():
+    ret[zu] = s[von]
+  return ret  
+  
+
+def konsolidiere(k1,k2):
+  ret = {}
+  for von,zu in k1.items():
+    if zu in k2:
+      ret[von] = k2[zu]
+      del k2[zu]
+    else:
+      ret[von] = zu
+  ret.update(k2)
+  return ret    
+        
 
 def permutation_möglich():
   def verteilung_zahlen(s): return set(s.count(str(i)) for i in range(1, 10))
@@ -54,16 +55,20 @@ def permutation_möglich():
 
 def prüfe_permutationen():
   for p1 in perm_blöcke:
-    s1 = mut_blockspalte(s, p1)
+    s1 = mut_blockspalte(p1)
     for p2 in perm_blöcke:
-      s2 = mut_blockzeile(s1, p2)
+      s2 = mut_blockzeile(p2)
+      k1 = konsolidiere(s1,s2)
       for p3 in perm_zeilen:
-        s3 = mut_spalten(s2, p3)
+        s3 = mut_spalten(p3)
+        k2 = konsolidiere(k1,s3)
         for p4 in perm_zeilen:
-          s4 = mut_zeilen(s3, p4)
+          s4 = mut_zeilen(p4)
+          k3 = konsolidiere(k2,s4)
+          buffer = mutate(k3)
           for rot in [False, True]:
-            s5 = rotiere(s4) if rot else s4
-            if s5 == e or (mapping := umbenennen(s5, e)):
+            buffer = rotiere(buffer) if rot else buffer
+            if buffer == e or (mapping := umbenennen(buffer, e)):
               return p1, p2, p3, p4, rot, mapping
 
 
