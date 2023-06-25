5bc04cf5c6e73f3bfdf3093d6076ca1107f96d59
Gravitar64 <gravitar@web.de>
2023-06-25 13:29:52 +0200

    YouTube

---
+++ Teil_92_Nathans Problem.py
@@ -0,0 +1,16 @@
+import random as rnd
+
+anz_karten = 55
+karten = [1]*anz_karten
+
+zähler = 0
+while any(karten):
+  einsen = [i for i,k in enumerate(karten) if k == 1]
+  i = rnd.choice(einsen)
+  karten[i] = 0
+  if i < anz_karten-1:
+    karten[i+1] = 0 if karten[i+1] == 1 else 1
+  zähler += 1
+  print(f'{zähler:<3} {"".join(map(str,karten))}')  
+    
+  
