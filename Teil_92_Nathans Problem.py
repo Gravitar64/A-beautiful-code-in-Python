15d5145fb93ba45a639d00df8eadebdc1c606364
Gravitar64 <gravitar@web.de>
2023-06-25 13:31:08 +0200

    Update Teil_92_Nathans Problem.py

--- Teil_92_Nathans Problem.py
+++ Teil_92_Nathans Problem.py
@@ -1,6 +1,6 @@
 import random as rnd
 
-anz_karten = 55
+anz_karten = 20
 karten = [1]*anz_karten
 
 zähler = 0
@@ -11,6 +11,4 @@ while any(karten):
   if i < anz_karten-1:
     karten[i+1] = 0 if karten[i+1] == 1 else 1
   zähler += 1
-  print(f'{zähler:<3} {"".join(map(str,karten))}')  
-    
-  
+  print(f'{zähler:<3} {"".join(map(str,karten))}') 
