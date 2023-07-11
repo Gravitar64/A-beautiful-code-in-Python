766b63edbc701bdca0dc81591db20eda560e7020
Gravitar64 <gravitar@web.de>
2023-07-11 15:50:59 +0200

    V2

--- Teil_93_Pig.py
+++ Teil_93_Pig.py
@@ -12,16 +12,9 @@ def würfeln(i, p):
 
 
 def rangliste(punkte):
-  tabelle = sorted([(p, i+1) for i, p in enumerate(punkte)], reverse=True)
-  platz, gleich, vorherige = 0, 1, 999
+  tabelle = sorted([(-p, s) for s, p in enumerate(punkte,start=1)])
   for p, s in tabelle:
-    if p == vorherige:
-      gleich += 1
-    else:
-      platz += gleich
-      gleich = 1
-    print(f'Platz {platz}: Spieler {s} mit {p} Punkten')
-    vorherige = p
+    print(f'Spieler {s} mit {-p} Punkten')
   print()    
 
 
@@ -29,7 +22,6 @@ while True:
   anz_spieler = input('Wieviele Spieler (2-6)? ')
   if not anz_spieler.isdigit(): continue
   if 1 < (spieler := int(anz_spieler)) < 7: break
-
 
 punkte = [0]*spieler
 while max(punkte) < 50:
