3ebdbf32a0520076207bd4c16ae1d061863701be
Gravitar64 <gravitar@web.de>
2023-07-18 00:52:55 +0200

    V3

--- Teil_94_Mathe_übung.py
+++ Teil_94_Mathe_übung.py
@@ -2,22 +2,28 @@ import random
 import time
 import readchar
 
-input('Spiel starten?')
-punkte = 0
-start = time.perf_counter()
-for _ in range(10):
-  a,b,op = random.randint(1,20), random.randint(1,20), random.choice('+ - * /'.split())
-  e = eval(f'{a} {op} {b}')
-  falsch = random.random() > 0.5
-  if falsch: e += random.randint(1,10) if random.random() > 0.5 else -random.randint(1,10)
-  print(f'{a} {op} {b} = {e}')
-  print('Richtig/Falsch (r/<any key>)? ', end='')
-  antwort = readchar.readchar()
-  if antwort == 'r' and not falsch or antwort != 'r' and falsch: 
-    punkte += 1
-    print(f'{antwort} = KORREKT')
-  else:
-    print(f'{antwort} = FALSCH')  
 
-print(punkte)
-print(time.perf_counter()-start+20*(10-punkte))
+def fifty():
+  return random.choice([True, False])
+
+
+def r(s,e):
+ return random.randint(s,e)
+
+
+readchar.readchar()
+fehler = 10
+start = time.perf_counter()
+
+for _ in range(10):
+  a,b,op, delta = r(1,10), r(1,10), random.choice('+ - *'.split()), r(1,5)
+  e = eval(f'{a} {op} {b}') 
+  if falsch := fifty(): e += delta if fifty() else -delta
+  print(f'{a} {op} {b} = {e} ')
+  antwort = readchar.readchar()
+  if antwort == 'r' and falsch or antwort != 'r' and not falsch: continue 
+  fehler -= 1
+   
+
+print(f'{fehler} Fehler')
+print(time.perf_counter()-start+20*fehler)
