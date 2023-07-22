041cf637382bdbf2176f172630797cd5bc42038f
Gravitar64 <gravitar@web.de>
2023-07-23 01:20:39 +0200

    V4

--- Teil_95_Magische_Quadrate.py
+++ Teil_95_Magische_Quadrate.py
@@ -2,48 +2,47 @@ import itertools
 import time
 
 
-def print_board(board):
-  for z in range(n):
-    print()
-    for s in range(n):
-      print(f'{board[z][s]:>2} ', end='')
-  print()
-
-
 def check(board):
-  for s in range(n):
-    if sum(board[z][s] for z in range(n)) != summe:
-      return False
-  if sum(board[i][i] for i in range(n)) != summe:
-    return False
-  if sum(board[i][n-i-1] for i in range(n)) != summe:
-    return False
+  for z in range(1, n):
+    if sum(board[z*n:z*n+n]) != summe: return False
+  if sum(board[i*n+i] for i in range(n)) != summe: return False
+  if sum(board[i*n+n-i-1] for i in range(n)) != summe: return False
   return True
 
 
-def löse(zahlen, board):
-  if not zahlen and check(board):
-    print_board(board)
-    print(time.perf_counter()-start)
-    lösungen.append(board)
-  for zeile in itertools.permutations(zahlen, n-1):
-    letzte = summe - sum(zeile)
-    zahlen2 = zahlen - set(zeile)
-    if letzte < 1 or letzte not in zahlen2: continue
-    zahlen2.remove(letzte)
-    zeile = list(zeile)+[letzte]
-    löse(zahlen2, board+[zeile])
-  return
+def rows(nr, zahlen, board):
+  for row in itertools.permutations(zahlen, n-1):
+    rest = summe-sum(row)
+    zahlen2 = zahlen-set(row)
+    if rest < 1 or rest not in zahlen2: continue
+    board[nr*n:nr*n+n] = list(row)+[rest]
+    cols(nr, zahlen2-{rest}, board)
+
+
+def cols(nr, zahlen, board):
+  if not zahlen:
+    if check(board):
+      print(f'{board} {time.perf_counter()-start:.2f}')
+      lösungen.append(board)
+    return
+  for col in itertools.permutations(zahlen, n-2):
+    rest = summe-sum(col)-board[nr]
+    zahlen2 = zahlen-set(col)
+    if rest < 1 or rest not in zahlen2: continue
+    for i, z in enumerate(list(col)+[rest], start=1):
+      board[i*n+nr] = z
+    cols(nr+1, zahlen2-{rest}, board)
 
 
 n = 4
 start = time.perf_counter()
 zahlen = set(range(1, n**2+1))
+board = [0]*n*n
 summe = (n**3+n)//2
 lösungen = []
 
 print(f'Magisches Quadrat mit Kantenlänge = {n} und {summe} als Summe')
 
-löse(zahlen, [])
+rows(0, zahlen, board)
 print(f'Anzahl Lösungen = {len(lösungen):,}')
 print(time.perf_counter()-start)
