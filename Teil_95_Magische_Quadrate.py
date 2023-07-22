974636c57b4e868fb3edfd26961f1e693a309c49
Gravitar64 <gravitar@web.de>
2023-07-22 18:47:19 +0200

    V3

--- Teil_95_Magische_Quadrate.py
+++ Teil_95_Magische_Quadrate.py
@@ -1,21 +1,49 @@
-n = 9
+import itertools
+import time
+
+
+def print_board(board):
+  for z in range(n):
+    print()
+    for s in range(n):
+      print(f'{board[z][s]:>2} ', end='')
+  print()
+
+
+def check(board):
+  for s in range(n):
+    if sum(board[z][s] for z in range(n)) != summe:
+      return False
+  if sum(board[i][i] for i in range(n)) != summe:
+    return False
+  if sum(board[i][n-i-1] for i in range(n)) != summe:
+    return False
+  return True
+
+
+def löse(zahlen, board):
+  if not zahlen and check(board):
+    print_board(board)
+    print(time.perf_counter()-start)
+    lösungen.append(board)
+  for zeile in itertools.permutations(zahlen, n-1):
+    letzte = summe - sum(zeile)
+    zahlen2 = zahlen - set(zeile)
+    if letzte < 1 or letzte not in zahlen2: continue
+    zahlen2.remove(letzte)
+    zeile = list(zeile)+[letzte]
+    löse(zahlen2, board+[zeile])
+  return
+
+
+n = 4
+start = time.perf_counter()
+zahlen = set(range(1, n**2+1))
 summe = (n**3+n)//2
+lösungen = []
 
 print(f'Magisches Quadrat mit Kantenlänge = {n} und {summe} als Summe')
 
-quadrat = [[0]*n for _ in range(n)]
-z, s = 0, n//2
-for i in range(1, n**2+1):
-  quadrat[z][s] = i
-  z2, s2 = z-1, s+1
-  if z2 < 0: z2 += n
-  if s2 == n: s2 = 0
-  if quadrat[z2][s2]:
-    z += 1
-  else:
-    z, s = z2, s2
-
-for z in quadrat:
-  for n in z:
-    print(f'{n:<2} ', end='')
-  print()
+löse(zahlen, [])
+print(f'Anzahl Lösungen = {len(lösungen):,}')
+print(time.perf_counter()-start)
