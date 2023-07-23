a949135f01ab6d18cd74a8179ef1720569cabecc
Gravitar64 <gravitar@web.de>
2023-07-23 19:51:23 +0200

    V6 Finds all Solutions 4x4 in 108 Sek.

--- Teil_95_Magische_Quadrate.py
+++ Teil_95_Magische_Quadrate.py
@@ -3,42 +3,40 @@ import itertools,time
 
 def check(quadrat):
   if sum(quadrat[i*N+i] for i in range(N)) != summe: return False
-  if sum(quadrat[i*N-i-1] for i in range(N)) != summe: return False
+  if sum(quadrat[i*N+N-i-1] for i in range(N)) != summe: return False
   return True
 
 
-def lösungsmenge():
-  ls = []
-  for l in itertools.combinations(range(1,N**2+1),N):
-    if sum(l) != summe: continue
-    ls.append(set(l))
-  return ls  
-    
+def zeilen(nr, zahlen, quadrat):
+  bisherige_summe = sum(quadrat[nr*N:nr*N+nr])
+  for z in itertools.permutations(zahlen,N-1-nr):
+    rest = summe - bisherige_summe - sum(z)
+    zahlen2 = zahlen - set(z)
+    if rest not in zahlen2: continue
+    quadrat[nr*N+nr:nr*N+N] = list(z)+[rest]
+    spalten(nr,zahlen2-{rest},quadrat)
 
-def zeilen(nr, ls, quadrat):
-  if nr == N-1:
-    füllen(ls,quadrat)
+
+def spalten(nr, zahlen, quadrat):
+  if not zahlen:
+    if check(quadrat):
+      lösungen.append(quadrat)
+      #print(f'{len(lösungen)} {quadrat} {time.perf_counter()-start:.2f}')
     return
   
-  for z in ls:
-    l = [x for x in ls if not x&z]
-    for p in itertools.permutations(z):
-      quadrat[N*nr:N*nr+N] = p
-      zeilen(nr+1, l, quadrat)
-
-
-def füllen(ls,quadrat):
-  letzte_zeile = [summe - sum(quadrat[z*N+s] for z in range(N-1)) for s in range(N)]
-  if set(letzte_zeile) not in ls: return
-  quadrat[N*(N-1):N*N] = letzte_zeile
-  if check(quadrat): 
-    lösungen.append(quadrat)
-    print(f'{len(lösungen)} {quadrat} {time.perf_counter()-start:.2f}')
+  bisherige_summe = sum(quadrat[i*N+nr] for i in range(nr+1))
+  for z in itertools.permutations(zahlen,N-2-nr):
+    rest = summe - bisherige_summe - sum(z)
+    zahlen2 = zahlen - set(z)
+    if rest not in zahlen2: continue
+    for i,zahl in enumerate(list(z)+[rest],start=nr+1):
+      quadrat[i*N+nr]=zahl
+    zeilen(nr+1,zahlen2-{rest},quadrat)    
+  
 
 start = time.perf_counter()
 N = 4
 summe = (N**3+N)//2
-ls = lösungsmenge()
 lösungen = []
-zeilen(0,ls,[0]*N*N)
+zeilen(0,set(range(1,N**2+1)),[0]*N**2)
 print(f'{len(lösungen):,} Lösungen für {N}x{N} magische Quadrate mit der Summe {summe} ({time.perf_counter()-start:.2f})')
