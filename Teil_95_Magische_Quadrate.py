3484ef0f02805076bead9a86882731b99fd2501a
Gravitar64 <gravitar@web.de>
2023-07-25 13:18:38 +0200

    findet jetzt auch perfekte 4x4

--- Teil_95_Magische_Quadrate.py
+++ Teil_95_Magische_Quadrate.py
@@ -12,6 +12,17 @@ def print_quadrat(quadrat):
 def check(quadrat):
   if sum(quadrat[i*N+i] for i in range(N)) != summe: return False
   if sum(quadrat[i*N+N-i-1] for i in range(N)) != summe: return False
+  
+  if N == 4 and perfect:
+    if sum(quadrat[0:2]+quadrat[4:6]) != summe: return False
+    if sum(quadrat[2:4]+quadrat[6:8]) != summe: return False
+    if sum(quadrat[8:10]+quadrat[12:14]) != summe: return False
+    if sum(quadrat[10:12]+quadrat[14:16]) != summe: return False
+    if sum(quadrat[5:7]+quadrat[9:11]) != summe: return False
+    if sum(quadrat[x] for x in (0,3,12,15)) != summe: return False
+    if sum(quadrat[x] for x in (1,7,8,14)) != summe: return False
+    if sum(quadrat[x] for x in (2,4,11,13)) != summe: return False
+  
   return True
 
 
@@ -28,7 +39,7 @@ def zeilen(nr, zahlen, quadrat):
 def spalten(nr, zahlen, quadrat):
   if not zahlen:
     if check(quadrat):
-      lösungen.append(quadrat)
+      lösungen.append(quadrat.copy())
       print_quadrat(quadrat)
     return
   
@@ -44,7 +55,14 @@ def spalten(nr, zahlen, quadrat):
 
 start = time.perf_counter()
 N = 4
+perfect = True
 summe = (N**3+N)//2
 lösungen = []
 zeilen(0,set(range(1,N**2+1)),[0]*N**2)
 print(f'{len(lösungen):,} Lösungen für {N}x{N} magische Quadrate mit der Summe {summe} ({time.perf_counter()-start:.2f})')
+
+if N==4 and perfect:
+  print('Dürer magische Quadrate mit 15 14 in der untersten Zeile')
+  for quadrat in lösungen:
+    if quadrat[13] != 15 or quadrat[14] != 14: continue
+    print_quadrat(quadrat) 
