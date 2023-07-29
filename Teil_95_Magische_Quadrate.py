f76db9f5464b9051145cdb5a079f3e1dc2d7c7ad
Gravitar64 <gravitar@web.de>
2023-07-29 16:58:03 +0200

    Lösungszeit 4x4 21 Sek.

--- Teil_95_Magische_Quadrate.py
+++ Teil_95_Magische_Quadrate.py
@@ -13,12 +13,6 @@ def print_quadrat():
   print('\n') 
 
 
-def check():
-  if sum(quadrat[i*N+i] for i in range(N)) != summe: return False
-  if sum(quadrat[i*N+N-i-1] for i in range(N)) != summe: return False
-  return True
-
-
 def zeilen(nr,zahlen):
   base = summe - sum(quadrat[nr*N:nr*N+nr])
   for p in itertools.permutations(zahlen,N-nr-1):
@@ -26,12 +20,14 @@ def zeilen(nr,zahlen):
     zahlen2 = zahlen - set(p)
     if rest not in zahlen2: continue
     quadrat[nr*N+nr:nr*N+N] = list(p)+[rest]
+    if N-2 == nr and sum(quadrat[i*N+N-i-1] for i in range(N)) != summe: return
     spalten(nr,zahlen2-{rest})
 
 
 def spalten(nr, zahlen):
   if not zahlen:
-    if check(): print_quadrat()
+    if sum(quadrat[i*N+i] for i in range(N)) == summe:
+      print_quadrat()
     return  
   
   base = summe - sum(quadrat[i*N+nr] for i in range(nr+1))
