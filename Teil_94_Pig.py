0a15c2dbd00ed45deb4da06f3760cf851165cad0
Gravitar64 <gravitar@web.de>
2023-08-03 19:50:43 +0200

    YouTube2

--- Teil_94_Pig.py
+++ Teil_94_Pig.py
@@ -1,26 +1,26 @@
 import random
 
 
-def würfeln(sp,pkt,zug=0):
+def würfel(s,p):
+  zug = 0
   while True:
-    wurf = random.randint(1, 6)
+    wurf = random.randint(1,6)
     zug = zug+wurf if wurf > 1 else 0
-    print(f'Spieler {sp}: {wurf} gewürfelt, Punkte Zug = {zug:<2}, Punkte Gesamt = {pkt+zug} (Max = {max(punkte)})')
-    if zug and input('Weiterwürfeln im Zug? (j/n) ').lower() == 'j': continue
+    print(f'Spieler {s+1} hat {wurf} gewürfelt, Punkte Zug = {zug}, Gesamt = {p+zug} (Max = {max(punkte)})')
+    if zug and input('Weiterwürfeln (j/n)? ') == 'j': continue
     return zug
-
+    
 
 def rangliste(punkte):
-  for p, s in sorted([(-p, s) for s, p in enumerate(punkte,start=1)]):
-    print(f'Spieler {s} mit {-p} Punkten')
-  print()    
+  for p,s in sorted([(p,s) for s,p in enumerate(punkte, start=1)], reverse=True):
+    print(f'Spieler {s} mit {p} Punkten')
+  print()
+    
 
-
-MA_SP, MA_PKT = 9,50
-anz_spieler = int(input(f'Wieviele Spieler (2-{MA_SP}) '))
+anz_spieler = int(input("Wieviele Spieler (2-9)? "))
 punkte = [0]*anz_spieler
 
-while max(punkte) < MA_PKT:
-  for i, p in enumerate(punkte):
-    punkte[i] += würfeln(i+1, p)
+while max(punkte) < 100:
+  for s,p in enumerate(punkte):
+    punkte[s] += würfel(s,p)
     rangliste(punkte)
