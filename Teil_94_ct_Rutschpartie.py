897e00db10fce7b17377dbd97ae3b254583c40ab
Gravitar64 <gravitar@web.de>
2023-11-14 21:06:06 +0100

    YouTube

--- Teil_94_ct_Rutschpartie.py
+++ Teil_94_ct_Rutschpartie.py
@@ -30,6 +30,7 @@ def gen_graph(labyrinth):
   return g
 
 
+
 start = t1()
 labyrinth = [z[:20] for z in open('Teil_94_Labyrinth.txt')]
 richtungen = {(-1, 0): 'L', (1, 0): 'R', (0, 1): 'D', (0, -1): 'U'}
