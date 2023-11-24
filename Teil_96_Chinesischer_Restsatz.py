326df4aa15c87879aa4fc1201532bf2936a2d826
Gravitar64 <gravitar@web.de>
2023-11-24 14:28:07 +0100

    YouTube

--- Teil_96_Chinesischer_Restsatz.py
+++ Teil_96_Chinesischer_Restsatz.py
@@ -1,20 +1,19 @@
 import math
-
 import time
 
+
 def chinesischer_restsatz(bedingungen):
-  x=0
+  x = 0
   gesamt_produkt = math.prod(e[1] for e in bedingungen)
   for rest, teiler in bedingungen:
     mul = 1
     produkt = gesamt_produkt // teiler
-    while (mul*produkt)%teiler != rest: mul += 1
-    x += mul*produkt
-  return x % gesamt_produkt  
-
+    while (mul * produkt) % teiler != rest: mul += 1
+    x += mul * produkt
+  return x % gesamt_produkt
 
 
 start = time.perf_counter()
-bedingungen = [(1,3), (0,7), (1,5)]
+bedingungen = [(2, 3), (3, 5), (2, 7)]
 print(chinesischer_restsatz(bedingungen))
 print(f'Ermittelt in {time.perf_counter()-start:.5f} Sek.')
