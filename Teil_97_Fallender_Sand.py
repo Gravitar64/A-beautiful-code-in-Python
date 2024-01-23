48205b64b60687b17afacf7e44a1cc8f46c191d4
Gravitar64 <gravitar@web.de>
2024-01-23 19:39:13 +0100

    YouTube

--- Teil_97_Fallender_Sand.py
+++ Teil_97_Fallender_Sand.py
@@ -1,52 +1,55 @@
-import pygame, random
+import pygame as pg
+import random as rnd
 
 
-def particle_update(particles):
-  particles2 = dict()
-  for (x,y),farbe in particles.items():
-    a,b,c = [(x+dx,y+1) for dx in range(-1,2)]
-    if b not in particles:
-      x,y = b
-    elif a not in particles and c not in particles:
-      x,y = x+random.choice([-1,1]),y+1
-    elif a not in particles:
-      x,y = a
-    elif c not in particles:
-      x,y = c
-    particles2[x,min(höhe//skalierung-1,y)]=farbe  
-  return particles2    
+def update(sandkörner):
+   sandkörner2 = dict()
+   for (x,y), farbe in sandkörner.items():
+      a,b,c = [(x-1,y+1), (x,y+1), (x+1,y+1)]
+      if b not in sandkörner:
+         x,y = b
+      elif a not in sandkörner and c not in sandkörner:
+         x,y = rnd.choice([a,c])
+      elif a not in sandkörner:
+         x,y = a
+      elif c not in sandkörner:
+         x,y = c
+      sandkörner2[x,min(höhe//skalierung,y)]=farbe
+   return sandkörner2      
 
 
-pygame.init()
-größe = breite, höhe = 1920, 1080
-fenster = pygame.display.set_mode(größe)
-clock = pygame.time.Clock()
+
+pg.init()
+größe = breite, höhe = 1920,1080
+fenster = pg.display.set_mode(größe)
+clock = pg.time.Clock()
 FPS = 80
-hue = 200
 
-particles = dict()
-skalierung=10
+sandkörner = dict()
+hue = 0
+skalierung = 5
 
 # Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
-  for ereignis in pygame.event.get():
-    if ereignis.type == pygame.QUIT or \
-       ereignis.type == pygame.KEYDOWN and ereignis.key == pygame.K_ESCAPE:
-      quit()
+    fenster.fill('black')
+    clock.tick(FPS)
+    
+    for ereignis in pg.event.get():
+        if ereignis.type == pg.QUIT or \
+           ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE:
+          quit()
 
-  clock.tick(FPS)
-  fenster.fill('black')
-        
-  if pygame.mouse.get_pressed()[0]:
-    farbe = pygame.Color(0)
-    hue = (hue+1)%360
-    farbe.hsva = hue,50,90
-    x,y = pygame.mouse.get_pos()
-    particles[x//skalierung, y//skalierung]=farbe
-  
-  particles = particle_update(particles)
-  
-  for (x,y), color in particles.items():
-    pygame.draw.rect(fenster,color,(x*skalierung,y*skalierung,skalierung,skalierung))
+    if pg.mouse.get_pressed()[0]:
+       farbe = pg.Color(0)
+       hue = (hue + 1) % 360
+       farbe.hsva = hue, 100, 100
+       x,y = pg.mouse.get_pos()
+       sandkörner[x//skalierung,y//skalierung]=farbe
 
-  pygame.display.flip()
+    
+    for (x,y), farbe in sandkörner.items():
+       pg.draw.rect(fenster, farbe, (x*skalierung,y*skalierung,skalierung,skalierung))
+    
+    sandkörner = update(sandkörner)
+    
+    pg.display.flip()
