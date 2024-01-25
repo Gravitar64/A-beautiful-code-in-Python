34cb58fe708a0e035b454815964b57128c2276f9
Gravitar64 <gravitar@web.de>
2024-01-25 17:38:55 +0100

    V2 Set

--- Teil_98_Wolfram_Automata.py
+++ Teil_98_Wolfram_Automata.py
@@ -2,55 +2,53 @@ import pygame as pg
 import time
 
 
-def next_generation(gen1,rules):
-  gen2 = []
-  for i in range(len(gen1)):
-    bits = [gen1[i-1], gen1[i], gen1[(i+1) % len(gen1)]]
-    i = int(''.join(str(n) for n in bits),2)
-    gen2.append(rules[i])
+def get_next_rule():
+  rule = -1
+  while True:
+    rule = (rule + 1) % 256 
+    yield rule, f'{rule:08b}'[::-1]
+    
+
+def next_generation(gen,rules):
+  gen2 = set()
+  for x in range(breite//skalierung):
+    bits = [x-1 in gen, x in gen, x+1 in gen]
+    i = int(''.join(str(int(n)) for n in bits),2)
+    if rules[i]=='1': gen2.add(x)
   return gen2
 
 
 pg.init()
-größe = breite, höhe = 1920, 1080
+größe = breite, höhe = 800,640
 fenster = pg.display.set_mode(größe)
-clock = pg.time.Clock()
-FPS = 80
+
 
 skalierung = 10
-gen1 = [0]*(breite//skalierung)
-gen1[len(gen1)//2] = 1
-rule = 0
-rule_bits = f'{rule:08b}'[::-1]
+gen = {breite//skalierung//2}
+gnr = get_next_rule()
+rule, bits = next(gnr)
 y=0
 
 
 # Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
-  clock.tick(FPS)
-
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or \
        ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE:
       quit()
 
-  if y > höhe//skalierung:
-    rule = (rule + 1) % 256
-    rule_bits = f'{rule:08b}'[::-1]
-    y=0
-    gen1 = [0]*(breite//skalierung)
-    gen1[len(gen1)//2] = 1
-    fenster.fill('black')
-    time.sleep(1) 
-    pg.display.set_caption(f'Elementary Cellular Automaton (rule {rule})')
-
-
-  for x,n in enumerate(gen1):
-    if n != '0': continue
-    pg.draw.rect(fenster, 'white', (x * skalierung, y*skalierung, skalierung, skalierung))
+  for x in gen:
+    pg.draw.rect(fenster, 'black', (x * skalierung, y*skalierung, skalierung, skalierung))
 
   y+=1
-  gen1 = next_generation(gen1,rule_bits)
+  gen = next_generation(gen,bits)
   
-  pg.display.flip()
-
+  
+  if y > höhe//skalierung:
+    pg.display.set_caption(f'Elementary Cellular Automaton (rule {rule})')
+    pg.display.flip()
+    rule, bits = next(gnr)
+    y=0
+    gen = {breite//skalierung//2}
+    fenster.fill('white')
+    time.sleep(1)
