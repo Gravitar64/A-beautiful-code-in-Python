d7d2b778dea9bcba4f47d3d2886a05cefadf043d
Gravitar64 <gravitar@web.de>
2024-01-27 17:03:24 +0100

    YouTube4

--- Teil_98_Wolfram_Automata.py
+++ Teil_98_Wolfram_Automata.py
@@ -2,14 +2,14 @@ import pygame as pg, time
 
 
 def nächste_generation(generation, regel):
-  next_gen = set()
+  gen2 = set()
   regel = f'{regel:08b}'[::-1]
-  for x in range(max_x):
-    bits = ['1' if pos % max_x in generation else '0' for pos in (x-1, x, x+1)]
-    i = int(''.join(bits),2)
-    if regel[i] != '1': continue
-    next_gen.add(x)
-  return next_gen
+  for spalte in range(max_spalten):
+    bits = ['1' if pos % max_spalten in generation else '0' for pos in [spalte-1, spalte, spalte+1]]
+    index = int(''.join(bits),2)
+    if regel[index] == '0': continue
+    gen2.add(spalte)
+  return gen2
 
 
 pg.init()
@@ -18,26 +18,24 @@ fenster = pg.display.set_mode(größe)
 fenster.fill('white')
 
 skalierung = 8
-max_x, max_y = breite//skalierung, höhe//skalierung
-generation={max_x//2}
-y = regel = 0
+max_spalten, max_zeilen = breite//skalierung, höhe//skalierung
+generation = {max_spalten//2}
+regel = zeile = 0
 
-# Zeichenschleife mit FPS Bildern pro Sekunde
 while True:
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or \
         ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE:
       quit()
-
-  for x in generation:
-    pg.draw.rect(fenster, 'black', (x*skalierung, y*skalierung, skalierung, skalierung))
+  for spalte in generation:
+    pg.draw.rect(fenster,'black',(spalte*skalierung, zeile*skalierung, skalierung, skalierung))
 
   generation = nächste_generation(generation, regel)
-  y += 1
+  zeile += 1
 
-  if y > max_y:
-    pg.display.set_caption(f'1-Dimenstionaler Zellulärer Automat (Regel = {regel})')
+  if zeile > max_zeilen:
+    pg.display.set_caption(f'Regel: {regel}')
     pg.display.flip()
+    time.sleep(1)
     fenster.fill('white')
-    #time.sleep(1)
-    regel, y, generation = (regel + 1) % 256, 0, {max_x//2}
+    zeile, generation, regel = 0, {max_spalten//2}, regel+1
