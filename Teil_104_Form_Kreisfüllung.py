17db95d2934d7231b39d2c41f84a66411a07f073
Gravitar64 <gravitar@web.de>
2024-05-26 11:10:14 +0200

    YouTube 2

--- Teil_104_Form_Kreisfüllung.py
+++ Teil_104_Form_Kreisfüllung.py
@@ -3,11 +3,12 @@ import pygame as pg, random as rnd
 pg.init()
 größe = breite, höhe = 1920, 1080
 fenster = pg.display.set_mode(größe)
-bild = pg.image.load('Teil_105_Gravitar.png')
+
+bild = pg.image.load('Teil_104_Gravitar.png')
 maske = [(x, y) for x in range(breite) for y in range(höhe) if bild.get_at((x, y))[0]]
 rnd.shuffle(maske)
 
-kreise = []
+gezeichnete_kreise = []
 while maske:
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
@@ -15,11 +16,13 @@ while maske:
   pos = pg.Vector2(maske.pop())
   rad = rnd.randrange(5, 20)
 
-  for pos2, rad2 in kreise:
+  for pos2, rad2 in gezeichnete_kreise:
     if pos.distance_to(pos2) - rad - rad2 < 0: break
   else:
-    kreise.append((pos, rad))
     pg.draw.circle(fenster, 'green', pos, rad, 3)
+    gezeichnete_kreise.append((pos, rad))
     pg.display.flip()
 
-pg.image.save(fenster, 'Teil_105_kreise.png')
+pg.image.save(fenster,'Teil_104_kreise.png')   
+
+
