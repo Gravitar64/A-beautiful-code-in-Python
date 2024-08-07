986cd1623a34467e0a8290d6b8779fd9e62a8cd7
Gravitar64 <gravitar@web.de>
2024-08-07 09:23:36 +0200

    V1

---
+++ Teil_110_Konvexe_Hülle.py
@@ -0,0 +1,43 @@
+import pygame as pg, random as rnd, time
+
+
+def gift_wrapping(punkte):
+  startpunkt = pg.Vector2(min((p.x, p.y) for p in punkte))
+  hülle = [startpunkt]
+  
+  while True:
+    endpunkt = rnd.choice(punkte)
+    if startpunkt == endpunkt: continue
+    
+    for p in punkte:
+      if (endpunkt - startpunkt).cross(p - startpunkt) < 0: endpunkt = p
+    
+    if endpunkt == hülle[0]: return hülle
+    hülle.append(endpunkt)
+    punkte.remove(endpunkt)
+    startpunkt = endpunkt
+
+
+pg.init()
+größe = breite, höhe = 1920, 1080
+fenster = pg.display.set_mode(größe)
+clock = pg.time.Clock()
+FPS = 5
+
+start = time.perf_counter()
+punkte = [pg.Vector2(rnd.randrange(breite), rnd.randrange(höhe)) for _ in range(25)]
+hülle = gift_wrapping(punkte)
+print(f'Ermittelt in {(time.perf_counter() - start)*1000:,.3f} ms')
+
+while True:
+  clock.tick(FPS)
+
+  for ereignis in pg.event.get():
+    if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
+
+  for punkt in punkte:  pg.draw.circle(fenster, 'white', punkt, 5)
+  for punkt in hülle:   pg.draw.circle(fenster, 'green', punkt, 10)
+
+  pg.draw.polygon(fenster, 'green', hülle, 2)
+
+  pg.display.flip()
