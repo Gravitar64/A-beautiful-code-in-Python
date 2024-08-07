3f273843ad607407de1a2ce5ecb3a3e3c6568030
Gravitar64 <gravitar@web.de>
2024-08-07 08:27:52 +0200

    Performance Update

--- teil_110_Konvexe_Hülle.py
+++ teil_110_Konvexe_Hülle.py
@@ -8,25 +8,23 @@ def gift_wrapping(punkte):
     endpunkt = rnd.choice(punkte)
     if startpunkt == endpunkt: continue
     for p in punkte:
-      if p == (startpunkt or endpunkt): continue
-      if (endpunkt - startpunkt).cross(p - startpunkt) < 0:
-        endpunkt = p
+      if (endpunkt - startpunkt).cross(p - startpunkt) < 0: endpunkt = p
     if endpunkt == hülle[0]: return hülle
     startpunkt = endpunkt
     hülle.append(startpunkt)
+    punkte.remove(startpunkt)
 
 
 pg.init()
 größe = breite, höhe = 1920, 1080
 fenster = pg.display.set_mode(größe)
 clock = pg.time.Clock()
-FPS = 40
+FPS = 5
 
 start = time.perf_counter()
-punkte = [pg.Vector2(rnd.randrange(breite), rnd.randrange(höhe)) for _ in range(25)]
+punkte = [pg.Vector2(rnd.randrange(breite), rnd.randrange(höhe)) for _ in range(16)]
 hülle = gift_wrapping(punkte)
 print(time.perf_counter() - start)
-
 
 while True:
   clock.tick(FPS)
@@ -34,10 +32,9 @@ while True:
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
 
-  for punkt in punkte:
-    farbe = 'green' if punkt in hülle else 'white'
-    größe = 10 if farbe == 'green' else 5
-    pg.draw.circle(fenster, farbe, punkt, größe)
+  for punkt in punkte:  pg.draw.circle(fenster, 'white', punkt, 5)
+  for punkt in hülle:   pg.draw.circle(fenster, 'green', punkt, 10)
 
   pg.draw.polygon(fenster, 'green', hülle, 2)
+
   pg.display.flip()
