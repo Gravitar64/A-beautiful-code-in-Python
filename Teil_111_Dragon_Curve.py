3d3597c0dad7953d764be7afebf6e4cc510829e7
Gravitar64 <gravitar@web.de>
2024-08-21 13:14:33 +0200

    V2

--- Teil_111_Dragon_Curve.py
+++ Teil_111_Dragon_Curve.py
@@ -1,39 +1,50 @@
 import pygame as pg, copy
 
 
-def rotiere_kurve(kurve):
+def rotiere_kurve(kurve, grad):
   rotiert = copy.deepcopy(kurve)
   drehpunkt = kurve[-1]
 
-  for _ in range(30):
-    for punkt in rotiert:
-      punkt -= drehpunkt
-      punkt.rotate_ip(-3)
-      punkt += drehpunkt
+  for punkt in rotiert:
+    punkt -= drehpunkt
+    punkt.rotate_ip(grad)
+    punkt += drehpunkt
 
-    clock.tick(FPS)
-    fenster.fill('black')
-    pg.draw.lines(fenster, 'green', False, kurve)
-    pg.draw.lines(fenster, 'green', False, rotiert)
-    pg.draw.circle(fenster, 'red', drehpunkt, 5)
-    pg.display.flip()
+  return rotiert
 
-  kurve.extend(rotiert[-2::-1])
-  return kurve
+
+def zeichne_kurven():
+  clock.tick(FPS)
+  fenster.fill('black')
+  pg.draw.lines(fenster, 'green', False, kurve)
+  if rotiert: pg.draw.lines(fenster, 'green', False, rotiert)
+  pg.draw.circle(fenster, 'red', kurve[-1], 5)
+  pg.display.flip()
 
 
 pg.init()
 größe = breite, höhe = pg.Vector2(1920, 1080)
 fenster = pg.display.set_mode(größe)
 clock = pg.time.Clock()
-FPS = 40
-start = pg.Vector2(größe / 2)
+FPS = 20
 
-curve = [start, start + pg.Vector2(200, 0)]
+start = pg.Vector2(größe / 2)
+kurve = [start, start + pg.Vector2(200, 0)]
+rotiert = []
+drag = mouse_pos = False
 
 
 while True:
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
     if ereignis.type == pg.MOUSEBUTTONDOWN:
-      curve = rotiere_kurve(curve)
+      match ereignis.button:
+
+        case 1:
+          for grad in range(0, 91, 3):
+            rotiert = rotiere_kurve(kurve, grad)
+            zeichne_kurven()
+          kurve.extend(rotiert[-2::-1])
+          rotiert = []
+
+  zeichne_kurven()
