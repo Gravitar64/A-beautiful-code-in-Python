68c9f426686eeac94c4d9dff3110ce1b0e5753be
Gravitar64 <gravitar@web.de>
2024-08-22 14:19:02 +0200

    YouTube

--- Teil_109_Dragon_Curve.py
+++ Teil_109_Dragon_Curve.py
@@ -1,7 +1,7 @@
 import pygame as pg, copy
 
 
-def rotiere_kurve(grad):
+def rotiere_kurve(kurve, grad):
   rotiert = copy.deepcopy(kurve)
   drehpunkt = kurve[-1]
 
@@ -14,7 +14,7 @@ def rotiere_kurve(grad):
 
 
 def zeichne_kurven():
-  clock.tick(FPS)
+  clock.tick(40)
   fenster.fill('black')
   pg.draw.lines(fenster, 'green', False, kurve)
   if rotiert: pg.draw.lines(fenster, 'green', False, rotiert)
@@ -25,26 +25,23 @@ def zeichne_kurven():
 pg.init()
 größe = breite, höhe = pg.Vector2(1920, 1080)
 fenster = pg.display.set_mode(größe)
-clock = pg.time.Clock()
-FPS = 20
-
-start = pg.Vector2(größe / 2)
+start = größe / 2
 kurve = [start, start + pg.Vector2(200, 0)]
 rotiert = []
-drag = mouse_pos = False
 
+
+clock = pg.time.Clock()
+FPS = 40
 
 while True:
+
   for ereignis in pg.event.get():
     if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
     if ereignis.type == pg.MOUSEBUTTONDOWN:
-      match ereignis.button:
-
-        case 1:
-          for grad in range(0, 91, 3):
-            rotiert = rotiere_kurve(grad)
-            zeichne_kurven()
-          kurve.extend(rotiert[-2::-1])
-          rotiert = []
+      for grad in range(0, 91, 3):
+        rotiert = rotiere_kurve(kurve, grad)
+        zeichne_kurven()
+      kurve.extend(rotiert[-2::-1])
+      rotiert = []
 
   zeichne_kurven()
