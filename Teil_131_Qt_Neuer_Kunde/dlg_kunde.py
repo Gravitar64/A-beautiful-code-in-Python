eb92711145745fd76854dcd45f3c18f28993f7da
Gravitar64 <gravitar@web.de>
2026-04-18 12:32:17 +0200

    YouTube-Video

---
+++ Teil_131_Qt_Neuer_Kunde/dlg_kunde.py
@@ -0,0 +1,41 @@
+from PySide6.QtWidgets import QDialog, QDataWidgetMapper
+from ui_dlg_kunde import Ui_Dlg_Kunde
+
+class Dlg_kunde(QDialog, Ui_Dlg_Kunde):
+  def __init__(self, parent, model, row):
+    super().__init__(parent)
+    self.setupUi(self)
+    self.show()
+
+    self.mapper = QDataWidgetMapper(self)
+    self.mapper.setModel(model)
+    self.mapper.setSubmitPolicy(QDataWidgetMapper.SubmitPolicy.ManualSubmit)
+
+    self.mapper.addMapping(self.Te_Anrede,2)
+    self.mapper.addMapping(self.Te_Titel,3)
+    self.mapper.addMapping(self.Te_Vorname, 4)
+    self.mapper.addMapping(self.Te_Nachname, 5)
+    self.mapper.addMapping(self.Te_Strasse, 6)
+    self.mapper.addMapping(self.Te_Land, 7)
+    self.mapper.addMapping(self.Te_PLZ, 8)
+    self.mapper.addMapping(self.Te_Ort, 9)
+    self.mapper.addMapping(self.Te_Tel, 10)
+    self.mapper.addMapping(self.Te_Mobile, 11)
+    self.mapper.addMapping(self.Te_EMail, 12)
+    self.mapper.addMapping(self.Te_Hinweis, 13)
+
+    self.mapper.setCurrentIndex(row)
+
+    self.buttonBox.accepted.connect(self.daten_übernehmen)
+
+  def daten_übernehmen(self):
+    self.mapper.submit()
+    super().accept()
+      
+    
+
+  
+
+
+
+
---
+++ Teil_131_Qt_Neuer_Kunde/main.py
@@ -0,0 +1,63 @@
+from PySide6 import QtWidgets, QtSql
+from ui_main_window import Ui_MainWindow
+from dlg_kunde import Dlg_kunde
+
+class Frm_main(QtWidgets.QMainWindow, Ui_MainWindow):
+  def __init__(self):
+    super().__init__()
+    self.setupUi(self)
+    self.mod_kunden = QtSql.QSqlTableModel()
+    self.mod_kunden.setTable('kunden')
+    self.tbv_kunden.setModel(self.mod_kunden)
+    self.mod_kunden.select()
+
+    for i in range(17):
+      if i in {4,5,6,8,9,13}: continue
+      self.tbv_kunden.hideColumn(i)
+    self.tbv_kunden.resizeColumnsToContents()
+
+    self.mod_buchungen = QtSql.QSqlTableModel()
+    self.mod_buchungen.setTable('buchungen')
+    self.tbv_buchungen.setModel(self.mod_buchungen)
+    
+    for i in range(27):
+      if i in {2,4,5,6,20}: continue
+      self.tbv_buchungen.hideColumn(i)
+    
+
+    self.le_suche.textChanged.connect(self.suche)
+    self.tbv_kunden.selectionModel().selectionChanged.connect(self.selection_kunde_changed)
+    self.bt_neuer_kunde.clicked.connect(self.neuer_kunde)
+    self.bt_kunde_aendern.clicked.connect(self.kunde_ändern)
+
+  def suche(self, text):
+    self.mod_kunden.setFilter(f'vorname || " " || nachname like "%{text}%"') 
+
+  def selection_kunde_changed(self, selected, deselected):
+    index = self.tbv_kunden.currentIndex()
+    if index.isValid():
+      kd_index = self.mod_kunden.index(index.row(), 0)
+      kd_id = self.mod_kunden.data(kd_index)
+      self.mod_buchungen.setFilter(f'kd_id = {kd_id}')
+      self.mod_buchungen.select()
+      self.tbv_buchungen.resizeColumnsToContents()
+
+  def neuer_kunde(self):
+    row = self.mod_kunden.rowCount()
+    self.mod_kunden.insertRow(row)
+    dialog = Dlg_kunde(self, self.mod_kunden, row)
+
+  def kunde_ändern(self):
+    index = self.tbv_kunden.currentIndex()
+    if index.isValid():
+      dialog = Dlg_kunde(self, self.mod_kunden, index.row())
+
+
+
+app = QtWidgets.QApplication()
+db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+db.setDatabaseName('db.sqlite')
+
+frm_main = Frm_main()
+frm_main.show()
+app.exec()
